d30049c62c72b521cd124b8463da8b1ae3d8bd47
no_author <another@cornell.edu>
2002-04-23 15:14:05 +0000

    Handle insets correctly.  Use Apple's suggested 12-pixel spacing between buttons, and none outside of that.  Looks beautiful.

--- Source/corina/gui/ButtonLayout.java
+++ Source/corina/gui/ButtonLayout.java
@@ -4,15 +4,18 @@ import java.awt.LayoutManager;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Insets;
 
 // lays out items like a 1-row gridlayout inside a right-side flowlayout,
 // just as most (apple's, sun's) HIGs say:
 //                                           [ Cancel ] [   OK   ]
 
 public class ButtonLayout implements LayoutManager {
-    private static final int LEADING = 10; // hmm...
+    private static final int LEADING = 12; // says apple
     
     public void layoutContainer(Container parent) {
+        Insets border = parent.getInsets();
+
         synchronized (parent.getTreeLock()) {
             int maxwidth=0;
             int n = parent.getComponentCount();
@@ -23,18 +26,21 @@ public class ButtonLayout implements LayoutManager {
                     maxwidth = Math.max(maxwidth, d.width);
                 }
             }
+            int wholeWidth = n*maxwidth + (n-1)*LEADING;
             for (int i=0; i<n; i++) { // second pass: cut
                 Component m = parent.getComponent(i);
                 if (m.isVisible()) {
                     Dimension d = m.getPreferredSize();
                     m.setSize(maxwidth, d.height);
-                    m.setLocation(parent.getWidth() - (LEADING + maxwidth)*(n-i), LEADING);
+                    m.setLocation(parent.getWidth() - wholeWidth - border.right + i*maxwidth + i*LEADING, border.top);
                 }
             }
         }
     }
 
     private Dimension getLayoutSize(Container parent, boolean isPreferred) {
+        Insets border = parent.getInsets();
+
         Dimension d = new Dimension(0, 0);
 
         int n = parent.getComponentCount();
@@ -44,10 +50,12 @@ public class ButtonLayout implements LayoutManager {
             d.height = Math.max(d.height, s.height);
             d.width = Math.max(d.width, s.width);
         }
-        d.width *= n;
+        d.width = (n-1)*LEADING + n*d.width;
 
-        d.height += 2*LEADING;
-        d.width += 2*LEADING;
+        // insets
+        d.width += border.left + border.right;
+        d.height += border.top + border.bottom;
+
         return d;
     }
     public Dimension minimumLayoutSize(Container parent) {
