50f862c0219c22e74356e1e1ee87b0b99b05f93a
no_author <another@cornell.edu>
2002-04-29 17:12:15 +0000

    Reindented and cleaned up some comments.
    Removed a completely unused "count rows" block.  (What was I thinking?)

--- Source/corina/prefs/PrefsDialog.java
+++ Source/corina/prefs/PrefsDialog.java
@@ -90,134 +90,125 @@ public class PrefsDialog extends JFrame {
 
     // the generic right-panel for a bunch of Options.  THIS IS GODAWFUL UGLY.  BREAK IT DOWN INTO METHODS OR SOMETHING.
     private class OptionsPanel extends JPanel {
-	public OptionsPanel(String category) {
-	    // gui setup: gridbag!
-	    GridBagLayout gridbag = new GridBagLayout();
-	    GridBagConstraints c = new GridBagConstraints();
-	    super.setLayout(gridbag);
+        public OptionsPanel(String category) {
+            // gui setup: gridbag!
+            GridBagLayout gridbag = new GridBagLayout();
+            GridBagConstraints c = new GridBagConstraints();
+            super.setLayout(gridbag);
 
-	    // global constraints
-	    c.fill = GridBagConstraints.HORIZONTAL;
-	    c.anchor = GridBagConstraints.NORTHWEST;
-	    c.insets = new Insets(3, 3, 3, 3); // half of 6...?
-	    c.weighty = 0.0;
+            // global constraints
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.anchor = GridBagConstraints.NORTHWEST;
+            c.insets = new Insets(3, 3, 3, 3); // half of 6...?
+            c.weighty = 0.0;
 
-	    // count rows
-	    int nrows=0;
-	    for (int i=0; i<options.size(); i++) {
-		PrefsTemplate.Option o = (PrefsTemplate.Option) options.get(i);
-		if (o.category.equals(category))
-		    nrows++;
-	    }
+            int row = 0;
+            for (int i=0; i<options.size(); i++) {
+                PrefsTemplate.Option o = (PrefsTemplate.Option) options.get(i);
+                if (o.category.equals(category)) {
+                    // grid position
+                    c.gridy = row++;
 
-	    int row = 0;
-	    for (int i=0; i<options.size(); i++) {
-		PrefsTemplate.Option o = (PrefsTemplate.Option) options.get(i);
-		if (o.category.equals(category)) {
-		    // grid position
-		    c.gridy = row++;
-
-		    // create and add the constrained label
-		    c.gridx = 0;
-		    c.weightx = 0.25;
+                    // create and add the constrained label
+                    c.gridx = 0;
+                    c.weightx = 0.25;
                     if (o.type != PrefsTemplate.Option.TYPE_BOOL) {
-//			super.add(new JLabel(o.description, SwingConstants.RIGHT), c);
+                        //	super.add(new JLabel(o.description, SwingConstants.RIGHT), c);
                         // does a nested panel help vertical alignment?
                         JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
                         panel.add(new JLabel(o.description, SwingConstants.RIGHT));
                         super.add(panel, c);
                     }
 
-		    // constraints for the edit box
-		    c.gridx = 1;
-		    c.weightx = 0.75;
+                    // constraints for the edit box
+                    c.gridx = 1;
+                    c.weightx = 0.75;
 
-		    switch (o.type) {
+                    switch (o.type) {
 
-		    case PrefsTemplate.Option.TYPE_COLOR: {
-			// popup
-			ColorPopup p = new ColorPopup(Color.decode(o.value), o);
-			JPanel t = new JPanel(new FlowLayout(FlowLayout.LEFT));
-			t.add(p);
-			super.add(t, c);
-			break;
-		    }
+                        case PrefsTemplate.Option.TYPE_COLOR: {
+                            // popup
+                            ColorPopup p = new ColorPopup(Color.decode(o.value), o);
+                            JPanel t = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                            t.add(p);
+                            super.add(t, c);
+                            break;
+                        }
 
-		    case PrefsTemplate.Option.TYPE_BOOL: {
-			final PrefsTemplate.Option glue = o;
-			JCheckBox t = new JCheckBox(o.description);
-			t.setSelected(o.value.equals("true"));
-			t.addChangeListener(new ChangeListener() {
-				public void stateChanged(ChangeEvent ce) {
-				    glue.value = (glue.value.equals("true") ? "false" : "true");
-				}
-			    });
-			super.add(t, c);
-			break;
-		    }
+                        case PrefsTemplate.Option.TYPE_BOOL: {
+                            final PrefsTemplate.Option glue = o;
+                            JCheckBox t = new JCheckBox(o.description);
+                            t.setSelected(o.value.equals("true"));
+                            t.addChangeListener(new ChangeListener() {
+                                public void stateChanged(ChangeEvent ce) {
+                                    glue.value = (glue.value.equals("true") ? "false" : "true");
+                                }
+                            });
+                            super.add(t, c);
+                            break;
+                        }
 
-		    case PrefsTemplate.Option.TYPE_FONT: {
-			final PrefsTemplate.Option glue = o;
+                        case PrefsTemplate.Option.TYPE_FONT: {
+                            final PrefsTemplate.Option glue = o;
 
-			JPanel flow = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                            JPanel flow = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-			final JLabel p = new JLabel("Sample");
-			p.setFont(Font.decode(glue.value));
+                            final JLabel p = new JLabel("Sample");
+                            p.setFont(Font.decode(glue.value));
 
-			JButton b = new JButton("Change font");
-			b.addActionListener(new AbstractAction() {
-				public void actionPerformed(ActionEvent ae) {
-				    String f = FontChooser.showDialog(me /* !!! */, "Choose new font", glue.value);
-				    if (f != null)
-					glue.value = f;
-				    p.setFont(Font.decode(glue.value));
-				}
-			    });
+                            JButton b = new JButton("Change font");
+                            b.addActionListener(new AbstractAction() {
+                                public void actionPerformed(ActionEvent ae) {
+                                    String f = FontChooser.showDialog(me /* !!! */, "Choose new font", glue.value);
+                                    if (f != null)
+                                        glue.value = f;
+                                    p.setFont(Font.decode(glue.value));
+                                }
+                            });
 
-                        flow.add(p);
-			flow.add(b);
+                            flow.add(p);
+                            flow.add(b);
 
-			super.add(flow, c);
-			break;
-		    }
+                            super.add(flow, c);
+                            break;
+                        }
 
-		    default: { // must be PrefsTemplate.Option.TYPE_STRING
-			// create the edit box
-			final JTextField t = new JTextField(System.getProperty(o.property, ""));
+                        default: { // must be PrefsTemplate.Option.TYPE_STRING
+                                   // create the edit box
+                            final JTextField t = new JTextField(System.getProperty(o.property, ""));
 
-			// listener
-			final PrefsTemplate.Option glue = o; // needed?
-			t.getDocument().addDocumentListener(new DocumentListener() {
-				public void changedUpdate(DocumentEvent e) {
-				    update(e);
-				}
-				public void insertUpdate(DocumentEvent e) {
-				    update(e);
-				}
-				public void removeUpdate(DocumentEvent e) {
-				    update(e);
-				}
-				private void update(DocumentEvent e) {
-				    // get text from the widget, store it
-				    glue.value = t.getText();
-				}
-			    });
+                            // listener
+                            final PrefsTemplate.Option glue = o; // needed?
+                            t.getDocument().addDocumentListener(new DocumentListener() {
+                                public void changedUpdate(DocumentEvent e) {
+                                    update(e);
+                                }
+                                public void insertUpdate(DocumentEvent e) {
+                                    update(e);
+                                }
+                                public void removeUpdate(DocumentEvent e) {
+                                    update(e);
+                                }
+                                private void update(DocumentEvent e) {
+                                    // get text from the widget, store it
+                                    glue.value = t.getText();
+                                }
+                            });
 
-			// put it in
-			super.add(t, c);
-		    }
+                            // put it in
+                            super.add(t, c);
+                        }
+                    }
+                }
+            }
 
-		    }
-		}
-	    }
-
-	    // add glue to bottom -- this aligns everything to the top
-	    Component myBox = Box.createVerticalGlue();
-	    c.gridx = 0;
-	    c.gridy = row;
-	    c.weighty = 1.0;
-	    super.add(myBox, c);
-	}
+            // add glue to bottom -- this aligns everything to the top
+            Component myBox = Box.createVerticalGlue();
+            c.gridx = 0;
+            c.gridy = row;
+            c.weighty = 1.0;
+            super.add(myBox, c);
+        }
     }
 
     public static void updateAll() {
@@ -271,29 +262,30 @@ public class PrefsDialog extends JFrame {
 	front.  This prevents multiple preferences dialogs from
 	appearing, which would serve only to confuse the user. */
     public static void showPreferences() {
-	if (me == null) {
-	    me = new PrefsDialog();
-        } else { // FIXME: THIS CODE IS NEVER USED (?)
-	    me.setVisible(true); // despite DISPOSE_ON_CLOSE, it's just getting hidden?
-	    me.setState(NORMAL);
-	    me.toFront();
-	}
+        if (me == null) {
+            me = new PrefsDialog();
+        } else { // FIXME: THIS CODE IS NEVER USED (...why?)
+            me.setVisible(true); // despite DISPOSE_ON_CLOSE, it's just getting hidden?
+            me.setState(NORMAL);
+            me.toFront();
+        }
     }
 
     // show a specific tab
     public static void showPreferences(String tab) {
-	// show dialog
-	showPreferences();
+        // show dialog
+        showPreferences();
 
         // select the tab
-        for (int i=0; i<me.categories.size(); i++)
+        for (int i=0; i<me.categories.size(); i++) {
             if (me.categories.get(i).equals(tab)) {
                 me.tabs.setSelectedIndex(i);
                 return;
             }
+        }
 
-                // note, if we're given a bogus tab name, it just falls
-                // through, and the first tab remains selected.
+        // note: if we're given a bogus tab name, it just falls
+        // through, and the first tab remains selected.
     }
 
     private JTabbedPane tabs;
@@ -304,12 +296,12 @@ public class PrefsDialog extends JFrame {
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE); // dispose?  why?
         setResizable(false);
 
-	// icon!
-	setIconImage(((ImageIcon) JarIcon.getJavaIcon("toolbarButtonGraphics/general/Preferences16.gif")).getImage());
+        // icon!
+        setIconImage(((ImageIcon) JarIcon.getJavaIcon("toolbarButtonGraphics/general/Preferences16.gif")).getImage());
 
-	// get categories, options from Prefs
-	categories = PrefsTemplate.getCategories();
-	options = PrefsTemplate.getOptions();
+        // get categories, options from Prefs
+        categories = PrefsTemplate.getCategories();
+        options = PrefsTemplate.getOptions();
 
         // use my own panel
         JPanel p = new JPanel();
@@ -366,7 +358,7 @@ public class PrefsDialog extends JFrame {
         // pack
         pack();
 
-        // center it
+        // center it -- REFACTOR ME
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(new Point(d.width/2 - getWidth()/2,
                               d.height/2 - getHeight()/2));
@@ -374,4 +366,4 @@ public class PrefsDialog extends JFrame {
         // show it
         show();
     }
-}	
+}
