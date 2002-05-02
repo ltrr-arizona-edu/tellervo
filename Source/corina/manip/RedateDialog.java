5ad3fdd7b7abe4c04137794f17f35d20abaf61da
no_author <another@cornell.edu>
2002-05-02 00:25:11 +0000

    Removed unused imports.

--- Source/corina/manip/RedateDialog.java
+++ Source/corina/manip/RedateDialog.java
@@ -31,9 +31,6 @@ import java.util.ResourceBundle;
 
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
