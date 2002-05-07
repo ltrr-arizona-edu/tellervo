a61222602f18d3b5a50c03a83c0b1ff3b81dcaca
no_author <another@cornell.edu>
2002-05-07 16:50:43 +0000

    Get icons from my classloader, not the system one (Web Start).

--- Source/corina/graph/GraphFrame.java
+++ Source/corina/graph/GraphFrame.java
@@ -349,26 +349,27 @@ public class GraphFrame extends XFrame implements SampleListener,
 	ButtonGroup bg = new ButtonGroup();
 	double t = Double.parseDouble(System.getProperty("corina.graph.thickness", "1.0"));
 
-	for (int i=1; i<=3; i++) {
-	    String iconFile = "Images/line-" + i + ".png";
+        for (int i=1; i<=3; i++) {
+            String iconFile = "Images/line-" + i + ".png";
 
-	    final int glue = i;
+            final int glue = i;
 
-	    JMenuItem lwi = new JRadioButtonMenuItem(new ImageIcon(ClassLoader.getSystemResource(iconFile)));
-	    lwi.addActionListener(new AbstractAction() {
-		    public void actionPerformed(ActionEvent e) {
-			// set corina.graph.thickness to |t|, fire something
-			System.setProperty("corina.graph.thickness", String.valueOf(glue));
-			PrefsDialog.updateAll();
-		    }
-		});
-	    lw.add(lwi);
-	    bg.add(lwi);
+            ClassLoader cl = this.getClass().getClassLoader();
+            JMenuItem lwi = new JRadioButtonMenuItem(new ImageIcon(cl.getResource(iconFile)));
+            lwi.addActionListener(new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    // set corina.graph.thickness to |t|, fire something
+                    System.setProperty("corina.graph.thickness", String.valueOf(glue));
+                    PrefsDialog.updateAll();
+                }
+            });
+            lw.add(lwi);
+            bg.add(lwi);
 
-	    // is this my default?
-	    if (Math.abs((double)i - t) < 0.001) // beautify me
-		lwi.setSelected(true);
-	}
+            // is this my default?
+            if (Math.abs((double)i - t) < 0.001) // beautify me
+                lwi.setSelected(true);
+        }
 
 	// (return the menus)
 	return new JMenu[] { v, lw }; // lw is a work-in-progress
