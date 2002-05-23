2c34f4ae2f4da7d6e814a44ee1d863cd7d8de034
no_author <another@cornell.edu>
2002-05-23 00:15:01 +0000

    Added spreadsheet and range exporters, which used to be scripts.

---
+++ Source/corina/files/RangesOnly.java
@@ -0,0 +1,50 @@
+package corina.files;
+
+import corina.Sample;
+import corina.Element;
+
+import java.io.IOException;
+
+public class RangesOnly extends Filetype {
+
+    // can't load a ranges-only file.  this should never get called,
+    // but in case it does...
+    public Sample load() throws IOException {
+        throw new WrongFiletypeException();
+    }
+
+    // save
+    public void save(Sample sample) throws IOException {
+        // verify it's a master
+        if (sample.elements == null)
+            throw new IOException("Not a master"); // i'm so helpful.
+
+        int n = sample.elements.size();
+        String eoln = System.getProperty("line.separator");
+        for (int i=0; i<n; i++) {
+            Element e = (Element) sample.elements.get(i);
+            e.loadMeta(); // this aborts if element can't be loaded.
+
+            // output (name, start, end)
+            if (e.details.containsKey("title"))
+                w.write(e.details.get("title").toString());
+            else
+                w.write(e.details.get("filename").toString());
+            w.write("\t");
+            w.write(e.range.getStart().toString());
+            w.write("\t");
+            w.write(e.range.getEnd().toString());
+            w.write(eoln);
+        }
+
+        w.close();
+    }
+
+    public String toString() {
+        return "Ranges Only";
+    }
+
+    public char getMnemonic() {
+        return 'R';
+    }
+}
---
+++ Source/corina/files/Spreadsheet.java
@@ -0,0 +1,70 @@
+package corina.files;
+
+import corina.Year;
+import corina.Sample;
+import corina.Element;
+
+import java.io.IOException;
+
+public class Spreadsheet extends Filetype {
+
+    // can't load a spreadsheet file.  this should never get called,
+    // but in case it does...
+    public Sample load() throws IOException {
+        throw new WrongFiletypeException();
+    }
+
+    // save
+    public void save(Sample sample) throws IOException {
+        // verify it's a master
+        if (sample.elements == null)
+            throw new IOException("Not a master"); // i'm so helpful.
+
+        // number of elements
+        int n = sample.elements.size();
+
+        // load all the elements into a buffer
+        Sample buf[] = new Sample[n];
+        for (int i=0; i<n; i++)
+            buf[i] = ((Element) sample.elements.get(i)).load();
+
+        // write header
+        String eoln = System.getProperty("line.separator");
+        w.write("Year");
+        for (int i=0; i<n; i++)
+            w.write("\t" + buf[i].meta.get("title"));
+        w.write(eoln);
+
+        // save line-at-a-time
+        for (Year y=sample.range.getStart(); y.compareTo(sample.range.getEnd())<=0; y=y.add(1)) {
+            // write year
+            w.write(y.toString());
+
+            // write each datum for this year
+            for (int i=0; i<n; i++) {
+                // tab
+                w.write("\t");
+
+                // data[y]
+                if (buf[i].range.contains(y)) {
+                    Year start = buf[i].range.getStart();
+                    int index = y.diff(start);
+                    w.write(buf[i].data.get(index).toString());
+                }
+            }
+
+            // end-of-line
+            w.write(eoln);
+        }
+        
+        w.close();
+    }
+
+    public String toString() {
+        return "Spreadsheet";
+    }
+
+    public char getMnemonic() {
+        return 'S';
+    }
+}
