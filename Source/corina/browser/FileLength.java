68d7c14f18b4a6ec2c82c8606424043082dedc76
no_author <another@cornell.edu>
2002-05-30 16:32:45 +0000

    Extend Number, so I get the benefits of being sorted like other numbers.

--- Source/corina/browser/FileLength.java
+++ Source/corina/browser/FileLength.java
@@ -9,14 +9,21 @@ import java.text.DecimalFormat;
 // (i'd extend Long, if they'd let me.)
 // the length of a folder is displayed as "--", and sorts as the smallest-sized file.
 // javadoc me!
-public class FileLength implements Comparable {
+public class FileLength extends Number implements Comparable {
     private long length;
-    public FileLength(long length) {
-        this.length = length;
-    }
+
+    // the length of a file
     public FileLength(File file) {
         this.length = (file.isDirectory() ? -1 : file.length());
     }
+
+    // why would you ever want this?  in exportdialog, i use it to measure
+    // the size of a buffer that will be saved later.
+    public FileLength(long length) {
+        this.length = length;
+    }
+
+    // compare, just like any other number does it
     public int compareTo(Object o2) {
         FileLength f2 = (FileLength) o2;
         return (length < f2.length ? -1 : (length == f2.length ? 0 : +1));
@@ -40,21 +47,39 @@ public class FileLength implements Comparable {
 
         double b = (float) length;
         for (int i=0; i<PREFIXES.length; i++) {
-            b /= 1024;
-            if (b < 1024) {
+            b /= K;
+            if (b < K)
                 return fmt.format(b) + " " + PREFIXES[i] + "B";
-            }
         }
 
         // can't happen.
         return "really, really big!";
     }
 
+    // bytes-per-K.  OAOO.  plus, maybe somebody likes 1K=1000B, which isn't completely unreasonable.
+    private static int K = 1024;
+
+    // format it prettily.
     public String toString() {
         // never called before => compute and store
         if (string == null)
             string = format(length);
 
         return string;
+    }
+
+    // let's extend Number, since, well, we are a number.
+    // plus we can get some stuff for free elsewhere (like, being sorted like a number, highest-to-lowest)
+    public double doubleValue() {
+        return (double) length;
+    }
+    public float floatValue() {
+        return (float) length;
+    }
+    public int intValue() {
+        return (int) length;
+    }
+    public long longValue() {
+        return length;
     }
 }
