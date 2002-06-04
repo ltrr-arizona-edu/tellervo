d8fc1e9d1a83e8100571c38dc9e3a71d38385b3a
no_author <another@cornell.edu>
2002-06-04 18:23:13 +0000

    Added string-almost-equals functionality.  When looking up the code for a string, if it's right to 80%, we take it.  Yes, this feature really is as cool as it sounds.

--- Source/corina/Species.java
+++ Source/corina/Species.java
@@ -53,7 +53,7 @@ public class Species {
         Enumeration e = species.keys();
         while (e.hasMoreElements()) {
             String maybe = (String) e.nextElement();
-            if (((String) species.get(maybe)).equalsIgnoreCase(s))
+            if (stringAlmostEquals((String) species.get(maybe), s))
                 return maybe;
         }
         throw new UnknownSpeciesException();
@@ -78,4 +78,45 @@ public class Species {
             common.add(t.nextToken());
         // sums will need the same sort of routine: "QUSP,PISP" => { "QUSP", "PISP" } => "Quercus, Pinus"
     }
+
+    // "string-almost-equals" algorithm.  good for finding typos.
+    // isn't species-specific, but i only plan to use it here.
+    // taken from: http://www.faqts.com/knowledge_base/view.phtml/aid/4418/fid/538
+    // (originally in python)
+    public static int stringDistance(String a, String b) {
+        int n = a.length();
+        int m = b.length();
+        int c[][] = new int[n+1][m+1];
+
+        for (int i=0; i<n+1; i++)
+            c[i][0] = i;
+        for (int j=0; j<m+1; j++)
+            c[0][j] = j;
+
+        for (int i=1; i<n+1; i++) {
+            for (int j=1; j<m+1; j++) {
+                int x = c[i-1][j] + 1;
+                int y = c[i][j-1] + 1;
+                int z = c[i-1][j-1];
+                if (a.charAt(i-1) != b.charAt(j-1))
+                    z++;
+                c[i][j] = Math.min(Math.min(x, y), z);
+            }
+        }
+
+        return c[n][m];
+    }
+    public static boolean stringAlmostEquals(String s1, String s2, int typosAllowed) {
+        return (stringDistance(s1, s2) <= typosAllowed);
+    }
+    public static boolean stringAlmostEquals(String s1, String s2) {
+        int distance = stringDistance(s1, s2);
+        int minLength = Math.min(s1.length(), s2.length());
+        return (distance / (double) minLength <= 0.20); // 80% ok?
+    }
+
+    // IDEA: this algorithm could be used to implement a findClosestMatch(String, String[]),
+    // which could be quite powerful.
+    // (it'd be a hell of a lot easier in lisp with #key, though.  what am i supposed to do,
+    // use reflection for every member-access?)
 }
