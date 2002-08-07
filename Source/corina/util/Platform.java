eca1004f8530b0828684be0f3637f4a278aee4cb
no_author <another@cornell.edu>
2002-08-07 14:45:19 +0000

    Changed isMac detection to method described in TN2042.
    Added setModified() method.

--- Source/corina/util/Platform.java
+++ Source/corina/util/Platform.java
@@ -1,9 +1,21 @@
 package corina.util;
 
+import javax.swing.JFrame;
+
 // platform-specific crap that java can't take care of.
 // (gee, thanks, james.)
 
 public class Platform {
-    public static final boolean isMac = System.getProperty("os.name").startsWith("Mac OS");
-}
+    // method from TN2042, http://developer.apple.com/technotes/tn/tn2042.html
+    public static final boolean isMac = (System.getProperty("mrj.version") != null);
+    public static final boolean isWindows = (System.getProperty("os.name").indexOf("Windows") != -1);
 
+    // mac-only -- windows prepends "* "
+    public static void setModified(JFrame window, boolean mod) {
+        if (isMac)
+            window.getRootPane().putClientProperty("windowModified",
+                                                   mod ? Boolean.TRUE : Boolean.TRUE); // no news is good news
+
+        // REFACTOR: add this to xframe, or whatever my document window is
+    }
+}
