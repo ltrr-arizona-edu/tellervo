ac60de1d9d8c020d4faa35efaa96b3009ff5aadb
no_author <another@cornell.edu>
2002-10-29 17:29:56 +0000

    Use floats internally; it appears to lose no precision.
    Compute significance based on a lookup table by overlap-length.

--- Source/corina/cross/TScore.java
+++ Source/corina/cross/TScore.java
@@ -55,23 +55,23 @@ import java.util.Arrays;
 
      </ul>
 
-     <li>For each possible overlap, compute the T-score:</li>
+     <li>For each possible overlap, compute the r (correlation coefficient):</li>
 
      <blockquote>
-     <i>s</i><sub>1</sub> = &Sigma;
+     <i>s</i><sub>1</sub> = <big>&Sigma;</big>
              ( <i>x</i><sub>i</sub><i>y</i><sub>i</sub> -
 	       <i>N</i> ( <i>x</i><sub>i</sub> - <i>x</i><sub>avg</sub> )
 	                ( <i>y</i><sub>i</sub> - <i>y</i><sub>avg</sub> ) )
      </blockquote>
 
      <blockquote>
-     <i>s</i><sub>2</sub> = &Sigma;
+     <i>s</i><sub>2</sub> = <big>&Sigma;</big>
              ( <i>x</i><sub>i</sub><sup>2</sup> -
 	       <i>N</i> ( <i>x</i><sub>i</sub> - <i>x</i><sub>avg</sub> )<sup>2</sup> )
      </blockquote>
 
      <blockquote>
-     <i>s</i><sub>2</sub> = &Sigma;
+     <i>s</i><sub>3</sub> = <big>&Sigma;</big>
              ( <i>y</i><sub>i</sub><sup>2</sup> -
 	       <i>N</i> ( <i>y</i><sub>i</sub> - <i>y</i><sub>avg</sub> )<sup>2</sup> )
      </blockquote>
@@ -80,11 +80,15 @@ import java.util.Arrays;
      <i>r</i> = <i>s</i><sub>1</sub> / &radic;( <i>s</i><sub>2</sub> <i>s</i><sub>3</sub> )
      </blockquote>
 
-     <blockquote>
-     <i>t</i> = <i>r</i> &radic;( (<i>N</i> - 2) / (1 - <i>r</i><sup>2</sup>) )
-     </blockquote>
+ <li>Compute the t-score:</li>
 
+ <blockquote>
+ <i>t</i> = <i>r</i> &radic;( (<i>N</i> - 2) / (1 - <i>r</i><sup>2</sup>) )
+ </blockquote>
+ 
    </ol>
+
+   <p>This class is dedicated to poor Mr. Potter out in Van Nuys.</p>
 
    @author <a href="mailto:kbh7@cornell.edu">Ken Harris</a>
    @version $Id$ */
@@ -97,16 +101,16 @@ public class TScore extends Cross {
     private final static int WINDOW=5;
 
     /** A mutable copy of the fixed data, in array form. */
-    private double fixedData[];
+    private float fixedData[];
 
     /** A mutable copy of the moving data, in array form. */
-    private double movingData[];
+    private float movingData[];
 
     /** The mean of the fixed data. */
-    private double fixedMean=0.0;
+    private float fixedMean=0.0f;
 
     /** The mean of the moving data. */
-    private double movingMean=0.0;
+    private float movingMean=0.0f;
 
     // don't use me
     protected TScore() { }
@@ -130,113 +134,153 @@ public class TScore extends Cross {
 	return System.getProperty("corina.cross.tscore.format", "0.00");
     }
 
-    /** The minimum significant T-score, 2.55.
-	@return the minimum significant score */
-    public double getMinimumSignificant() {
-	return 2.55;
+    // for 99.5% confidence, from PIK's table
+    private static float table[] = {
+        63.657f,
+        9.925f,
+        5.841f,
+        4.604f,
+        4.032f,
+        3.707f,
+        3.499f,
+        3.355f,
+        3.250f,
+        3.169f,
+        3.106f,
+        3.055f,
+        3.012f,
+        2.977f,
+        2.947f,
+        2.921f,
+        2.898f,
+        2.878f,
+        2.861f,
+        2.845f,
+        2.831f,
+        2.819f,
+        2.807f,
+        2.797f,
+        2.787f,
+        2.779f,
+        2.771f,
+        2.763f,
+        2.756f,
+        2.750f,
+    };
+    public boolean isSignificant(double score, int overlap) {
+	if (overlap == 0) // it happens...
+	    return false;
+
+	// 2.704 is for overlap=40; 60 is 2.660, inf=2.576
+        float threshold = ((overlap <= 30) ? table[overlap-1] : 2.576f);
+
+        return (score >= threshold);
     }
 
-    /** Normalize the data.  This creates a new array of doubles from
+    public double getMinimumSignificant() {
+        return 2.55;
+    }
+    
+    /** Normalize the data.  This creates a new array of floats from
 	the List of Numbers.  The data is smoothed using a 5-point
 	window, and then the natural logarithm is taken.
 	@param data a List of Numbers holding the data to normalize
-	@return an array of doubles containing the normalized data */
-    private double[] normalize(List data) {
+	@return an array of floats containing the normalized data */
+    private float[] normalize(List data) {
         // size of the data: used many times below
         int N = data.size();
-        double smoothed[] = new double[N];
+        float smoothed[] = new float[N];
 
-/*
- for (int i=0; i<N; i++) {
-            int a = Math.max(i-WINDOW/2, 0);
-            int z = Math.min(i+WINDOW/2, N-1);
-
-            // what if sum stays 0.0?  rare, but it could happen.
-
-            // this isn't very efficient.  sum[i] = sum[i-1] -
-            // data[i-6] + data[i+5], with a few special cases --
-            // that's 2 flops per iteration instead of 10.
-
-            double sum=0.0;
-            for (int j=a; j<=z; j++)
-                sum += ((Number) data.get(j)).doubleValue();
-            double avg = sum/(z-a+1);
-            smoothed[i] = ((Number) data.get(i)).doubleValue() / avg; // avg; was: sum
-        }
-
-        // natural logarithm -- log(0) is bad, so log(max(s[i],eps))
-        for (int i=0; i<smoothed.length; i++)
-            smoothed[i] = Math.log(Math.max(100.0*smoothed[i], Double.MIN_VALUE));
-        */
-
-        // TESTING: make sure everything is positive (for GAZ37ABC.TRU)
+        // make sure everything is positive (for GAZ37ABC.TRU)
+	Integer one = new Integer(1); // (singleton)
         for (int i=0; i<N; i++)
             if (((Number) data.get(i)).intValue() < 0)
-                data.set(i, new Integer(1));
+                data.set(i, one);
 
         // make array weights={1,1,1,...}
         int weights[] = new int[WINDOW];
         Arrays.fill(weights, 1);
+	// REFACTOR: if WINDOW is fixed, this should be static
 
         // borrow a high-pass filter
-        List tmp = HighPass.filter(data, weights); // eep, high-pass should return an array!  (and maybe take an array, too!)
+        List tmp = HighPass.filter(data, weights);
+	// eep, high-pass should return an array!  (and maybe take an array, too!)
 
         // natural logarithm -- log(0) is bad news, so log(max(s[i],eps))
         for (int i=0; i<N; i++) {
-            smoothed[i] = ((Number) data.get(i)).doubleValue() / ((Number) tmp.get(i)).doubleValue(); // ratio
-            smoothed[i] = Math.log(Math.max(100.0*smoothed[i], Double.MIN_VALUE));
+            smoothed[i] = ((Number) data.get(i)).floatValue() / ((Number) tmp.get(i)).floatValue(); // ratio
+            smoothed[i] = (float) Math.log(Math.max(100.0f*smoothed[i], Float.MIN_VALUE));
         }
 
         return smoothed;
     }
 
-    /** Compute the mean of an array of doubles.
-	@param array the array of doubles to average
+    /** Compute the mean of an array of floats.
+	@param array the array of floats to average
 	@return the average (arithmetic mean) of the given array */
-    private double mean(double array[]) {
-	// (/ (map '+ array) (length array))
-	double s = 0.0;
-	for (int i=0; i<array.length; i++)
-	    s += array[i];
-	return s / array.length;
+    private float mean(float array[]) {
+        // (/ (reduce '+ array) (length array))
+        float s = 0.0f;
+        for (int i=0; i<array.length; i++)
+            s += array[i];
+        return s / array.length;
+
+	// what if array.length==0? -- only happens if one sample has no data (possible?)
     }
 
     /** Given offsets into the fixed and moving data, compute a single
 	T-score for that position.
 	@return the T-score for this possible cross */
     public double compute(int offsetFixed, int offsetMoving) {
-	int i=offsetFixed, j=offsetMoving;
-	int overlap=0;
+        int i=offsetFixed, j=offsetMoving;
+        int overlap=0;
 
-	double z1 = 0.0;
-	double z2 = 0.0;
-	double z3 = 0.0;
+	float z1 = 0.0f;
+	float z2 = 0.0f;
+        float z3 = 0.0f;
 
-	while (i<fixedData.length && j<movingData.length) {
-	    double xx = fixedData[i] - fixedMean;
-	    double yy = movingData[j] - movingMean;
+        float xx, yy;
 
-	    z1 += xx*xx;
-	    z2 += yy*yy;
-	    z3 += xx*yy;
+        while (i<fixedData.length && j<movingData.length) {
+            xx = fixedData[i] - fixedMean;
+            yy = movingData[j] - movingMean;
 
-	    overlap++;
-	    i++;  j++;
-	}
+	    // NOTE: looking at this today (22.8.2002), i'm not sure this shouldn't be
+	    // the mean of the overlapping sections only (instead of the mean of the
+	    // entire samples), but this appears to be what baillie did, i'm pretty
+	    // sure it's what mecki did, and it gives reasonable results (changing
+	    // which mean it uses probably wouldn't have much effect), so i'm going
+	    // to leave it for now.
 
-	if (z3 < 0.0)
-	    return 0.0;
+            z1 += xx*xx;
+            z2 += yy*yy;
+            z3 += xx*yy;
 
-	double sigx = Math.sqrt(z1/overlap);
-	double sigy = Math.sqrt(z2/overlap);
-	double cal1 = z3/(overlap*sigx*sigy);
-	double cal2 = Math.sqrt(overlap-2);
-	double cal3 = Math.sqrt(1-cal1*cal1);
+            overlap++;
+            i++;  j++;
+        }
 
-	return cal1*cal2/cal3;
+	// fail ... i don't completely understand this, but it's Bad (trust me)
+        if (z3 < 0.0f)
+            return 0.0f;
+
+	// DOCUMENTME: what this computes, and why i'm scratching my right ear with my left hand
+        float sigx = (float) Math.sqrt(z1/overlap);
+        float sigy = (float) Math.sqrt(z2/overlap);
+        float cal1 = z3/(overlap*sigx*sigy);
+        float cal2 = (float) Math.sqrt(overlap-2);
+        float cal3 = (float) Math.sqrt(1-cal1*cal1);
+
+        float t = cal1*cal2/cal3;
+
+        // if overlap=1 or some other silliness, make it zero
+        if (Float.isNaN(t))
+            t = 0.0f;
+
+        return t;
     }
 
-    /** Preamble: copy all data to (mutable) double arrays, normalize
+    /** Preamble: copy all data to (mutable) float arrays, normalize
         the data, and compute means of the series. */
     protected void preamble() {
 	// normalize (while copying to mutable arrays)
@@ -247,6 +291,4 @@ public class TScore extends Cross {
 	fixedMean = mean(fixedData);
 	movingMean = mean(movingData);
     }
-
-    // histo was: 0.00, 0.25, 5.00
 }
