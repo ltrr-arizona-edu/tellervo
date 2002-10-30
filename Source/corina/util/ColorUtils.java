8ced5cc53c0e592e46814f5e7ca7480f7b690f23
no_author <another@cornell.edu>
2002-10-30 21:58:03 +0000

    Added blend(), addAlpha(Color,float), and reallyDark(Color) methods.

--- Source/corina/util/ColorUtils.java
+++ Source/corina/util/ColorUtils.java
@@ -10,8 +10,24 @@ public class ColorUtils {
                          (c1.getBlue() + c2.getBlue()) / 2);
     }
 
+    // blend f1 of c1 and f2 of c2 (f1+f2=1.0)
+    public static Color blend(Color c1, float f1, Color c2, float f2) {
+        return new Color((int) (f1*c1.getRed() + f2*c2.getRed()),
+                         (int) (f1*c1.getGreen() + f2*c2.getGreen()),
+                         (int) (f1*c1.getBlue() + f2*c2.getBlue()));
+    }
+
     // make c transparent
     public static Color addAlpha(Color c, int alpha) {
         return new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
     }
+    public static Color addAlpha(Color c, float f) {
+        return addAlpha(c, (int) Math.round(255*f));
+    }
+
+    public static boolean reallyDark(Color c) {
+        Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), hsb);
+        return (hsb[2] < 0.1f); // pick a value... 10%
+    }
+    private static float hsb[] = new float[3];
 }
