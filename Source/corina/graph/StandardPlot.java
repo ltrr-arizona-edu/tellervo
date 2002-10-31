9139d6dbfb83046cf324ec59616f9a02d468e2e7
no_author <another@cornell.edu>
2002-10-31 14:30:14 +0000

    Refactored by extracting method makeStroke().

--- Source/corina/graph/StandardPlot.java
+++ Source/corina/graph/StandardPlot.java
@@ -45,112 +45,111 @@ public class StandardPlot {
 
     // nb: this gets recreated every from refreshFromPreferences()
     public StandardPlot(Range bounds, JPanel panel) {
-	// read sys props
-	update();
+        // read sys props
+        update();
 
-	// copy parms
-	_bounds = bounds;
-	_panel = panel;
+        // copy parms
+        _bounds = bounds;
+        _panel = panel;
+    }
+
+    private BasicStroke makeStroke(float width, boolean dotted) {
+	if (dotted)
+	    return new BasicStroke(width, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL,
+				   10f, new float[] { 8f }, 0f);
+	else
+	    return new BasicStroke(width, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
     }
 
     public void draw(Graphics2D g2, Graph g, boolean isCurrent, int xscroll) {
-	// set pen
-	g2.setStroke(new BasicStroke(isCurrent ? 2f*_width : _width,
-				     BasicStroke.CAP_BUTT,
-				     BasicStroke.JOIN_BEVEL));
+        // set pen
+        g2.setStroke(makeStroke(isCurrent ? 2f*_width : _width, false));
 
-	// use dotted line for indexes?  (refactor me?)
-	if (_dottedIndexes && (g.graph instanceof Index)) {
-	    g2.setStroke(new BasicStroke(isCurrent ? 2f*_width : _width,
-					 BasicStroke.CAP_BUTT,
-					 BasicStroke.JOIN_BEVEL,
-					 10f,
-					 new float[] { 8f },
-					 0f));
-	}
+        // use dotted line for indexes?  (refactor me?)
+        if (_dottedIndexes && (g.graph instanceof Index)) {
+            g2.setStroke(makeStroke(isCurrent ? 2f*_width : _width, true));
+        }
 
-	// bottom
-	int bottom = GrapherPanel.getBottom(_panel);
+        // bottom
+        int bottom = GrapherPanel.getBottom(_panel);
 
-	// baseline
-	if (_baselines) {
-	    int y = bottom - g.yoffset;
-	    g2.drawLine(xscroll, y, xscroll+10*_yearsize, y); // 1 decade wide -- ok?
-	}
+        // baseline
+        if (_baselines) {
+            int y = bottom - g.yoffset;
+            g2.drawLine(xscroll, y, xscroll+10*_yearsize, y); // 1 decade wide -- ok?
+        }
 
-	// no data?  stop.
-	if (g.graph.getData().isEmpty())
-	    return;
+        // no data?  stop.
+        if (g.graph.getData().isEmpty())
+            return;
 
-	// compute sapwood
-	int sapwoodIndex, sapwoodCount=0, unmeasPost=0;
-	if (g.graph instanceof Sample) {
-	    Sample sample = (Sample) g.graph;
-	    if (sample.isOak()) {
-		try {
-		    if (sample.meta.containsKey("sapwood"))
-			sapwoodCount = ((Integer) sample.meta.get("sapwood")).intValue();
-		    if (sample.meta.containsKey("unmeas_post"))
-			unmeasPost = ((Integer) sample.meta.get("unmeas_post")).intValue();
-		} catch (ClassCastException cce) {
-		    // we've already warned the user above, ignore it now.
-		}
-	    }
-	}
-	sapwoodIndex = g.graph.getData().size() - sapwoodCount + unmeasPost + 1;
+        // compute sapwood
+        int sapwoodIndex, sapwoodCount=0, unmeasPost=0;
+        if (g.graph instanceof Sample) {
+            Sample sample = (Sample) g.graph;
+            if (sample.isOak()) {
+                try {
+                    if (sample.meta.containsKey("sapwood"))
+                        sapwoodCount = ((Integer) sample.meta.get("sapwood")).intValue();
+                    if (sample.meta.containsKey("unmeas_post"))
+                        unmeasPost = ((Integer) sample.meta.get("unmeas_post")).intValue();
+                } catch (ClassCastException cce) {
+                    // we've already warned the user above, ignore it now.
+                }
+            }
+        }
+        sapwoodIndex = g.graph.getData().size() - sapwoodCount + unmeasPost + 1;
 
-	// my path
-	GeneralPath p = new GeneralPath();
+        // my path
+        GeneralPath p = new GeneralPath();
 
-	// x-position
-	int x = _yearsize*(g.graph.getStart().diff(_bounds.getStart()) + g.xoffset);
+        // x-position
+        int x = _yearsize*(g.graph.getStart().diff(_bounds.getStart()) + g.xoffset);
 
-	// move to the first point -- THIS IS NOT REALLY A SPECIAL CASE!
-	int value;
-	try {
-	    value = ((Number) g.graph.getData().get(0)).intValue();
-	} catch (ClassCastException cce) {
-	    value = 0; // BAD!  instead: (1) just continue now, and (2) NEXT point is a move-to.
-	}
-	p.moveTo(x, bottom - (int)(value*g.scale) - g.yoffset);
+        // move to the first point -- THIS IS NOT REALLY A SPECIAL CASE!
+        int value;
+        try {
+            value = ((Number) g.graph.getData().get(0)).intValue();
+        } catch (ClassCastException cce) {
+            value = 0; // BAD!  instead: (1) just continue now, and (2) NEXT point is a move-to.
+        }
+        p.moveTo(x, bottom - (int)(value*g.scale) - g.yoffset);
 
-	// connect the lines through the rest of the graph
-	int n = g.graph.getData().size(); // does this help performance any?
-	for (int i=1; i<n; i++) {
-	    // sapwood?  draw what we've got, and start a new (thicker) path
-	    if (i==sapwoodIndex) {
-		g2.draw(p);
-		g2.setStroke(new BasicStroke(isCurrent ? 4f*_width : 2f*_width,
-					     BasicStroke.CAP_SQUARE,
-					     BasicStroke.JOIN_BEVEL));
-		p = new GeneralPath();
-		p.moveTo(_yearsize*(i-1 + g.graph.getStart().diff(_bounds.getStart()) + g.xoffset),
-			 bottom - (int)(value*g.scale) - g.yoffset);
-	    }
+        // connect the lines through the rest of the graph
+        int n = g.graph.getData().size(); // does this help performance any?
+        for (int i=1; i<n; i++) {
+            // sapwood?  draw what we've got, and start a new (thicker) path
+            if (i==sapwoodIndex) {
+                g2.draw(p);
+                g2.setStroke(makeStroke(isCurrent ? 4f*_width : 2f*_width, false));
+                p = new GeneralPath();
+                p.moveTo(_yearsize*(i-1 + g.graph.getStart().diff(_bounds.getStart()) + g.xoffset),
+                         bottom - (int)(value*g.scale) - g.yoffset);
+            }
 
-	    // compute this value
-	    try {
-		value = ((Number) g.graph.getData().get(i)).intValue();
-	    } catch (ClassCastException cce) {
-		value = 0; // e.g., if it's being edited, it's still a string
-		// BAD!  instead: (1) draw what i've got so far, and (2) NEXT point is a move-to.
-		// -- try to parse String as an integer?
-	    }
+            // compute this value
+            try {
+                value = ((Number) g.graph.getData().get(i)).intValue();
+            } catch (ClassCastException cce) {
+                value = 0; // e.g., if it's being edited, it's still a string
+                           // BAD!  instead: (1) draw what i've got so far, and (2) NEXT point is a move-to.
+                           // -- try to parse String as an integer?
+            }
 
-	    // (x,y) position
-	    x += _yearsize;
-	    int y = bottom - (int)(value*g.scale) - g.yoffset;
+            // (x,y) position
+            x += _yearsize;
+            int y = bottom - (int)(value*g.scale) - g.yoffset;
 
-	    // if MR, draw a vertical line -- use (int) Sample.MR, for now
-	    if (value <= Sample.MR && g.graph instanceof Sample)
-		g2.drawLine(x, y-20, x, y+20);
+            // if MR, draw a vertical line -- use (int) Sample.MR, for now
+            if (value <= Sample.MR && g.graph instanceof Sample)
+                g2.drawLine(x, y-20, x, y+20);
 
-	    // move to next point
-	    p.lineTo(x, y);
-	}
+            // move to next point
+            p.lineTo(x, y);
+        }
 
-	// draw it!
-	g2.draw(p);
+        // draw it!
+        g2.draw(p);
     }
 
     public boolean contact(Graph gg, Point p) {
@@ -196,10 +195,10 @@ public class StandardPlot {
     }
 
     public void update() {
-	// read sys props
-	_baselines = Boolean.getBoolean("corina.graph.baselines");
-	_yearsize = Integer.parseInt(System.getProperty("corina.graph.pixelsperyear", "10"));
-	_width = Float.parseFloat(System.getProperty("corina.graph.thickness", "1.0"));
-	_dottedIndexes = Boolean.getBoolean("corina.graph.dotindexes");
+        // read sys props
+        _baselines = Boolean.getBoolean("corina.graph.baselines");
+        _yearsize = Integer.parseInt(System.getProperty("corina.graph.pixelsperyear", "10"));
+        _width = Float.parseFloat(System.getProperty("corina.graph.thickness", "1.0"));
+        _dottedIndexes = Boolean.getBoolean("corina.graph.dotindexes");
     }
 }
