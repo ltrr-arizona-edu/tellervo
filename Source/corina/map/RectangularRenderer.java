6063cfe98932278c1e60117dac5ccef310efabc2
no_author <another@cornell.edu>
2002-11-03 21:47:22 +0000

    Converted to new Renderer interface.

--- Source/corina/map/RectangularRenderer.java
+++ Source/corina/map/RectangularRenderer.java
@@ -7,21 +7,21 @@ public class RectangularRenderer extends Renderer {
         this.view = v;
     }
 
+    // (normal zoom doesn't look quite the same on spherical and rectangular, so adjust it)
     private static final int EXTRA_ZOOM = 16;
 
-    public Vector3 project(Location loc) {
-        Vector3 v = new Vector3();
-        v.x = view.size.width/2 + (loc.longitude - view.center.longitude)*view.zoom*EXTRA_ZOOM;
-        v.y = view.size.height/2 - (loc.latitude - view.center.latitude)*view.zoom*EXTRA_ZOOM;
-        v.z = +1; // it's a flat map, everything's theoretically visible. -- THIS IS ANOTHER REASON WHY VECTOR3 IS WRONG
-        return v;
+    public void render(Location loc, Vector3 pt) {
+        float zoom = view.zoom * EXTRA_ZOOM;
+
+        pt.x = view.size.width/2 + (loc.longitude - view.center.longitude)*zoom;
+        pt.y = view.size.height/2 - (loc.latitude - view.center.latitude)*zoom;
+        pt.z = +1; // it's a flat map, everything's theoretically visible. -- THIS IS ANOTHER REASON WHY VECTOR3 IS WRONG
     }
 
-    // just the opposite of project()
-    public Location unrender(Point p) {
-        Location l = new Location();
-        l.longitude = view.center.longitude + (p.x - view.size.width/2)/(view.zoom*EXTRA_ZOOM);
-        l.latitude = view.center.latitude + (p.y - view.size.height/2)/(-view.zoom*EXTRA_ZOOM);
-        return l;
+    // unproject pt, store result in loc
+    public void unrender(Point pt, Location loc) {
+        // FIXME: use a set method!
+        loc.longitude = view.center.longitude + (pt.x - view.size.width/2)/(view.zoom*EXTRA_ZOOM);
+        loc.latitude = view.center.latitude + (pt.y - view.size.height/2)/(-view.zoom*EXTRA_ZOOM);
     }
 }
