0eb2dd655a4c0c1ccd659eac5526e0d3e69f2d1a
no_author <another@cornell.edu>
2002-11-03 21:18:36 +0000

    Named methods to the incredibly intuitive render() and unrender(). Plus they now operate on data in-line, to save a whole boatload of allocations and garbage collections.
    Added method pixelsForDistanceAtPoint(), to make drawing the legend
    very easy.

--- Source/corina/map/Renderer.java
+++ Source/corina/map/Renderer.java
@@ -4,13 +4,19 @@ import java.awt.Point;
 
 // a renderer interface for points on a map.
 public abstract class Renderer {
-    // factory
-    public static Renderer makeRenderer(View view) {
+    // factory -- TODO: let me pick which renderer at runtime!
+    public static Renderer createRenderer(View view) {
         return new RectangularRenderer(view);
     }
+    // PERF: rectrend is really fast, but sphericalrenderer needs to
+    // do some matrix multiplications; caching renderers (by their
+    // views) would definitely be adventageous, then -- as
+    // getRenderer() perhaps, to indicate that it's not really
+    // creating a new one every time.
 
-    public abstract Vector3 project(Location loc); // FIXME: returns a Point, not a Vector3, or null if it's not visible (?)
-    public abstract Location unrender(Point p);
+    public abstract void render(Location loc, Vector3 pt); // project loc, store result in pt
+    public abstract void unrender(Point pt, Location loc); // unproject pt, store result in loc
+
     public boolean isVisible(Vector3 p1, Vector3 p2) {
         // z-clipping: THIS DOESNT EVEN MAKE SENSE IN RECTANGULAR PROJECTION!
         if (p1.z < 0 || p2.z < 0)
@@ -26,10 +32,24 @@ public abstract class Renderer {
         return true;
     }
 
-    public View view;
+    protected View view;
 
     // nonzero iff the current segment is visible.
     public static final int VISIBLE_NO = 0; // out of view
     public static final int VISIBLE_YES = 1; // in view
     public static final int VISIBLE_POINT = 2; // in view but only 1 pixel
+
+    // compute the number of pixels |km| is at |p|, for drawing a scale/legend
+    private Location leftLoc = new Location(), rightLoc = new Location();
+    private Vector3 rightVec = new Vector3();
+    public int pixelsForDistanceAtPoint(Point p, float km) {
+        unrender(p, leftLoc);
+        double radius = 40000. * Math.cos(Math.toRadians(leftLoc.latitude)); // radius of circle at latitude
+        double deg = (km / radius) * 360.; // deg for |km|
+        deg = Math.abs(deg); // (just in case)
+        // rightLoc = new Location(leftLoc.latitude, leftLoc.longitude + (float) deg); // PERF: new!
+	rightLoc.latitude = leftLoc.latitude;
+	rightLoc.longitude = leftLoc.longitude + (float) deg;
+        render(rightLoc, rightVec);
+        return (int) (rightVec.x - p.x); }
 }
