b3a827e2af0e2ac7c892e2ed8ee7b715112ae5a2
no_author <another@cornell.edu>
2002-11-19 04:17:05 +0000

    Fixed tests so they pass in all locales.

--- Source/corina/ui/UnitTests.java
+++ Source/corina/ui/UnitTests.java
@@ -4,6 +4,12 @@ import corina.util.Platform;
 
 import junit.framework.TestCase;
 
+/*
+  yup, this looks weird.  it's fairly generic stuff, so i can change
+  the translation files (or build in a different locale) without my
+  tests failing.
+ */
+
 public class UnitTests extends TestCase {
     public UnitTests(String name) {
         super(name);
@@ -13,15 +19,33 @@ public class UnitTests extends TestCase {
     // testing I18n
     //
     public void testGetText() {
-	assertEquals(I18n.getText("edit"), "Edit");
+	String edit = I18n.getText("edit");
+
+	// make sure it's a word
+	assertTrue(edit.length() > 0);
+	assertTrue(Character.isLetter(edit.charAt(0)));
+	assertTrue(Character.isUpperCase(edit.charAt(0)));
+
+	// make sure there's no []&
+	assertEquals(-1, edit.indexOf('['));
+	assertEquals(-1, edit.indexOf(']'));
+	assertEquals(-1, edit.indexOf('&'));
     }
 
     public void testGetKeyStroke() {
+	// "file" should not have a keystroke
 	assertEquals(I18n.getKeyStroke("file"), null);
-	assertEquals(I18n.getKeyStroke("save"), (Platform.isMac ? "meta S" : "control S"));
+
+	// "save" should, and it should be "<accel> <key>"
+	String save = I18n.getKeyStroke("save");
+	assertTrue(save != null);
+	assertTrue(save.indexOf(' ') != -1);
     }
 
     public void testGetMnemonic() {
-	assertEquals(I18n.getMnemonic("edit").charValue(), 'E');
+	// edit should have a mnemonic, and it should be upper-case
+	Character e = I18n.getMnemonic("edit");
+	assertTrue(e != null);
+	assertTrue(Character.isUpperCase(e.charValue()));
     }
 }
