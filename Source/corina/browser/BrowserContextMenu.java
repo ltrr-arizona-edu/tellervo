9f074a86353dc781570c310b581e58eedc8f12c6
no_author <another@cornell.edu>
2003-06-19 07:00:25 +0000

    Adding BrowserContextMenu.

---
+++ Source/corina/browser/BrowserContextMenu.java
@@ -0,0 +1,236 @@
+//
+// This file is part of Corina.
+//
+// Corina is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Corina is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Corina; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Copyright 2003 Ken Harris <kbh7@cornell.edu>
+//
+
+package corina.browser;
+
+import corina.Sample;
+import corina.editor.Editor;
+import corina.cross.CrossdateKit;
+import corina.cross.CrossdateWindow;
+import corina.cross.Sequence;
+import corina.graph.GraphWindow;
+import corina.graph.BargraphFrame;
+import corina.gui.TextWindow;
+import corina.gui.Bug;
+import corina.ui.Builder;
+import corina.util.PopupListener;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JPopupMenu;
+import javax.swing.JMenuItem;
+import javax.swing.AbstractAction;
+
+/**
+   A context (right-click) menu for browser entries.
+
+   <p>The entries are:</p>
+   <ul>
+     <li>Open
+     <li>Graph
+     <li>Bargraph</li>
+     <br>
+     <li>Make Sum</li>
+     <br>
+     <li>View Source</li>
+     <br>
+     <li>Crossdate
+     <li>&nbsp;&nbsp;&nbsp;&nbsp;1-by-1
+     <li>&nbsp;&nbsp;&nbsp;&nbsp;1-by-N
+     <li>&nbsp;&nbsp;&nbsp;&nbsp;N-by-1
+     <li>&nbsp;&nbsp;&nbsp;&nbsp;N-by-N
+   </ul>
+
+   <h2>Left to do:</h2>
+   <ul>
+     <li>Use different methods for accessing browser data?
+         (browser.table, browser.getSelectedFilenames(), browser.getSelectedElements())
+     <li>Better error handling
+     <li>Add "Move to Trash"?
+     <li>Add "Get Info"?
+   </ul>
+
+   @author Ken Harris &lt;kbh7 <i style="color: gray">at</i> cornell <i style="color: gray">dot</i> edu&gt;
+   @version $Id$
+*/
+public class BrowserContextMenu extends JPopupMenu {
+
+    private JMenuItem open, graph, bargraph;
+    private JMenuItem makeSum;
+    private JMenuItem viewSource;
+    private JMenuItem A, B, C, D; // the 4 crossdating sequences
+
+    private Browser browser;
+
+    /**
+        Make a new browser context menu.  (This attaches a listener to the browser's
+        JTable, too.)
+        
+        @param browser the Browser to add this context menu to.
+    */
+    public BrowserContextMenu(Browser browser) {
+	this.browser = browser;
+
+	initMenuItems();
+
+	browser.table.addMouseListener(new PopupListener(this) {
+		public void showPopup(MouseEvent e) {
+		    // enable/disable the menuitems on the popup, as necessary
+		    enableDisable();
+
+		    // select this row, if necessary, and show the popup
+		    super.showPopup(e);
+		}
+	    });
+    }
+
+    private void enableDisable() {
+	int n = browser.table.getSelectedRows().length;
+	open.setEnabled(n == 1);
+	graph.setEnabled(n >= 1);
+	bargraph.setEnabled(n >= 1);
+	makeSum.setEnabled(n >= 2);
+	viewSource.setEnabled(n == 1);
+	A.setEnabled(n == 2);
+	B.setEnabled(n >= 1);
+	C.setEnabled(n >= 1);
+	D.setEnabled(n >= 2);
+    }
+
+    private void initMenuItems() {
+	open = Builder.makeMenuItem("open");
+	open.addActionListener(new AbstractAction() {
+		public void actionPerformed(ActionEvent e) {
+		    try {
+                        String filename = browser.getSelectedRow().getPath();
+			new Editor(new Sample(filename));
+		    } catch (IOException ioe) {
+			// TODO: display USER error message
+			Bug.bug(ioe);
+		    }
+		}
+	    });
+
+	graph = Builder.makeMenuItem("graph");
+	graph.addActionListener(new AbstractAction() {
+		public void actionPerformed(ActionEvent e) {
+		    List list = browser.getSelectedElements();
+		    new GraphWindow(list);
+		}
+	    });
+	bargraph = Builder.makeMenuItem("bargraph");
+	bargraph.addActionListener(new AbstractAction() {
+		public void actionPerformed(ActionEvent e) {
+		    List list = browser.getSelectedElements();
+		    // here, i re-use elements, which makes this really fast
+		    new BargraphFrame(list);
+		}
+	    });
+
+	makeSum = Builder.makeMenuItem("make_sum...");
+	makeSum.addActionListener(new AbstractAction() {
+		public void actionPerformed(ActionEvent e) {
+		    browser.makeSumFromSelection();
+		}
+	    });
+
+	viewSource = Builder.makeMenuItem("view_source");
+	// Builder.addAction(source, "corina.browser.Browser.viewSource()"); // ???
+	viewSource.addActionListener(new AbstractAction() {
+		public void actionPerformed(ActionEvent e) {
+		    try {
+                        String filename = browser.getSelectedRow().getPath();
+			new TextWindow(filename);
+		    } catch (IOException ioe) {
+			// TODO: display USER error message
+			Bug.bug(ioe);
+		    }
+		}
+	    });
+
+	add(open);
+	add(graph);
+	add(bargraph);
+	addSeparator();
+	add(makeSum);
+	addSeparator();
+	// add(Builder.makeMenuItem("get_info...", false)); // WRITEME -- how?
+	add(viewSource);
+	addSeparator();
+	{
+	    add(Builder.makeMenuItem("crossdate", false));
+	    // submenu? -- REFACTOR: need abstraction for that!
+	    A = new JMenuItem(Builder.INDENT + "1-by-1");
+	    B = new JMenuItem(Builder.INDENT + "1-by-N");
+	    C = new JMenuItem(Builder.INDENT + "N-by-1");
+	    D = new JMenuItem(Builder.INDENT + "N-by-N");
+
+	    A.addActionListener(new AbstractAction() {
+		    public void actionPerformed(ActionEvent e) {
+			List files = browser.getSelectedFilenames();
+
+			List fixed = Collections.singletonList(files.get(0));
+			List moving = Collections.singletonList(files.get(1));
+			new CrossdateWindow(new Sequence(fixed, moving));
+		    }
+		});
+
+	    B.addActionListener(new AbstractAction() {
+		    public void actionPerformed(ActionEvent e) {
+			List files = browser.getSelectedFilenames();
+			new CrossdateKit(new Sequence(Collections.EMPTY_LIST, files));
+			// REFACTOR: should crossdatekit take lists, too?
+			// RENAME: "CrossdateKit" isn't terribly descriptive ... "EditLists?" need something better
+		    }
+		});
+
+	    C.addActionListener(new AbstractAction() {
+		    public void actionPerformed(ActionEvent e) {
+			List files = browser.getSelectedFilenames();
+			new CrossdateKit(new Sequence(files, Collections.EMPTY_LIST));
+			// REFACTOR: should crossdatekit take lists, too?
+		    }
+		});
+
+	    D.addActionListener(new AbstractAction() {
+		    public void actionPerformed(ActionEvent e) {
+			List files = browser.getSelectedFilenames();
+			new CrossdateWindow(new Sequence(files, files));
+		    }
+		});
+
+	    add(A);
+	    add(B);
+	    add(C);
+	    add(D);
+	}
+	// WRITEME:
+	// addSeparator();
+	// add(Builder.makeMenuItem("move_to_trash", false));
+    }
+}
