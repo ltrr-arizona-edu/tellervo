9f2ac6e3eb1a93bbaecf4188f3e9deb6725856c0
no_author <another@cornell.edu>
2003-06-20 14:24:19 +0000

    Updated some documentation.  Added union(), intersection(), etc.

--- Source/corina/Range.java
+++ Source/corina/Range.java
@@ -21,22 +21,23 @@
 package corina;
 
 /**
-   This class represents a range of years which can be redated safely.
-   It's immutable; all destructive operations on it return new Ranges.
+   A range of years.  Ranges are immutable; all otherwise-destructive
+   operations on a Range return a new Range.
 
-   Unfortunately, this data structure violates single-instance
-   storage: we hope that the usage of Range and Sample will always
-   keep <code>Sample.data.size() == Range.span()</code>, but there
+   <p>Unfortunately, use of this data structure often violates
+   the single-instance storage principle: we hope that the usage
+   of Range and Sample will always keep
+   <code>Sample.data.size() == Range.span()</code>, but there
    aren't any built-in ways to do this, so it's up to you.  The
-   problem is that <code>end</code> is a duplicate of
-   <code>Sample.data.size() + start</code>.
+   problem is that the <code>end</code> field is a duplicate of
+   <code>Sample.data.size() + start</code>.</p>
 
    @see Year
    @see Sample
 
-   @author <a href="mailto:kbh7@cornell.edu">Ken Harris</a>
-   @version $Id$ */
-
+   @author Ken Harris &lt;kbh7 <i style="color: gray">at</i> cornell <i style="color: gray">dot</i> edu&gt;
+   @version $Id$
+*/
 public class Range implements Comparable {
 
     /** Starting year of the Range. */
@@ -49,13 +50,14 @@ public class Range implements Comparable {
 	@see Year */
     public Range() {
 	// this is only used by GraphFrame (which shouldn't use it)
-	// and Sample
+	// -- and Sample
 	start = Year.DEFAULT;
 	end = start.add(-1);
     }
 
     /** Construct a new range, from y<sub>1</sub> to y<sub>2</sub>.
-	(Neither year may be <code>null</code>.)
+	(Neither year may be <code>null</code>.)  If y<sub>2</sub> &lt; y<sub>1</sub>,
+	it is an empty interval.
 	@param y1 starting year
 	@param y2 ending year */
     public Range(Year y1, Year y2) {
@@ -73,7 +75,10 @@ public class Range implements Comparable {
 	}
     }
 
-    // start at |y|, length of |span| years, inclusive
+    /** Construct a range, given a starting year and span.
+       @param y the starting year
+       @param span the number of years
+    */
     public Range(Year y, int span) {
 	this.start = y;
 	this.end = y.add(span-1);
@@ -123,6 +128,10 @@ public class Range implements Comparable {
 	return redateBy(y.diff(start));
     }
 
+    /** Redate a range by a certain number of years.
+	Usually, you'll use redateStartTo() or redateEndTo(), which
+	are more convenient.
+	@param dy the number of years to shift this range by */
     public Range redateBy(int dy) {
 	return new Range(start.add(dy), end.add(dy));
     }
@@ -143,16 +152,36 @@ public class Range implements Comparable {
 	return end.diff(start) + 1;
     }
 
-    /** Return a simple string representation of the range,
-        like "1001 - 1036".
-        @return a string representation of the range */
-    public String toString() {
-        return start + " - " + end; // use \u2014 EM DASH?
+    /** Compute the number of rows this Range will take to display, assuming
+	rows are marked off as the row() method does.
+	@return the number of rows this range spans */
+    public int rows() {
+	return getEnd().row() - getStart().row() + 1;
     }
 
-    // "(1001 - 1036, n=36)"
+    /**
+       Return a simple string representation of the range,
+       like "1001 - 1036".
+
+       @return a string representation of the range
+    */
+    public String toString() {
+        // this tends to get called a lot, so we'll memoize it.
+	if (memo == null)
+	    memo = start + " - " + end;  // use \u2014 EM DASH?
+        return memo;
+    }
+    private String memo = null;
+
+    /**
+       Return a string representation of the range, including
+       the span, like "(1001 - 1036, n=36)".
+
+       @return a string representation of the range, including span
+    */
     public String toStringWithSpan() {
-        return "(" + start + " - " + end + ", n=" + span() + ")"; // use \u2014 EM DASH?
+        return "(" + start + " - " + end + ", n=" + span() + ")";
+	// use \u2014 EM DASH?
     }
 
     /** Return true if (and only if) the given year is inside the range, inclusive.
@@ -162,31 +191,54 @@ public class Range implements Comparable {
 	return (start.compareTo(y) <= 0) && (y.compareTo(end) <= 0);
     }
 
-    // is start of row?
+    /** Return true if (and only if) the given range is completely inside the range, inclusive.
+	@param r range to check 
+	@return true if <code>r</code> is entirely in the range, else false */
+    public boolean contains(Range r) {
+	return contains(r.start) && contains(r.end);
+    }
+
+    /** Return true, iff this year is the start of a row.  (Year 1 is
+	considered the start of that row.)
+	@return true, iff this year is the start of a row */
     public boolean startOfRow(Year y) {
 	return y.equals(start) || y.column()==0 || y.isYearOne();
     }
 
-    // is end of row?
+    /** Return true, iff this year is the end of a row.
+	@return true, iff this year is the end of a row */
     public boolean endOfRow(Year y) {
 	return y.equals(end) || y.column()==9;
     }
 
     /** Return the number of years overlap between this range and the
 	given range.
+
 	@param r range to compare
 	@return number of years overlap */
     public int overlap(Range r) {
 	return intersection(r).span();
     }
 
-    // the intersection of this and |r|
+    /** The intersection of this range with r.  If they don't overlap,
+	returns an empty range (1 - -1).
+
+        @see #union
+        
+	@param r the range to intersect with this range
+	@return the intersection of this and r */
     public Range intersection(Range r) {
 	return new Range(Year.max(start, r.start),
 			 Year.min(end, r.end));
     }
 
-    // the union of this and |r|
+    /** The union of this range with r.  Since there is no concept of
+	"range with a gap" in Corina, it assumes they overlap.
+
+        @see #intersection
+        
+	@param r the range to union with this range
+	@return the union of this and r */
     public Range union(Range r) {
 	return new Range(Year.min(start, r.start),
 			 Year.max(end, r.end));
@@ -196,9 +248,23 @@ public class Range implements Comparable {
 	@param r range to compare with this
 	@return true, if the ranges are equal, else false */
     public boolean equals(Object o) {
-        // o instanceof Range first?
-	Range r = (Range) o;
-	return start.equals(r.start) && end.equals(r.end);
+	if (o instanceof Range) {
+	    Range r = (Range) o;
+	    return start.equals(r.start) && end.equals(r.end);
+	} else {
+	    // not even a Range, can't be equal
+	    return false;
+	}
+    }
+
+    /**
+       A hash code for the Range.  (Since I define equals(),
+       I need to define hashCode().)
+        
+       @return a hash code for this Range
+    */
+    public int hashCode() {
+	return start.hashCode() + 2*end.hashCode();
     }
 
     /** Compares this and o, for placing in fallback order.
@@ -208,12 +274,12 @@ public class Range implements Comparable {
     public int compareTo(Object o) {
 	Range r2 = (Range) o;
 
-	// since we know years are monotonically increasing -- the
-	// only weirdness is the skip at 0 -- this can be done with a
-	// ?:-statement and save a method call or two.
-
 	int c1 = end.compareTo(r2.end);
+	if (c1 != 0)
+	    return c1;
 
-	return (c1 != 0 ? c1 : start.compareTo(r2.start));
+	// negative, because fallback puts longest samples first
+	int c2 = -start.compareTo(r2.start);
+	return c2;
     }
 }
