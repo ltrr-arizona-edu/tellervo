e1e31b10cefc7941113568574c239379e7b81c5b
no_author <another@cornell.edu>
2004-01-18 18:03:46 +0000

    added hide button

--- Source/corina/gui/SamplePreview.java
+++ Source/corina/gui/SamplePreview.java
@@ -1,16 +1,16 @@
 //
 // This file is part of Corina.
-// 
+//
 // Corina is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // Corina is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Corina; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -20,9 +20,10 @@
 
 package corina.gui;
 
+import corina.Preview;
 import corina.Previewable;
 import corina.Sample;
-import corina.files.WrongFiletypeException;
+import corina.formats.WrongFiletypeException;
 import corina.cross.Grid;
 
 import java.io.File;
@@ -32,15 +33,82 @@ import java.io.IOException;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
-public class SamplePreview extends JLabel implements PropertyChangeListener {
+public class SamplePreview extends JPanel implements PropertyChangeListener {
+    //private PreviewComponent preview;
+    private JScrollPane sp;
 
     public SamplePreview(JFileChooser fc) {
-	setVerticalAlignment(TOP);
-        setPreferredSize(new Dimension(240, 200));
+        setLayout(new GridBagLayout());
+        //setPreferredSize(new Dimension(240, 200));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        //label.setContentType("text/html");
+        //label.setEditable(false);
+        // --- using preview component now ---- label.setVerticalAlignment(SwingConstants.TOP);
+
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.insets = new java.awt.Insets(0,0,2,0);
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.fill = GridBagConstraints.BOTH;
+
+        System.out.println(getInsets());
+        setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(0,2,0,0),
+                                                     BorderFactory.createTitledBorder("Preview")));
+        //setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED ));
+        //label.setContentType("text/html");
+        //label.setEditable(false);
+        //label.setBorder(BorderFactory.createTitledBorder("label"));
+        // --- using preview component now --- label.setOpaque(false);
+
+        sp = new JScrollPane() {
+          public Dimension getPreferredSize () {
+            Dimension pref = super.getPreferredSize();
+            Dimension max = super.getMaximumSize();
+            if (pref.height > max.height) pref.height = max.height;
+            if (pref.width > max.width) pref.width = max.width;
+            return pref;
+          };
+        };
+        sp.setBorder(BorderFactory.createEmptyBorder());
+        sp.setMaximumSize(new Dimension(200, 200));
+
+        add(sp, gbc);
+
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.gridy = 1;
+        //gbc.insets = new java.awt.Insets(0,2,0,2);
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.NONE;
+
+        JButton b = new JButton("Hide >>");
+
+        add(b, gbc);
+
+        b.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent ae) {
+            setVisible(false);
+          }
+        });
+
+
         fc.addPropertyChangeListener(this);
+        setVisible(false);
     }
 
     // PropertyChangeListener helper
@@ -49,55 +117,98 @@ public class SamplePreview extends JLabel implements PropertyChangeListener {
         if (file == null)
             return;
  
+	// i would check for file.isDirectory() here and display a
+	// blank preview component if it's a folder, but apparently
+	// JFileChooser doesn't fire a propertyChangeEvent (or event
+	// of any sort) if a folder is selected.  why?  i have no
+	// idea.
+
 	try {
 	    Previewable s=null;
 
 	    // new: loop to find a Previewable
 	    try {
+        System.out.println("creating grid");
+        // XXX: aaron - well, Grid constructor likes to hang on
+        // invalid files apparently... not sure what causes it
+        // but the parse method never returns
 		s = new Grid(file.getPath());
+    System.out.println("done creating grid");
 	    } catch (WrongFiletypeException wfte) {
+        wfte.printStackTrace();
 		s = new Sample(file.getPath());
 	    } // but can't string catches here ... darn
 
-	    // old: Sample s = new Sample(file.getPath());
+	    // get preview, and show it.
+	    showPreview(s.getPreview());
 
-	    // JIT-compiling this (HTML JLabel) is *slow*!  => thread it, but how?
-	    final String glue = s.getHTMLPreview();
-	    (new Thread() {
-		    public void run() {
-			setText(glue);
-		    }
-		}).start();
+	    // TODO: if title is too long, wrap?  use tooltip?  (jlinedlabel?)
 
 	} catch (WrongFiletypeException wfte) {
-	    final int size = 256;
-	    char buffer[] = new char[size];
-	    int len;
-	    try {
-		FileReader fr = new FileReader(file);
-		len = fr.read(buffer);
-	    } catch (IOException ioe) {
-		setText("<html><b>Unreadable file</b>");
-		return;
-	    }
-	    // the next line won't always work.  fixme.
-	    setText("<html><b>Unreadable file:</b><pre>" + new String(buffer, 0, len) +
-		    (len==size ? "\n...</pre>" : "</pre>")); // no "..." if that's all there is
-	    // apparently an empty file has len=-1 (!!!)
+    wfte.printStackTrace();
+	    showPreview(new Preview.NotDendroDataPreview());
 	} catch (IOException ioe) {
-	    setText("<html><b>Error loading:</b><br>" + ioe.getMessage());
+    ioe.printStackTrace();
+	    showPreview(new Preview.ErrorLoadingPreview(ioe));
 	}
+    }
+
+    private void showPreview(Preview p) {
+	// remove old value, and put new preview component up.
+	/* aaron if (getComponentCount() > 0)
+	    remove(0);*/
+      sp.getViewport().setView(new PreviewComponent(p)); 
+	//add(pc, BorderLayout.CENTER);
+
+	// beat swing with a stick until it repaints me.
+	/* aaron invalidate();
+	validate();*/
     }
 
     // implements PropertyChangeListener
     public void propertyChange(PropertyChangeEvent e) {
         String prop = e.getPropertyName();
+        System.out.println("PROPERTY NAME: " + e.getPropertyName());
+        System.out.println("OLD VALUE: " + e.getOldValue());
+        System.out.println("NEW VALUE: " + e.getNewValue());
         if (prop.equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)) {
-            if (isShowing()) {
-                loadSample((File) e.getNewValue());
-                repaint();
+            if (e.getNewValue() != null) {
+              System.out.println("Setting visible");
+              setVisible(true);
+              //if (isShowing()) {
+                System.out.println("Setting empty size");
+                  setSize(new Dimension(0, 0));
+                  System.out.println("loading sample");
+                  loadSample((File) e.getNewValue());
+
+                  //sp.getViewport().setView(label);
+                   /*label.setSize(0,0);
+                  sp.setSize(0, 0);
+                  setSize(0,0);
+                  setVisible(false);
+
+                  label.revalidate();
+                  sp.revalidate();
+                  /*label.repaint();
+                  sp.repaint();*/
+                  /*label.invalidate();
+                  sp.invalidate();
+                  invalidate();
+                  ((JComponent) getParent()).invalidate();
+                  ((JComponent) getParent()).validate();
+
+                  //setVisible(true);*/
+                  System.out.println("revalidating");
+                  revalidate(); // must check if it had to be resized
+                  
+                  //System.out.println("LABEL: " + label.getSize() + " " + label.getPreferredSize() + " " + label.getMaximumSize());
+                  System.out.println("SP: " + sp.getSize() + " " + sp.getPreferredSize() + " " + sp.getMaximumSize());
+                  System.out.println("this: " + getSize() + " " + getPreferredSize() + " " + getMaximumSize());
+              //}
+            } else {
+              System.out.println("HIDING");
+              setVisible(false);
             }
         }
     }
-
 }
---
+++ Source/corina/io/Files.java
@@ -0,0 +1,170 @@
+package corina.io;
+
+import corina.Sample;
+import corina.formats.Filetype;
+import corina.formats.WrongFiletypeException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.lang.reflect.Constructor;
+
+// wrapper for corina.formats: load() and save() a file, given only its filename
+public class Files {
+    // don't instantiate me
+    private Files() { }
+
+    // load a file
+    public static Sample load(String filename) throws IOException {
+        // don't try a bunch of formats that we know a priori will all
+        // fail: try some quick tests first
+        File f = new File(filename);
+	if (!f.exists())
+            throw new FileNotFoundException("File doesn't exist.");
+        if (f.isDirectory())
+            throw new IOException("Can't load a folder.");
+        if (!f.isFile())
+            throw new IOException("Not a file.");
+        if (!f.canRead())
+            throw new IOException("No read access allowed.");
+
+        // i can ignore a lot of common filetypes without even
+        // looking -- see SKIP_EXTENSIONS
+        for (int i=0; i<SKIP_EXTENSIONS.length; i++)
+            if (filename.toUpperCase().endsWith(SKIP_EXTENSIONS[i]))
+                throw new WrongFiletypeException();
+
+	// make a buffered reader for it
+	BufferedReader br = new BufferedReader(new FileReader(filename));
+
+        // try each loader in turn
+        for (int i=0; i<LOADERS.length; i++) {
+            try {
+                // use factory to make a Filetype from the class name
+                Filetype format = makeFileFormat(LOADERS[i]);
+
+		// if somebody closed the stream, any call to read(), ready(), mark(), or reset()
+		// will throw an IOE.  so i'll try a ready() call to see if it's been closed.
+		// (any SAX-based reader will close the stream after trying to load it,
+		// whether it succeeded or not -- but note that a file format can check the first
+		// line for "<?xml", and fail immediately if it doesn't look like an XML file,
+		// so this doesn't even happen whenever an XML-based format fails.)
+		// this way i'll only have to re-open the stream if needed;
+		// not the most efficient way, sure, but not bad, in the most common case.
+		try {
+		    br.ready();
+		} catch (IOException ioe) {
+		    // somebody closed my stream, re-open it
+		    br = new BufferedReader(new FileReader(filename));
+		}
+
+		// TODO: if the mark can't be reset, for some reason, the stream will
+		// need to be re-opened, also.  (but it would be better to simply
+		// snarf it up into memory, and parse it there.)
+
+                // try loading
+                br.mark(80*5); // is 5 lines enough?
+                Sample s = format.load(br);
+		br.close();
+                String filetype = format.toString();
+                s.meta.put("filetype", filetype); // (used only for preview)
+		s.meta.put("filename", filename);
+
+                // if we made it this far without throwing a
+                // WrongFiletypeException or IOException (or any other
+                // Exception), it must have loaded correctly.
+		return s;
+	    } catch (IllegalArgumentException iae) {
+		// can't create loader -- ??? (bug, probably)
+		System.out.println("bug: " + iae);
+            } catch (WrongFiletypeException wfe) {
+                continue;
+            } catch (IOException ioe) {
+                String l = LOADERS[i];
+                l = l.substring(l.lastIndexOf('.') + 1);
+                throw new IOException(l + ": " + ioe.getMessage());
+            } catch (Exception e) {
+                // load() failed -- unknown reason -- log me?  (once,
+                // some loaders threw crazy things like
+                // NullPointerExceptions, so this is a catch-all for
+                // those.  it should be unnecessary now, but it can't
+                // hurt to have too much error-checking.)
+
+                // use Bug to report it if this ever happens!
+                continue;
+            } finally {
+                try {
+                    br.reset();
+                } catch (IOException ioe) {
+                    // it's ok, stream is closed, but we have the sample.
+                }
+            }
+	}
+
+	// WRITEME
+	throw new WrongFiletypeException();
+    }
+
+    // make a file format loader, given its name (e.g., "TwoColumn")
+    private static Filetype makeFileFormat(String name) throws IllegalArgumentException {
+	try {
+	    Constructor cons =  Class.forName(name).getConstructor(new Class[] {});
+	    return (Filetype) cons.newInstance(new Object[] {});
+	} catch (Exception e) { // class not found, no such method, instantiation exceptions
+	    throw new IllegalArgumentException();
+	}
+    }
+
+    /**
+       Default Filetype class to use for saving files.  Value is "corina.formats.Corina".
+
+       @see corina.formats.Corina
+    */
+    private final static String DEFAULT_SAVER = "corina.formats.Corina";
+    // FIXME: make pref: "default save format: corina, tucson, ..., whatever it was before.
+
+    // save a file
+    public static void save(Sample s, String filename) throws IOException {
+	Filetype format = makeFileFormat(DEFAULT_SAVER);
+	BufferedWriter w = new BufferedWriter(new FileWriter(filename));
+	format.save(s, w);
+	w.close();
+    }
+
+    /** Filename extensions of files to ignore out of principle: .XLS,
+        .DOC, .JPG, .GIF, and .TIF are a good start.  They
+        should be all upper case. */
+    private final static String SKIP_EXTENSIONS[] = {
+	".XLS", ".SLK",
+	".DOC", ".PPT",
+	".RTF",
+	".EXE",
+	".ZIP", // auto-(de)-compression?  no, not enough benefit.
+	".JPG", ".JPEG",
+	".GIF",
+	".TIF", ".TIFF",
+	".14I", ".14S", ".14D", ".14P", ".14L", // Oxcal, my arch-nemesis!
+	".MPJ", ".MTW", // dunno what these are, but they're not mine
+	".GRF", // "GS Draw File: Copyright Golden Software Inc. 1991-1994" (a GRaF)
+	".JAR", ".CLASS", ".JAVA",
+	"SITE.LOC", "SITE.XML", // (old corina data files -- remove these later)
+	corina.browser.Summary.CACHE_FILENAME.toUpperCase(),
+
+	// TODO: should i skip backup~ files here, too?  (they're not actually invalid, just "hidden")
+    };
+
+    private final static String LOADERS[] = {
+	"corina.formats.Corina",
+	"corina.formats.TRML",
+	"corina.formats.TSAPMatrix",
+	"corina.formats.Hohenheim",
+	"corina.formats.Heidelberg",
+	"corina.formats.Tucson",
+	"corina.formats.TwoColumn", // <-- should always be last
+    };
+}
