3985f99601214eb8ddf32b375902cff64f2e2205
no_author <another@cornell.edu>
2004-04-23 02:12:10 +0000

    Some javadoc reformatting and note.

--- Source/corina/gui/Bug.java
+++ Source/corina/gui/Bug.java
@@ -72,193 +72,193 @@ import java.awt.event.ActionEvent;
    @version $Id$
 */
 public class Bug extends JDialog {
-    /**
-       Get the stack trace from a Throwable, as a String.
+  /**
+   * Get the stack trace from a Throwable, as a String.
+   * 
+   * <p>I think Java 1.4 has a method for this, but I'm not
+   * targetting 1.4 (yet).</p>
+   *
+   * @param t the Throwable (usually an Exception) to examine
+   * @return its stack trace
+   */
+  public static String getStackTrace(Throwable t) {
+  	// use PureStringWriter here because otherwise windows
+  	// would show funny boxes when displaying it.
+    // XXX: is that right... I need to check this -- Aaron
+    PureStringWriter sw = new PureStringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    t.printStackTrace(pw);
+    return sw.toString();
+  }
 
-       <p>I think Java 1.4 has a method for this, but I'm not
-       targetting 1.4 (yet).</p>
+  /**
+   * Assemble some system info useful for bug reports.
+   *
+   * <p>This consists of:</p>
+   *
+   * <ul>
+   *   <li>the date and time of the Corina build
+   *   <li>the name, version, and architecture of the OS
+   *   <li>the name, version, and vendor for the Java runtime/VM
+   *       specification/implementation
+   *   <li>the current date and time
+   * </ul>
+   *
+   * @return a string containing some system info
+   */
+  public static String getSystemInfo() {
+	  StringBuffer buf = new StringBuffer();
 
-       @param t the Throwable (usually an Exception) to examine
-       @return its stack trace
-    */
-    public static String getStackTrace(Throwable t) {
-	// use PureStringWriter here because otherwise windows
-	// would show funny boxes when displaying it.
-        PureStringWriter sw = new PureStringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        t.printStackTrace(pw);
-	return sw.toString();
-    }
+  	// a nice header
+  	buf.append("System Information:\n");
+  	buf.append("\n");
+  
+  	// time/date/version of build
+  	buf.append("Corina\n");
+  	buf.append("   Version " + Build.VERSION + "\n");
+  	buf.append("   Built at " + Build.TIMESTAMP + "\n");
+  
+  	// properties of the OS
+  	buf.append("Operating system\n");
+  	buf.append("   Name: " + System.getProperty("os.name") + "\n");
+  	buf.append("   Version: " + System.getProperty("os.version") + "\n");
+  	buf.append("   Architecture: " + System.getProperty("os.arch") + "\n");
 
-    /**
-       Assemble some system info useful for bug reports.
+  	// java runtime environment
+  	buf.append("Java Runtime Environment\n");
+  
+  	// spec
+  	{
+  	    String version = System.getProperty("java.specification.version");
+  	    String vendor = System.getProperty("java.specification.vendor");
+  	    String name = System.getProperty("java.specification.name");
+  	    buf.append("   Specification: " + name +
+  		       ", version " + version +
+  		       ", by " + vendor + "\n");
+  	}
 
-       <p>This consists of:</p>
+  	// impl
+  	{
+  	    String version = System.getProperty("java.version");
+  	    String vendor = System.getProperty("java.vendor");
+  	    buf.append("   Implementation: version " + version +
+  		       ", by " + vendor + "\n");
+  	}
+  
+  	// java VM
+  	buf.append("Java Virtual Machine\n");
+  
+  	// spec
+  	{
+  	    String version = System.getProperty("java.vm.specification.version");
+  	    String vendor = System.getProperty("java.vm.specification.vendor");
+  	    String name = System.getProperty("java.vm.specification.name");
+  	    buf.append("   Specification: " + name +
+  		       ", version " + version +
+  		       ", by " + vendor + "\n");
+  	}
+  
+  	// impl
+  	{
+  	    String version = System.getProperty("java.vm.version");
+  	    String vendor = System.getProperty("java.vm.vendor");
+  	    String name = System.getProperty("java.vm.name");
+  	    buf.append("   Implementation: " + name +
+  		       ", version " + version +
+  		       ", by " + vendor + "\n");
+  	}
+  
+  	// do i care about java.home, java.class.version ("48.0"?),
+  	// or java.class.path?  probably not.
+  
+  	// current date/time
+  	Date now = new Date();
+  	DateFormat date = DateFormat.getDateInstance(DateFormat.LONG);
+  	DateFormat time = DateFormat.getTimeInstance(DateFormat.LONG);
+  	buf.append("\n");
+  	buf.append("Bug report generated: " + date.format(now) +
+  		   " at " + time.format(now) + "\n");
+  	return buf.toString();
+  }
 
-       <ul>
-         <li>the date and time of the Corina build
-         <li>the name, version, and architecture of the OS
-         <li>the name, version, and vendor for the Java runtime/VM
-         specification/implementation
-         <li>the current date and time
-       </ul>
+  /**
+   * Make a new bug dialog.
+   *
+   * @param t a Throwable, usually an Exception, that is a bug
+   */
+  public Bug(Throwable t) {
+	  super();
+	  setTitle("Bug!");
+	  setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-       @return a string containing some system info
-    */
-    public static String getSystemInfo() {
-	StringBuffer buf = new StringBuffer();
+  	JTextArea textArea = new JTextArea("Exception:\n" +
+  					   getStackTrace(t) + "\n\n" +
+  					   getSystemInfo(), 10, 50);
+  	textArea.setEditable(false);
+  	stackTrace = new JScrollPane(textArea);
+  
+  	JPanel message = Layout.flowLayoutL("You found a bug in Corina!");
+  
+  	JButton bummer = new JButton("Bummer");
+  	bummer.addActionListener(new AbstractAction() {
+  		public void actionPerformed(ActionEvent e) {
+  		  dispose();
+  		}
+  	});
+  
+  	more = new JButton("Show Details");
+  	more.addActionListener(new AbstractAction() {
+  		public void actionPerformed(ActionEvent e) {
+  	    // toggle visibility of stackTrace component
+        if (visible) {
+    			// hide it
+    			getContentPane().remove(stackTrace);
+    			pack();
+  
+          // change text: next op will be "show"
+          more.setText("Show Details");
+  		  } else {
+          // show it
+          getContentPane().add(stackTrace,
+  				  BorderLayout.CENTER);
+          pack();
+  
+          // change text: next op will be "hide"
+          more.setText("Hide Details");
+  		  }
+        visible = !visible;
+  		}
+	  });
 
-	// a nice header
-	buf.append("System Information:\n");
-	buf.append("\n");
+  	JPanel buttons = Layout.buttonLayout(more, null, bummer);
+  	buttons.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+  
+  	JPanel content = Layout.borderLayout(message,
+  					     null, null, null,
+  					     buttons);
+  	content.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+  
+  	setContentPane(content);
 
-	// time/date/version of build
-	buf.append("Corina\n");
-	buf.append("   Version " + Build.VERSION + "\n");
-	buf.append("   Built at " + Build.TIMESTAMP + "\n");
+  	pack();
+  	OKCancel.addKeyboardDefaults(bummer);
+  	// setResizable(false);
+  	Center.center(this);
+  	show();
+  }
 
-	// properties of the OS
-	buf.append("Operating system\n");
-	buf.append("   Name: " + System.getProperty("os.name") + "\n");
-	buf.append("   Version: " + System.getProperty("os.version") + "\n");
-	buf.append("   Architecture: " + System.getProperty("os.arch") + "\n");
+  private JComponent stackTrace;
 
-	// java runtime environment
-	buf.append("Java Runtime Environment\n");
+  private JButton more;
 
-	// spec
-	{
-	    String version = System.getProperty("java.specification.version");
-	    String vendor = System.getProperty("java.specification.vendor");
-	    String name = System.getProperty("java.specification.name");
-	    buf.append("   Specification: " + name +
-		       ", version " + version +
-		       ", by " + vendor + "\n");
-	}
+  private boolean visible = false;
 
-	// impl
-	{
-	    String version = System.getProperty("java.version");
-	    String vendor = System.getProperty("java.vendor");
-	    buf.append("   Implementation: version " + version +
-		       ", by " + vendor + "\n");
-	}
-
-	// java VM
-	buf.append("Java Virtual Machine\n");
-
-	// spec
-	{
-	    String version = System.getProperty("java.vm.specification.version");
-	    String vendor = System.getProperty("java.vm.specification.vendor");
-	    String name = System.getProperty("java.vm.specification.name");
-	    buf.append("   Specification: " + name +
-		       ", version " + version +
-		       ", by " + vendor + "\n");
-	}
-
-	// impl
-	{
-	    String version = System.getProperty("java.vm.version");
-	    String vendor = System.getProperty("java.vm.vendor");
-	    String name = System.getProperty("java.vm.name");
-	    buf.append("   Implementation: " + name +
-		       ", version " + version +
-		       ", by " + vendor + "\n");
-	}
-
-	// do i care about java.home, java.class.version ("48.0"?),
-	// or java.class.path?  probably not.
-
-	// current date/time
-	Date now = new Date();
-	DateFormat date = DateFormat.getDateInstance(DateFormat.LONG);
-	DateFormat time = DateFormat.getTimeInstance(DateFormat.LONG);
-	buf.append("\n");
-	buf.append("Bug report generated: " + date.format(now) +
-		   " at " + time.format(now) + "\n");
-	return buf.toString();
-    }
-
-    /**
-       Make a new bug dialog.
-
-       @param t a Throwable, usually an Exception, that is a bug
-    */
-    public Bug(Throwable t) {
-	super();
-	setTitle("Bug!");
-	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
-	JTextArea textArea = new JTextArea("Exception:\n" +
-					   getStackTrace(t) + "\n\n" +
-					   getSystemInfo(), 10, 50);
-	textArea.setEditable(false);
-	stackTrace = new JScrollPane(textArea);
-
-	JPanel message = Layout.flowLayoutL("You found a bug in Corina!");
-
-	JButton bummer = new JButton("Bummer");
-	bummer.addActionListener(new AbstractAction() {
-		public void actionPerformed(ActionEvent e) {
-		    dispose();
-		}
-	    });
-
-	more = new JButton("Show Details");
-	more.addActionListener(new AbstractAction() {
-		public void actionPerformed(ActionEvent e) {
-		    // toggle visibility of stackTrace component
-		    if (visible) {
-			// hide it
-			getContentPane().remove(stackTrace);
-			pack();
-
-			// change text: next op will be "show"
-			more.setText("Show Details");
-		    } else {
-			// show it
-			getContentPane().add(stackTrace,
-					     BorderLayout.CENTER);
-			pack();
-
-			// change text: next op will be "hide"
-			more.setText("Hide Details");
-		    }
-
-		    visible = !visible;
-		}
-	    });
-
-	JPanel buttons = Layout.buttonLayout(more, null, bummer);
-	buttons.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-
-	JPanel content = Layout.borderLayout(message,
-					     null, null, null,
-					     buttons);
-	content.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-	setContentPane(content);
-
-	pack();
-	OKCancel.addKeyboardDefaults(bummer);
-	// setResizable(false);
-	Center.center(this);
-	show();
-    }
-
-    private JComponent stackTrace;
-
-    private JButton more;
-
-    private boolean visible = false;
-
-    /**
-       Old interface! -- fix all occurances of this, then remove.
-
-       @deprecated use new Bug(t)
-    */
-    public static void bug(Throwable t) {
-	new Bug(t);
-    }
+  /**
+   * Old interface! -- fix all occurances of this, then remove.
+   *
+   * @deprecated use new Bug(t)
+   */
+  public static void bug(Throwable t) {
+	  new Bug(t);
+  }
 }
