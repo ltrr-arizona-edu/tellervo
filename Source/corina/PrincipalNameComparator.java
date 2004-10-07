277f3f010ae9ab5528c8d931dee0a93ee033c05b
no_author <another@cornell.edu>
2004-10-07 14:18:09 +0000

    added comment

--- Source/corina/PrincipalNameComparator.java
+++ Source/corina/PrincipalNameComparator.java
@@ -6,6 +6,13 @@ import javax.security.auth.Subject;
 
 import com.sun.security.auth.PrincipalComparator;
 
+/**
+ * XXX: BUGS BUGS BUGS
+ * http://archives.java.sun.com/cgi-bin/wa?A2=ind0307&L=jini-users&P=R3341&I=-3
+ * http://archives.java.sun.com/cgi-bin/wa?A2=ind0308&L=jini-users&F=&S=&P=14578
+ * http://archives.java.sun.com/cgi-bin/wa?A2=ind0307&L=jini-users&P=R3341&I=-3
+ * @author Aaron Hamid
+ */
 public class PrincipalNameComparator implements PrincipalComparator {
   private String principal;
   public PrincipalNameComparator(String principal) {
