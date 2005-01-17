7486d260aada53fe956317975029aedcd8027890
no_author <another@cornell.edu>
2005-01-17 22:51:22 +0000

    updated year

--- Source/corina/Build.java
+++ Source/corina/Build.java
@@ -68,7 +68,7 @@ public class Build {
     public final static String VERSION = "1.0 \u03B2"; // beta
 
     /** Year (for copyright). */
-    public final static String YEAR = "2001-2004";
+    public final static String YEAR = "2001-2005";
 
     /** Author's name (for copyright). */
     public final static String AUTHOR = "Ken Harris, Aaron Hamid";
