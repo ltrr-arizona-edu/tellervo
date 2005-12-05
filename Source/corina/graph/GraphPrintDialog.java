b1409e30e2e8f1dac87b08a34eaab41a1e7a02c0
lucasmo <lucas.madar@gmail.com>
2005-12-05 18:43:12 +0000

    Final graph goodness!

--- Source/corina/graph/GraphPrintDialog.java
+++ Source/corina/graph/GraphPrintDialog.java
@@ -3,22 +3,30 @@
  */
 package corina.graph;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import corina.prefs.PrefsDialog;
 import corina.util.Center;
 
 import java.awt.FlowLayout;
+import java.awt.Container;
 import java.util.List;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.text.DecimalFormat;
 
@@ -26,6 +34,12 @@ import java.awt.print.*;
 
 import javax.swing.text.*;
 import javax.swing.event.*;
+import javax.swing.filechooser.FileFilter;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfWriter;
 
 /**
  * @author Lucas
@@ -45,16 +59,17 @@ public class GraphPrintDialog extends JPanel {
 		final JDialog d;
 		final PreviewPanel preview;
 		final ParamsPanel params;
+		final GraphPrintDialog me = this;
 		
 		gInfo = plot.getPrinterGraphInfo();
 		
-		d = new JDialog(parent, "Printing options...", true);
+		d = new JDialog(parent, "Printing / Exporting options...", true);
 		d.setContentPane(this);
 		d.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
 		setLayout(new BorderLayout());
 		
 		preview = new PreviewPanel(plot, gInfo);
-		params = new ParamsPanel(gInfo);
+		params = new ParamsPanel(gInfo, printType);
 		
 		JTabbedPane tabs = new JTabbedPane();
 		tabs.addTab("Parameters", params);
@@ -78,7 +93,10 @@ public class GraphPrintDialog extends JPanel {
 	    String i18text = (printType == PRINT_PRINTER) ? "print" : "save_as";
 	    String deftext = (printType == PRINT_PRINTER) ? "Print..." : "Save as...";
 	    
-	    String oktext = corina.ui.I18n.getText(i18text);
+	    String oktext = null;
+	    try {
+	    	oktext = corina.ui.I18n.getText(i18text);
+	    } catch (Exception e) { }
 	    if (oktext == null) oktext = deftext;
 	    JButton okButton = new JButton(oktext);
 	    okButtonContainer.add(okButton);
@@ -90,6 +108,14 @@ public class GraphPrintDialog extends JPanel {
 	      public void actionPerformed(ActionEvent ae) {
 	    	  if(_printType == PRINT_PRINTER) {
 	    		  if(new GraphPrinter(_info, _plotter, params.getDPI()).doPrint())	    	  	    	  
+	    			  d.dispose();
+	    	  }
+	    	  else if(_printType == PRINT_PDF) {
+	    		  if(me.printPDF(_info, _plotter))
+	    			  d.dispose();
+	    	  }
+	    	  else if(_printType == PRINT_PNG) {
+	    		  if(me.printPNG(_info, _plotter))
 	    			  d.dispose();
 	    	  }
 	      }
@@ -112,6 +138,171 @@ public class GraphPrintDialog extends JPanel {
 	    d.setVisible(true);	    
 	}
 	
+	protected boolean printPDF(final GraphInfo pinfo, final GrapherPanel plotter) {
+		final JFileChooser chooser = new JFileChooser();
+		final Container me = getParent();
+		chooser.setFileFilter(new FileFilter() {
+			public boolean accept(File f) {
+				return f.getName().endsWith(".pdf");
+			}
+
+			public String getDescription() {
+				return "PDF Document files (*.pdf)";
+			}
+		});
+		chooser.setMultiSelectionEnabled(false);
+		chooser.setAcceptAllFileFilterUsed(false);
+		//chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int returnVal = chooser.showSaveDialog(this);
+		if (returnVal != JFileChooser.APPROVE_OPTION)
+			return false;
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+
+				ProgressMonitor pm = new ProgressMonitor(me, // parent
+						"Exporting graph to PDF file...", // message
+						"", // note
+						0, 5); // round up to 45 MB
+
+				pm.setMillisToDecideToPopup(0);
+				pm.setMillisToPopup(0);
+
+				pm.setProgress(1);
+				pm.setNote("Creating PDF document...");
+
+				String fn = chooser.getSelectedFile().getAbsolutePath();
+				if (!fn.toLowerCase().endsWith(".pdf"))
+					fn += ".pdf";
+
+				// create a PDF document
+				
+				Rectangle rect = new Rectangle(0, 0,
+						(int) (pinfo.getDrawRange().span() * pinfo.getYearSize()),
+						(int) (pinfo.getPrintHeight()));
+				com.lowagie.text.Rectangle pageSize = new com.lowagie.text.Rectangle(
+						rect.width, rect.height);
+				Document document = new Document(pageSize);
+
+				try {
+					// create an associated writer
+					PdfWriter writer = PdfWriter.getInstance(document,
+							new FileOutputStream(new File(fn)));
+
+					// "open" the PDF
+					document.open();
+
+					// set up a font mapper
+					//DefaultFontMapper mapper = new DefaultFontMapper();					
+					//FontFactory.registerDirectories();
+
+					pm.setProgress(2);
+					pm.setNote("Creating PDF memory context...");
+
+					// do some magic to set up the pdf context
+					PdfContentByte cb = writer.getDirectContent();
+					Graphics2D g = cb.createGraphicsShapes(pageSize
+							.width(), pageSize.height());
+
+					plotter.computeRange(pinfo, g);
+					StandardPlot agent = new StandardPlot(pinfo.getDrawRange(), pinfo);
+					plotter.paintGraph(g, pinfo, agent);
+					
+					// finish up the PDF cruft
+					//cb.addTemplate(tp, 0, 0);								
+
+					//dispose of the graphics content
+					pm.setProgress(4);
+					pm.setNote("Cleaning up...");
+					g.dispose();
+				} catch (DocumentException de) {
+					System.err.println(de.getMessage());
+					de.printStackTrace();
+				} catch (IOException ioe) {
+					System.err.println(ioe.getMessage());
+					ioe.printStackTrace();
+				}
+
+				document.close();
+				pm.setProgress(5);
+			}
+		});
+		return true;
+	}
+
+	protected boolean printPNG(final GraphInfo pinfo, final GrapherPanel plotter) {
+		final JFileChooser chooser = new JFileChooser();
+		final Container me = getParent();
+		chooser.setFileFilter(new FileFilter() {
+			public boolean accept(File f) {
+				return f.getName().endsWith(".png");
+			}
+
+			public String getDescription() {
+				return "PNG Image files (*.png)";
+			}
+		});
+		chooser.setMultiSelectionEnabled(false);
+		chooser.setAcceptAllFileFilterUsed(false);
+		//chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int returnVal = chooser.showSaveDialog(this);
+		if (returnVal != JFileChooser.APPROVE_OPTION)
+			return false;
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+
+				ProgressMonitor pm = new ProgressMonitor(me, // parent
+						"Exporting graph to PNG file...", // message
+						"", // note
+						0, 5); // round up to 45 MB
+
+				pm.setMillisToDecideToPopup(0);
+				pm.setMillisToPopup(0);
+
+				pm.setProgress(1);
+				pm.setNote("Creating PNG document...");
+				
+				Rectangle rect = new Rectangle(0, 0,
+						(int) (pinfo.getDrawRange().span() * pinfo.getYearSize()),
+						(int) (pinfo.getPrintHeight()));
+				
+				BufferedImage fileImage = new BufferedImage(rect.width,
+						rect.height, BufferedImage.TYPE_INT_RGB);
+				Graphics2D g = fileImage.createGraphics();
+
+				pm.setProgress(2);
+				pm.setNote("Drawing graph...");
+
+				plotter.computeRange(pinfo, g);
+				StandardPlot agent = new StandardPlot(pinfo.getDrawRange(), pinfo);
+				plotter.paintGraph(g, pinfo, agent);
+				
+				pm.setProgress(3);
+				pm.setNote("Encoding as PNG and saving file...");
+
+				try {
+					String fn = chooser.getSelectedFile()
+							.getAbsolutePath();
+					if (!fn.toLowerCase().endsWith(".png"))
+						fn += ".png";
+					ImageIO.write(fileImage, "png", new File(fn));
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+
+				pm.setProgress(4);
+				pm.setNote("Cleaning up...");
+				
+				// ack! don't forget to dispose of the graphics context!
+				g.dispose();
+
+				pm.setProgress(5);
+			}
+		});
+		return true;
+	}
+	
 	private class ParamsPanel extends JPanel implements DocumentListener {		
 		JTextField pixelsperyear;
 		JTextField dpi;
@@ -128,7 +319,7 @@ public class GraphPrintDialog extends JPanel {
 	    	return Integer.parseInt(dpi.getText());
 	    }
 		
-		public ParamsPanel(GraphInfo g) {
+		public ParamsPanel(GraphInfo g, int printType) {
 			super();
 		
 			gInfo = g;
@@ -171,6 +362,8 @@ public class GraphPrintDialog extends JPanel {
 		    gbc.gridx++;
 		    
 		    dpi = new JTextField(6);
+		    if(printType != GraphPrintDialog.PRINT_PRINTER)
+		    	dpi.setEditable(false);
 		    dpi.setText(Integer.toString(72));
 		    dpi.getDocument().addDocumentListener(this);
 		    dpi.getDocument().putProperty("propname", "dpi");
@@ -264,7 +457,7 @@ public class GraphPrintDialog extends JPanel {
 		}
 		
 		private void recalc(DocumentEvent d) {
-			Document doc = (Document) d.getDocument();
+			javax.swing.text.Document doc = (javax.swing.text.Document) d.getDocument();
 			String source = (String) doc.getProperty("propname");
 			
 			if(source.equals("pixelsperyear")) {
