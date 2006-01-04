46f43c8b7c1fba5cb712c03cbb687679d09c420b
lucasmo <lucas.madar@gmail.com>
2006-01-04 07:25:36 +0000

    Add "rename to" menu option for samples...

--- Source/corina/editor/EditorFileMenu.java
+++ Source/corina/editor/EditorFileMenu.java
@@ -2,12 +2,20 @@ package corina.editor;
 
 import corina.Sample;
 import corina.io.ExportDialog;
+import corina.gui.FileDialog;
+import corina.gui.SaveableDocument;
+import corina.gui.UserCancelledException;
 import corina.gui.menus.FileMenu;
+import corina.gui.menus.OpenRecent;
 import corina.ui.Builder;
+import corina.ui.I18n;
+import corina.util.Overwrite;
 
 import javax.swing.JMenuItem;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import javax.swing.JOptionPane;
 
 // a FileMenu with "Export..." for samples.
 // TODO: this doesn't need to be public.
@@ -43,5 +51,44 @@ public class EditorFileMenu extends FileMenu {
 		}
 	    });
 	add(export);
+    
+	// add "Rename to..." menuitem
+	JMenuItem rename_to = Builder.makeMenuItem("rename_to...");
+	rename_to.addActionListener(new AbstractAction() {
+		public void actionPerformed(ActionEvent e) {
+			try {
+			    // get doc
+			    SaveableDocument doc = (SaveableDocument) f;
+			    if(doc.getFilename() == null) {
+			    	JOptionPane.showMessageDialog(f, "Can't 'rename' an unsaved Sample.\nUse save as instead.", 
+			    			"Error", JOptionPane.ERROR_MESSAGE);
+			    	return;
+			    }
+			    
+			    // be careful.. this may be overkill?
+			    File oldFile = new File(new String(doc.getFilename()));
+				
+			    // DESIGN: start out in the same folder as the old filename,
+			    // if there is one?
+
+			    // get new filename
+			    String filename = FileDialog.showSingle(I18n.getText("rename_to..."));
+			    File newFile = new File(filename);
+			    
+			    if(newFile.exists()) {
+			    	JOptionPane.showMessageDialog(f, "Can't rename to a file that already exists.\nUse save as instead.",
+			    			"Error renaming...", JOptionPane.ERROR_MESSAGE);
+			    	return;
+			    }
+			    
+			    doc.setFilename(filename);
+			    OpenRecent.fileOpened(doc.getFilename());			    
+			    oldFile.renameTo(newFile);
+			} catch (UserCancelledException uce) {
+			    // do nothing
+			}			
+		}
+	    });
+	add(rename_to);
     }
 }
