ab8b27177e8f72f8dec49a09bc45e59aafe22740
lucasmo <lucas.madar@gmail.com>
2006-02-24 20:51:34 +0000

    Format for readability

--- Source/corina/cross/RValue.java
+++ Source/corina/cross/RValue.java
@@ -29,183 +29,194 @@ import corina.index.HighPass;
 import corina.ui.I18n;
 
 /**
-   An R-value.
+ An R-value.
 
-   <p>Originally, the r-value was never explicitly reported: it was
-   merely an intermediate value used in computing the T-score.
-   Eventually, though, the statisticians insisted on seeing the
-   r-values for themselves, so it was extracted.  (The T-score is now
-   phrased in terms of this r-value.)</p>
+ <p>Originally, the r-value was never explicitly reported: it was
+ merely an intermediate value used in computing the T-score.
+ Eventually, though, the statisticians insisted on seeing the
+ r-values for themselves, so it was extracted.  (The T-score is now
+ phrased in terms of this r-value.)</p>
 
-   @author Ken Harris &lt;kbh7 <i style="color: gray">at</i> cornell <i style="color: gray">dot</i> edu&gt;
-   @version $Id$ */
+ @author Ken Harris &lt;kbh7 <i style="color: gray">at</i> cornell <i style="color: gray">dot</i> edu&gt;
+ @version $Id$ */
 public class RValue extends Cross {
 
-    /** Size of window to use for the smoothing curve done by the
-	normalization.
-	@see #normalize */
-    private final static int WINDOW=5;
+	/** Size of window to use for the smoothing curve done by the
+	 normalization.
+	 @see #normalize */
+	private final static int WINDOW = 5;
 
-    /** A mutable copy of the fixed data, in array form. */
-    private float fixedData[];
+	/** A mutable copy of the fixed data, in array form. */
+	private float fixedData[];
 
-    /** A mutable copy of the moving data, in array form. */
-    private float movingData[];
+	/** A mutable copy of the moving data, in array form. */
+	private float movingData[];
 
-    /** The mean of the fixed data. */
-    private float fixedMean=0;
+	/** The mean of the fixed data. */
+	private float fixedMean = 0;
 
-    /** The mean of the moving data. */
-    private float movingMean=0;
+	/** The mean of the moving data. */
+	private float movingMean = 0;
 
-    // don't use me
-    protected RValue() { }
+	// don't use me
+	protected RValue() {
+	}
 
-    /** Construct a new R-value from two samples.
-	@param s1 the fixed sample
-	@param s2 the moving sample */
-    public RValue(Sample s1, Sample s2) {
-	super(s1, s2);
-    }
+	/** Construct a new R-value from two samples.
+	 @param s1 the fixed sample
+	 @param s2 the moving sample */
+	public RValue(Sample s1, Sample s2) {
+		super(s1, s2);
+	}
 
-    /** Return a prettier name for this cross: "R-Value".
-	@return the name of this cross, "R-Value" */
-    public String getName() {
-	return I18n.getText("rvalue");
-    }
+	/** Return a prettier name for this cross: "R-Value".
+	 @return the name of this cross, "R-Value" */
+	public String getName() {
+		return I18n.getText("rvalue");
+	}
 
-    /** A format string for R-values.
-	@return a format string for R-values */
-    public String getFormat() {
-	return App.prefs.getPref("corina.cross.rvalue.format", "0.00");
-    }
+	/** A format string for R-values.
+	 @return a format string for R-values */
+	public String getFormat() {
+		return App.prefs.getPref("corina.cross.rvalue.format", "0.00");
+	}
 
-    public boolean isSignificant(float score, int overlap) {
-	if (overlap == 0) // it happens...
-	    return false;
+	public boolean isSignificant(float score, int overlap) {
+		if (overlap == 0) // it happens...
+			return false;
 
-	// WRITEME: what's a sig r-value?
-	return (score > 0.25f);
-    }
+		// WRITEME: what's a sig r-value?
+		return (score > 0.25f);
+	}
 
-    // FIXME: shouldn't need both of these!
-    public float getMinimumSignificant() {
-        return 0.25f; // FIXME: what's a sig r-value?
-    }
-    
-    /** Normalize the data.  This creates a new array of floats from
-	the List of Numbers.  The data is smoothed using a 5-point
-	window, and then the natural logarithm is taken.
-	@param data a List of Numbers holding the data to normalize
-	@return an array of floats containing the normalized data */
-    private float[] normalize(List data) {
-        // size of the data: used many times below
-        int N = data.size();
-        float smoothed[] = new float[N];
+	// FIXME: shouldn't need both of these!
+	public float getMinimumSignificant() {
+		return 0.25f; // FIXME: what's a sig r-value?
+	}
 
-        // make sure everything is positive (for GAZ37ABC.TRU)
-	Integer one = new Integer(1); // (singleton)
-        for (int i=0; i<N; i++)
-            if (((Number) data.get(i)).intValue() < 0)
-                data.set(i, one);
+	/** Normalize the data.  This creates a new array of floats from
+	 the List of Numbers.  The data is smoothed using a 5-point
+	 window, and then the natural logarithm is taken.
+	 @param data a List of Numbers holding the data to normalize
+	 @return an array of floats containing the normalized data */
+	private float[] normalize(List data) {
+		// size of the data: used many times below
+		int N = data.size();
+		float smoothed[] = new float[N];
 
-        // make array weights={1,1,1,...}
-        int weights[] = new int[WINDOW];
-        Arrays.fill(weights, 1);
-	// FIXME: if WINDOW is fixed, this should be static
+		// make sure everything is positive (for GAZ37ABC.TRU)
+		Integer one = new Integer(1); // (singleton)
+		for (int i = 0; i < N; i++)
+			if (((Number) data.get(i)).intValue() < 0)
+				data.set(i, one);
 
-        // borrow a high-pass filter
-        List tmp = HighPass.filter(data, weights);
-	// high-pass should return an array!  (and maybe take an array, too!)
+		// make array weights={1,1,1,...}
+		int weights[] = new int[WINDOW];
+		Arrays.fill(weights, 1);
+		// FIXME: if WINDOW is fixed, this should be static
 
-        // natural logarithm -- log(0) is bad news, so log(max(s[i],eps))
-        for (int i=0; i<N; i++) {
-	    float value = ((Number) data.get(i)).floatValue();
-	    float filter = ((Number) tmp.get(i)).floatValue();
-	    float ratio = value / filter;
+		// temporary test of alternate weighting system
+		/*
+		weights[0] = 1;
+		weights[1] = 2;
+		weights[2] = 4;
+		weights[3] = 2;
+		weights[4] = 1;
+		*/
+		
+		// borrow a high-pass filter
+		List tmp = HighPass.filter(data, weights);
+		// high-pass should return an array!  (and maybe take an array, too!)
 
-            smoothed[i] = (float) Math.log(Math.max(100*ratio, EPS));
-        }
+		// natural logarithm -- log(0) is bad news, so log(max(s[i],eps))
+		for (int i = 0; i < N; i++) {
+			float value = ((Number) data.get(i)).floatValue();
+			float filter = ((Number) tmp.get(i)).floatValue();
+			float ratio = value / filter;
 
-        return smoothed;
-    }
+			smoothed[i] = (float) Math.log(Math.max(100 * ratio, EPS));
+		}
 
-    // a really small value
-    private final static float EPS = Float.MIN_VALUE;
+		return smoothed;
+	}
 
-    /** Compute the mean of an array of floats.
-	@param array the array of floats to average
-	@return the average (arithmetic mean) of the given array */
-    private float mean(float array[]) {
-        // (/ (reduce '+ array) (length array))
-        float s = 0.0f;
-        for (int i=0; i<array.length; i++)
-            s += array[i];
-        return s / array.length;
+	// a really small value
+	private final static float EPS = Float.MIN_VALUE;
 
-	// what if array.length==0? -- only happens if one sample has no data (possible?)
-    }
+	/** Compute the mean of an array of floats.
+	 @param array the array of floats to average
+	 @return the average (arithmetic mean) of the given array */
+	private float mean(float array[]) {
+		// (/ (reduce '+ array) (length array))
+		float s = 0.0f;
+		for (int i = 0; i < array.length; i++)
+			s += array[i];
+		return s / array.length;
 
-    /** Given offsets into the fixed and moving data, compute a single
-	R-value for that position.
-	@return the R-value for this possible cross */
-    public float compute(int offsetFixed, int offsetMoving) {
-        int i=offsetFixed, j=offsetMoving;
-        int overlap=0;
+		// what if array.length==0? -- only happens if one sample has no data (possible?)
+	}
 
-	float z1 = 0.0f;
-	float z2 = 0.0f;
-        float z3 = 0.0f;
+	/** Given offsets into the fixed and moving data, compute a single
+	 R-value for that position.
+	 @return the R-value for this possible cross */
+	public float compute(int offsetFixed, int offsetMoving) {
+		int i = offsetFixed, j = offsetMoving;
+		int overlap = 0;
 
-        float xx, yy;
+		float z1 = 0.0f;
+		float z2 = 0.0f;
+		float z3 = 0.0f;
 
-        while (i<fixedData.length && j<movingData.length) {
-            xx = fixedData[i] - fixedMean;
-            yy = movingData[j] - movingMean;
+		float xx, yy;
 
-	    // NOTE: looking at this today (22.8.2002), i'm not sure
-	    // this shouldn't be the mean of the overlapping sections
-	    // only (instead of the mean of the entire samples), but
-	    // this appears to be what baillie did, i'm pretty sure
-	    // it's what mecki did, and it gives reasonable results
-	    // (changing which mean it uses probably wouldn't have
-	    // much effect), so i'm going to leave it for now.
+		while (i < fixedData.length && j < movingData.length) {
+			xx = fixedData[i] - fixedMean;
+			yy = movingData[j] - movingMean;
 
-            z1 += xx*xx;
-            z2 += yy*yy;
-            z3 += xx*yy;
+			// NOTE: looking at this today (22.8.2002), i'm not sure
+			// this shouldn't be the mean of the overlapping sections
+			// only (instead of the mean of the entire samples), but
+			// this appears to be what baillie did, i'm pretty sure
+			// it's what mecki did, and it gives reasonable results
+			// (changing which mean it uses probably wouldn't have
+			// much effect), so i'm going to leave it for now.
 
-            overlap++;
-            i++;  j++;
-        }
+			z1 += xx * xx;
+			z2 += yy * yy;
+			z3 += xx * yy;
 
-	// in B&P's t-score algorithm, they aborted here with t=0 if z3<0.
-	// i'm just an r-value now, so returning negative numbers is fine.
-	// if z3<0, then r<0, so the t-score can abort with t=0 if r<0.
-	// it's ever-so-slightly less efficient, but equally correct.
+			overlap++;
+			i++;
+			j++;
+		}
 
-	// DOCUMENTME: why i'm computing r so strangely (it's the accuracy)
-        float sigx = (float) Math.sqrt(z1/overlap);
-        float sigy = (float) Math.sqrt(z2/overlap);
-        float r = z3/(overlap*sigx*sigy);
+		// in B&P's t-score algorithm, they aborted here with t=0 if z3<0.
+		// i'm just an r-value now, so returning negative numbers is fine.
+		// if z3<0, then r<0, so the t-score can abort with t=0 if r<0.
+		// it's ever-so-slightly less efficient, but equally correct.
 
-        // if overlap=1 or some other silliness, make it zero -- BUT WHY?
-        if (Float.isNaN(r))
-            r = 0;
+		// DOCUMENTME: why i'm computing r so strangely (it's the accuracy)
+		float sigx = (float) Math.sqrt(z1 / overlap);
+		float sigy = (float) Math.sqrt(z2 / overlap);
+		float r = z3 / (overlap * sigx * sigy);
 
-        return r;
-    }
+		// if overlap=1 or some other silliness, make it zero -- BUT WHY?
+		if (Float.isNaN(r))
+			r = 0;
 
-    /** Preamble: copy all data to (mutable) float arrays, normalize
-        the data, and compute means of the series. */
-    // FIXME: preamble() in Cross is dumb; make it just a lazy-evaluation in RValue's compute()
-    protected void preamble() {
-	// normalize (while copying to mutable arrays)
-	fixedData = normalize(getFixed().data);
-	movingData = normalize(getMoving().data);
+		return r;
+	}
 
-	// compute means (used later by compute())
-	fixedMean = mean(fixedData);
-	movingMean = mean(movingData);
-    }
+	/** Preamble: copy all data to (mutable) float arrays, normalize
+	 the data, and compute means of the series. */
+	// FIXME: preamble() in Cross is dumb; make it just a lazy-evaluation in RValue's compute()
+	protected void preamble() {
+		// normalize (while copying to mutable arrays)
+		fixedData = normalize(getFixed().data);
+		movingData = normalize(getMoving().data);
+
+		// compute means (used later by compute())
+		fixedMean = mean(fixedData);
+		movingMean = mean(movingData);
+	}
 }
--- Source/corina/cross/TScore.java
+++ Source/corina/cross/TScore.java
@@ -25,58 +25,58 @@ import corina.core.App;
 import corina.ui.I18n;
 
 /**
-   <p>A (Student's) T-Score crossdate.</p>
+ <p>A (Student's) T-Score crossdate.</p>
 
-   <p>This algorithm is based on Mecki Pohl's algorithm, and gives
-   very similar results (reason for deviations unknown, possibly
-   rounding error).  I'm not completely certain this implementation is
-   correct, but it gives good results.</p>
+ <p>This algorithm is based on Mecki Pohl's algorithm, and gives
+ very similar results (reason for deviations unknown, possibly
+ rounding error).  I'm not completely certain this implementation is
+ correct, but it gives good results.</p>
 
-   <p>There are apparently many (dozens of?) T-score algorithms in
-   existance.  This one is taken from Baillie and Pilcher's "A Simple
-   Crossdating Program", pp. 7-14, Tree-Ring Bulletin, Vol. 33, 1973.
-   (That version was "written in FORTRAN IV and uses a card reader and
-   line printer".)</p>
+ <p>There are apparently many (dozens of?) T-score algorithms in
+ existance.  This one is taken from Baillie and Pilcher's "A Simple
+ Crossdating Program", pp. 7-14, Tree-Ring Bulletin, Vol. 33, 1973.
+ (That version was "written in FORTRAN IV and uses a card reader and
+ line printer".)</p>
 
-   <p>The procedure used is as follows:</p>
+ <p>The procedure used is as follows:</p>
 
-   <ol>
+ <ol>
 
-     <li>Normalize the data (make it "bivariate-normal"):</li>
+ <li>Normalize the data (make it "bivariate-normal"):</li>
 
-     <ul>
+ <ul>
 
-       <li>convert each value to the percentage of the mean of the 5
-       values it is the center of; then</li>
+ <li>convert each value to the percentage of the mean of the 5
+ values it is the center of; then</li>
 
-       <li>take the natural logarithm of each value</li>
+ <li>take the natural logarithm of each value</li>
 
-     </ul>
+ </ul>
 
-     <li>For each possible overlap, compute the r (correlation coefficient):</li>
+ <li>For each possible overlap, compute the r (correlation coefficient):</li>
 
-     <blockquote>
-     <i>s</i><sub>1</sub> = <big>&Sigma;</big>
-             ( <i>x</i><sub>i</sub><i>y</i><sub>i</sub> -
-	       <i>N</i> ( <i>x</i><sub>i</sub> - <i>x</i><sub>avg</sub> )
-	                ( <i>y</i><sub>i</sub> - <i>y</i><sub>avg</sub> ) )
-     </blockquote>
+ <blockquote>
+ <i>s</i><sub>1</sub> = <big>&Sigma;</big>
+ ( <i>x</i><sub>i</sub><i>y</i><sub>i</sub> -
+ <i>N</i> ( <i>x</i><sub>i</sub> - <i>x</i><sub>avg</sub> )
+ ( <i>y</i><sub>i</sub> - <i>y</i><sub>avg</sub> ) )
+ </blockquote>
 
-     <blockquote>
-     <i>s</i><sub>2</sub> = <big>&Sigma;</big>
-             ( <i>x</i><sub>i</sub><sup>2</sup> -
-	       <i>N</i> ( <i>x</i><sub>i</sub> - <i>x</i><sub>avg</sub> )<sup>2</sup> )
-     </blockquote>
+ <blockquote>
+ <i>s</i><sub>2</sub> = <big>&Sigma;</big>
+ ( <i>x</i><sub>i</sub><sup>2</sup> -
+ <i>N</i> ( <i>x</i><sub>i</sub> - <i>x</i><sub>avg</sub> )<sup>2</sup> )
+ </blockquote>
 
-     <blockquote>
-     <i>s</i><sub>3</sub> = <big>&Sigma;</big>
-             ( <i>y</i><sub>i</sub><sup>2</sup> -
-	       <i>N</i> ( <i>y</i><sub>i</sub> - <i>y</i><sub>avg</sub> )<sup>2</sup> )
-     </blockquote>
+ <blockquote>
+ <i>s</i><sub>3</sub> = <big>&Sigma;</big>
+ ( <i>y</i><sub>i</sub><sup>2</sup> -
+ <i>N</i> ( <i>y</i><sub>i</sub> - <i>y</i><sub>avg</sub> )<sup>2</sup> )
+ </blockquote>
 
-     <blockquote>
-     <i>r</i> = <i>s</i><sub>1</sub> / &radic;( <i>s</i><sub>2</sub> <i>s</i><sub>3</sub> )
-     </blockquote>
+ <blockquote>
+ <i>r</i> = <i>s</i><sub>1</sub> / &radic;( <i>s</i><sub>2</sub> <i>s</i><sub>3</sub> )
+ </blockquote>
 
  <li>Compute the t-score:</li>
 
@@ -84,123 +84,98 @@ import corina.ui.I18n;
  <i>t</i> = <i>r</i> &radic;( (<i>N</i> - 2) / (1 - <i>r</i><sup>2</sup>) )
  </blockquote>
  
-   </ol>
+ </ol>
 
-   <p>The work of computing the r-value is independent, and it turns
-   out users want that statistic, too, so that has been extracted into
-   the class RValue, which TScore now extends.</p>
+ <p>The work of computing the r-value is independent, and it turns
+ out users want that statistic, too, so that has been extracted into
+ the class RValue, which TScore now extends.</p>
 
-   <p>This class is dedicated to poor Mr. Potter out in Van Nuys.</p>
+ <p>This class is dedicated to poor Mr. Potter out in Van Nuys.</p>
 
-   @author Ken Harris &lt;kbh7 <i style="color: gray">at</i> cornell <i style="color: gray">dot</i> edu&gt;
-   @version $Id$ */
+ @author Ken Harris &lt;kbh7 <i style="color: gray">at</i> cornell <i style="color: gray">dot</i> edu&gt;
+ @version $Id$ */
 public class TScore extends RValue {
 
-    // don't use me
-    protected TScore() { }
+	// don't use me
+	protected TScore() {
+	}
 
-    /** Construct a new T-score from two samples.
-	@param s1 the fixed sample
-	@param s2 the moving sample */
-    public TScore(Sample s1, Sample s2) {
-	super(s1, s2);
-    }
+	/** Construct a new T-score from two samples.
+	 @param s1 the fixed sample
+	 @param s2 the moving sample */
+	public TScore(Sample s1, Sample s2) {
+		super(s1, s2);
+	}
 
-    /** Return a prettier name for this cross: "T-Score".
-	@return the name of this cross, "T-Score" */
-    public String getName() {
-	return I18n.getText("tscore");
-    }
+	/** Return a prettier name for this cross: "T-Score".
+	 @return the name of this cross, "T-Score" */
+	public String getName() {
+		return I18n.getText("tscore");
+	}
 
-    /** A format string for T-scores.
-	@return a format string for T-scores */
-    public String getFormat() {
-	    return App.prefs.getPref("corina.cross.tscore.format", "0.00");
-    }
+	/** A format string for T-scores.
+	 @return a format string for T-scores */
+	public String getFormat() {
+		return App.prefs.getPref("corina.cross.tscore.format", "0.00");
+	}
 
-    // for 99.5% confidence, from PIK's table (source?)
-    // "statistics for archaeologists" (DRENNAN) is off-by-a-column; is he wrong?
-    // though i can't find his source, "contemporary statistical methods" (KOOPMANS) to check.
-    // update: carol says it's probably one-sided versus two-sided (it's a symmetric
-    // distribution).  that seems fair.
-    private static float table[] = {
-        63.657f,
-        9.925f,
-        5.841f,
-        4.604f,
-        4.032f,
-        3.707f,
-        3.499f,
-        3.355f,
-        3.250f,
-        3.169f,
-        3.106f,
-        3.055f,
-        3.012f,
-        2.977f,
-        2.947f,
-        2.921f,
-        2.898f,
-        2.878f,
-        2.861f,
-        2.845f,
-        2.831f,
-        2.819f,
-        2.807f,
-        2.797f,
-        2.787f,
-        2.779f,
-        2.771f,
-        2.763f,
-        2.756f,
-        2.750f,
-    };
-    public boolean isSignificant(float score, int overlap) {
-	if (overlap == 0) // it happens...
-	    return false;
+	// for 99.5% confidence, from PIK's table (source?)
+	// "statistics for archaeologists" (DRENNAN) is off-by-a-column; is he wrong?
+	// though i can't find his source, "contemporary statistical methods" (KOOPMANS) to check.
+	// update: carol says it's probably one-sided versus two-sided (it's a symmetric
+	// distribution).  that seems fair.
+	private static float table[] = { 63.657f, 9.925f, 5.841f, 4.604f, 4.032f,
+			3.707f, 3.499f, 3.355f, 3.250f, 3.169f, 3.106f, 3.055f, 3.012f,
+			2.977f, 2.947f, 2.921f, 2.898f, 2.878f, 2.861f, 2.845f, 2.831f,
+			2.819f, 2.807f, 2.797f, 2.787f, 2.779f, 2.771f, 2.763f, 2.756f,
+			2.750f, };
 
-	// 2.704 is for overlap=40; 60 is 2.660, inf=2.576
-	// WRITEME: so take care of those cases!
-        float threshold = ((overlap <= 30) ? table[overlap-1] : 2.576f);
+	public boolean isSignificant(float score, int overlap) {
+		if (overlap == 0) // it happens...
+			return false;
 
-        return (score >= threshold);
-    }
+		// 2.704 is for overlap=40; 60 is 2.660, inf=2.576
+		// WRITEME: so take care of those cases!
+		float threshold = ((overlap <= 30) ? table[overlap - 1] : 2.576f);
 
-    // OBSOLETE! -- but still used by CrossPrinter, CrossFrame
-    public float getMinimumSignificant() {
-        return 2.55f;
-    }
-    
-    /**
-       Given offsets into the fixed and moving data, compute a single
-       T-score for that position.
+		return (score >= threshold);
+	}
 
-       @return the T-score for this possible cross
-    */
-    public float compute(int offsetFixed, int offsetMoving) {
+	// OBSOLETE! -- but still used by CrossPrinter, CrossFrame
+	public float getMinimumSignificant() {
+		return 2.55f;
+	}
 
-	int overlap = Math.min(getFixed().data.size() - offsetFixed,
-			       getMoving().data.size() - offsetMoving);
+	/**
+	 Given offsets into the fixed and moving data, compute a single
+	 T-score for that position.
 
-	// already know how to compute r
-	float r = super.compute(offsetFixed, offsetMoving);
-	// FIXME: get rid of cast!
+	 @return the T-score for this possible cross
+	 */
+	public float compute(int offsetFixed, int offsetMoving) {
 
-	// if r is negative, t is zero.
-	// (baillie & pilcher caught this earlier, at r's z3, but this
-	// way is more convenient for me, and gives the same result.)
-	if (r < 0)
-	    return 0;
+		int overlap = Math.min(getFixed().data.size() - offsetFixed,
+				getMoving().data.size() - offsetMoving);
 
-	// t = r * sqrt(n-2)/sqrt(1-r^2)
-	float num = (float) Math.sqrt(overlap-2);
-	float den = (float) Math.sqrt(1-r*r);
-        float t = r*num/den;
+		// already know how to compute r
+		float r = super.compute(offsetFixed, offsetMoving);
+		// FIXME: get rid of cast!
 
-        // if overlap=1 or some other silliness, make it zero.
-        if (Float.isNaN(t))
-            t = 0;
+		// if r is negative, t is zero.
+		// (baillie & pilcher caught this earlier, at r's z3, but this
+		// way is more convenient for me, and gives the same result.)
+		if (r < 0)
+			return 0;
 
-        return t;
-    }
+		// t = r * sqrt(n-2)/sqrt(1-r^2)
+		float num = (float) Math.sqrt(overlap - 2);
+		float den = (float) Math.sqrt(1 - r * r);
+		float t = r * num / den;
+
+		// if overlap=1 or some other silliness, make it zero.
+		if (Float.isNaN(t))
+			t = 0;
+
+		return t;
+	}
 }
