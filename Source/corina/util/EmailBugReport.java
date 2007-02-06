a4f05af8528378f191b2e2a09ba3d70fc0c4077a
lucasmo <lucas.madar@gmail.com>
2007-02-06 20:02:33 +0000

    add support to e-mail bug reports

---
+++ Source/corina/util/EmailBugReport.java
@@ -0,0 +1,56 @@
+package corina.util;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import corina.core.App;
+import corina.ui.Alert;
+import corina.logging.CorinaLog;
+
+import javax.swing.ListModel;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+
+public class EmailBugReport {
+	// don't instantiate me!
+	private EmailBugReport() {}
+	
+	public static void submitBugReport() {
+		try {
+			SimpleEmail email = new SimpleEmail();
+			email.setHostName(App.prefs.getPref("corina.mail.mailhost"));
+			email.setFrom("corina-auto-report@dendro.cornell.edu", "Corina Automated Bug Report");
+			email.addTo("lucas@dendro.cornell.edu");
+			email.setSubject("Subject!");
+			
+			ListModel logEntries = CorinaLog.getLogListModel();
+			StringBuffer errors = new StringBuffer();
+			
+	        Date date = new Date();
+	        errors.append("Corina error log submission\r\n");
+	        errors.append("Submitted on ");
+	        errors.append(DateFormat.getDateInstance().format(date));
+	        errors.append(" ");
+	        errors.append(DateFormat.getTimeInstance().format(date));
+	        errors.append(" by ");
+	        errors.append(System.getProperty("user.name", "(unknown user)"));
+	        errors.append("\r\n\r\n");
+			
+			for(int i = 0; i < logEntries.getSize(); i++) {
+				errors.append(logEntries.getElementAt(i));
+				errors.append("\r\n");
+			}
+			
+			email.setMsg(errors.toString());
+			email.send();
+		} catch (EmailException e) {
+			Alert.error("Error report email", "Could not send error report e-mail. View the error log.");
+			e.printStackTrace();
+			return;
+		}
+		
+		Alert.message("Error report email", "Error report e-mail successfully sent.");
+	}
+}
