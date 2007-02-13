aa984135faac385798e8d8a847c162226d4552ec
lucasmo <lucas.madar@gmail.com>
2007-02-13 18:58:57 +0000

    Heidelberg reading and writing fixes for various "new" formats

--- Source/corina/formats/Heidelberg.java
+++ Source/corina/formats/Heidelberg.java
@@ -101,16 +101,53 @@ public class Heidelberg implements Filetype {
 
 			// WRITE ME: parse other tags and interpret metadata as
 			// intelligently as possible
+			if(tag.equals("Species"))
+				s.meta.put("species", value);
+			else if(tag.equals("Location"))
+				s.meta.put("title", value);
 		}
+
+		// if we got here, line starts with DATA:...
+		String dataFormat = line.substring(5);
+				
+		// no end?  die.
+		if (end == null)
+			throw new WrongFiletypeException();
+		
+		StreamTokenizer t = new StreamTokenizer(r);
+		
+		if(dataFormat.compareToIgnoreCase("single") == 0 || dataFormat.compareToIgnoreCase("tree") == 0) {
+			parseSingle(s, t, length);
+		}
+		else if(dataFormat.compareToIgnoreCase("double") == 0) {
+			parseDouble(s, t, length);
+		}
+		else if(dataFormat.compareToIgnoreCase("chrono") == 0) {
+			parseFull(s, t, length);
+		}
+		else {
+			// hmm. what should we do here?
+			// let's call this a bug, so we get better user feedback.
+
+			IOException ioe = new IOException("Couldn't open Heidelberg data format: " + dataFormat);
+			new corina.gui.Bug(ioe);
+			throw ioe;
+		}
+
+		// set range, and return
+		s.range = new Range(end.add(1 - s.data.size()), end);
+		return s;
+	}
+
+	private void parseFull(Sample s, StreamTokenizer t, int length) throws IOException {
+		int idx = 0;
+
+		// data -- i'll assume all data is (width,count,up,down)
 
 		s.count = new ArrayList();
 		s.incr = new ArrayList();
 		s.decr = new ArrayList();
-
-		// data -- i'll assume all data is (width,count,up,down)
-		StreamTokenizer t = new StreamTokenizer(r);
-		int idx = 0;
-
+		
 		for (;;) {
 			// parse (datum, count, up, dn)
 			int datum, count, up, dn;
@@ -143,15 +180,75 @@ public class Heidelberg implements Filetype {
 			if (idx == length)
 				break;
 		}
-
-		// no end?  die.
-		if (end == null)
-			throw new WrongFiletypeException();
-
-		// set range, and return
-		s.range = new Range(end.add(1 - s.data.size()), end);
-		return s;
 	}
+	
+	private void parseSingle(Sample s, StreamTokenizer t, int length) throws IOException {
+		int idx = 0;
+
+		// data -- i'll assume all data is just a width
+		
+		for (;;) {
+			// parse (datum)
+			int datum;
+			try {
+				t.nextToken();
+				datum = (int) t.nval;
+			} catch (IOException ioe) {
+				throw new WrongFiletypeException();
+			}
+
+			// (0) seems to mean end-of-sample
+			if (datum == 0)
+				break;
+
+			// add to lists
+			s.data.add(new Integer(datum));
+
+			idx++;
+
+			// break out if we have 'length' samples
+			if (idx == length)
+				break;
+		}
+		
+	}
+
+	private void parseDouble(Sample s, StreamTokenizer t, int length) throws IOException {
+		int idx = 0;
+
+		// data -- i'll assume all data is (width,count)
+
+		s.count = new ArrayList();
+		
+		for (;;) {
+			// parse (datum, count)
+			int datum, count;
+			try {
+				t.nextToken();
+				datum = (int) t.nval;
+				t.nextToken();
+				count = (int) t.nval;
+			} catch (IOException ioe) {
+				throw new WrongFiletypeException();
+			}
+
+			// (0,0) seems to mean end-of-sample
+			if (datum == 0)
+				break;
+
+			// add to lists
+			s.data.add(new Integer(datum));
+			s.count.add(new Integer(count));
+
+			idx++;
+
+			// break out if we have 'length' samples
+			if (idx == length)
+				break;
+		}
+		
+	}
+
 
 	public void save(Sample s, BufferedWriter w) throws IOException {
 		// header
@@ -161,68 +258,137 @@ public class Heidelberg implements Filetype {
 		w.newLine();
 		w.write("Length=" + s.data.size());
 		w.newLine();
+		
+		String tmp;
+		if((tmp = (String) s.meta.get("title")) != null) {
+			w.write("Location=" + tmp);
+			w.newLine();
+		}
+		if((tmp = (String) s.meta.get("species")) != null) {
+			w.write("Species=" + tmp);
+			w.newLine();
+		}
 
 		// WRITE ME: finish out the header stuff
 
-		// don't want to die if somebody tries to save a raw file
-		boolean countIsNull = (s.count == null);
-		boolean wjIsNull = !s.hasWeiserjahre();
+		// NOTE
+		// We convert all values of '0' to '1' here.
+		// This is heidelberg's signal for end. Hmm.
+		// This should be better documented
+		// (BUG: what if it's negative?)
+		// (ICK: if it changes the data, it's harder to test)
+		
+		// We're writing out a 'tree' or a 'single'...
+		// Data is for a single sample, not a sum or anything.
+		if(s.count == null) {
+			w.write("DATA:Tree");
+			w.newLine();
+			int column = 0;
 
-		// data
-		w.write("DATA:Chrono"); // what's "chrono" mean?
-		w.newLine();
-		int column = 0;
-		for (int i = 0; i < s.data.size(); i++) {
-			String datum = s.data.get(i).toString();
-			if (datum.equals("0"))
-				datum = "1"; // DOCUMENT me!
-			// (BUG: what if it's negative?)
-			// (ICK: if it changes the data, it's harder to test)
-			String count = (countIsNull ? "0" : s.count.get(i).toString());
-			String up = (wjIsNull ? "0" : s.incr.get(i).toString());
-			String dn = (wjIsNull ? "0" : s.decr.get(i).toString());
-
-			// output ("%-5d%-5d%-5d%-5d", datum, count, up, dn)
-			writeDatum(w, s.data.get(i));
-			// WAS: w.write(StringUtils.leftPad(s.data.get(i).toString(), 5));
-			writeDatum(w, (countIsNull ? "0" : s.count.get(i)));
-			// WAS: w.write(StringUtils.leftPad(countIsNull ? "0" : s.count.get(i).toString(), 5));
-			w.write(StringUtils.leftPad(wjIsNull ? "0" : s.incr.get(i)
-					.toString(), 5));
-			w.write(StringUtils.leftPad(wjIsNull ? "0" : s.decr.get(i)
-					.toString(), 5));
-
-			// newline every 4
-			if (column % 4 == 3)
-				w.newLine();
-
-			column++;
+			for (int i = 0; i < s.data.size(); i++) {
+				String datum = s.data.get(i).toString();
+				if (datum.equals("0"))
+					datum = "1"; 
+				
+				// six space padding in this type of file
+				writeDatum6(w, datum);
+				
+				// newline every 10
+				if (column % 10 == 9)
+					w.newLine();
+				
+				column++;
+			}
+			
+			// extra 0's to pad
+			while (column % 10 != 0) {
+				w.write("     0");
+				column++;
+			}
+			w.newLine();
 		}
+		// Data is for a sum without Weiserjahre data
+		else if(!s.hasWeiserjahre()) {
+			w.write("DATA:Double");
+			w.newLine();
+			int column = 0;
 
-		// COMBINE these: newline-every-4, and extra-0's-to-pad both
-		// care about exactly one value: position (column).
-
-		// EXTRACT method: writeData(data, count, incr, decr)
-
-		// REFACTOR: use iterators instead of for loop?
-
-		// extra 0's to pad
-		while (column % 4 != 3) {
-			w.write("    0    0    0    0");
-			column++;
+			for (int i = 0; i < s.data.size(); i++) {
+				String datum = s.data.get(i).toString();
+				if (datum.equals("0"))
+					datum = "1"; 
+				
+				// six space padding in this type of file
+				writeDatum6(w, datum);
+				writeDatum6(w, s.count.get(i));
+				
+				// newline every 5
+				if (column % 5 == 4)
+					w.newLine();
+				
+				column++;
+			}
+			
+			// extra 0's to pad
+			while (column % 5 != 0) {
+				w.write("     0     0");
+				column++;
+			}
+			w.newLine();
+			
 		}
-		w.newLine();
+		else {
+			boolean countIsNull = (s.count == null);
+			boolean wjIsNull = !s.hasWeiserjahre();
+			
+			// data
+			w.write("DATA:Chrono"); // what's "chrono" mean?
+			w.newLine();
+			int column = 0;
+			for (int i = 0; i < s.data.size(); i++) {
+				String datum = s.data.get(i).toString();
+				if (datum.equals("0"))
+					datum = "1"; // DOCUMENT me!
+				// (BUG: what if it's negative?)
+				// (ICK: if it changes the data, it's harder to test)
+				String count = (countIsNull ? "0" : s.count.get(i).toString());
+				String up = (wjIsNull ? "0" : s.incr.get(i).toString());
+				String dn = (wjIsNull ? "0" : s.decr.get(i).toString());
+
+				// output ("%-5d%-5d%-5d%-5d", datum, count, up, dn)
+				writeDatum5(w, datum);
+				// WAS: w.write(StringUtils.leftPad(s.data.get(i).toString(), 5));
+				writeDatum5(w, (countIsNull ? "0" : s.count.get(i)));
+				// WAS: w.write(StringUtils.leftPad(countIsNull ? "0" : s.count.get(i).toString(), 5));
+				w.write(StringUtils.leftPad(wjIsNull ? "0" : s.incr.get(i)
+						.toString(), 5));
+				w.write(StringUtils.leftPad(wjIsNull ? "0" : s.decr.get(i)
+						.toString(), 5));
+
+				// newline every 4
+				if (column % 4 == 3)
+					w.newLine();
+
+				column++;
+			}
+
+			// COMBINE these: newline-every-4, and extra-0's-to-pad both
+			// care about exactly one value: position (column).
+
+			// extra 0's to pad
+			while (column % 4 != 0) {
+				w.write("    0    0    0    0");
+				column++;
+			}
+			w.newLine();
+		}
 	}
 
-	private void writeData(BufferedWriter w, String data, String count,
-			String incr, String decr) throws IOException {
-		writeDatum(w, data);
-		writeDatum(w, count);
-		writeDatum(w, incr);
-		writeDatum(w, decr);
-	}
-
-	private void writeDatum(BufferedWriter w, Object o) throws IOException {
+	private void writeDatum5(BufferedWriter w, Object o) throws IOException {
 		w.write(StringUtils.leftPad(o.toString(), 5));
+	}
+
+	private void writeDatum6(BufferedWriter w, Object o) throws IOException {
+		w.write(StringUtils.leftPad(o.toString(), 6));
 	}
 }
