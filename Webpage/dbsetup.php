4f05364f011273041c4a09b707e138da41dbef22
petebrew <p.brewer@ltrr.arizona.edu>
2007-09-26 19:08:06 +0000

    Typo

--- Webpage/dbsetup.php
+++ Webpage/dbsetup.php
@@ -2,7 +2,7 @@
 
 //Set up database connection
 $conn_string = "host=localhost port=5432 dbname=dendro user=user password=pwd";
-$dbconn = pg_coonnect ($conn_string);
+$dbconn = pg_connect ($conn_string);
 
 //Date format
 $sql = "set datestyle to 'Postgres, European'";
