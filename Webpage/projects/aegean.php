c8c3326d2d233f2d8f69b3270eea6f50d8ce562a
petebrew <p.brewer@ltrr.arizona.edu>
2007-09-26 18:52:49 +0000

    Project webpages

---
+++ Webpage/projects/aegean.php
@@ -0,0 +1,58 @@
+<?php
+
+$title = "Aegean Dendrochronology Project";
+$vertnavbarfilename = "phpinc/vert-navbar-projects.php";
+
+
+include('../phpinc/2col-header.php');
+
+$sideimagesarray["housecoringcrete.jpg"] = "Drilling a core sample from an old house in Crete.";
+//$sideimagesarray["britameasuring.jpg"] = "Measuring a sample of Gordion wood.";
+$sideimagesarray["alalakh.jpg"] = "Sampling archaeological charcoal at Alalakh, southern Turkey.";
+//$sideimagesarray["gordion3.jpg"] = "Sample from Gordion 3 timber from the Midas Mount Tumulus, Turkey.";
+$sideimagesarray["bellaarti.jpg"] = "Waterlogged timbers, Bologna, Bella Arti, Italy.";
+$sideimagesarray["yenikapi.jpg"] = "Sampling oak timbers at Yenikap&#305; &#304;stanbul.";
+$sideimagesarray["brescialavagnone.jpg"] = "Brescia, Lavagnone, Italy.";
+
+?>
+
+<p>
+Our key long-range goal is to build long multi-millennial scale tree-ring chronologies in the Aegean and Near East that will extend from the present to the early Holocene to cover, broadly speaking, the last 10,000 years of human and environmental history. Our raison d'&ecirc;tre is to provide a dating method for the study of history and prehistory in the Aegean that is accurate to the year. This kind of precision has, up to now, been lacking in ancient studies of this area. Indeed, few archaeological problems stimulate as much rancor as chronology, especially that of the Eastern Mediterranean. The work of the Aegean and Near Eastern Dendrochronology Project aims to help to bring some kind of rational and neutral order to Aegean and Near Eastern chronology from the Neolithic to the present.
+</p>
+
+<p>
+We also aim to provide a fundamental climate and environmental studies resource for a region which was the cradle of a number of civilizations central to human history from the origins of agriculture through to the Classical period, the Medieval period, and beyond. 
+</p>
+
+<p>To date, more than 10 million tree-ring measurements have led to the compilation of chronologies covering (but not wholly covering) some 9,000 years. Our aim is to fill in the gaps. See our bargraph (in PDF format) to get an idea of the time periods covered. The most difficult periods for us are the first half millennium B.C. and the first millennium A.D. If you have relevant samples, <a href="/samples.php">please contact us</a>!
+</p>
+
+<div id="gallery">
+    <div class="gallery-thumbnail">
+        <a href="http://dendro.cornell.edu/photo.php?photourl=/images/fullres/gordion3.jpg&photocaption=Sample of Gordion 3 timber (Juniper) from the Midas Mound Tumulus, Turkey.">
+            <img class="thumbnail" src="/images/thumbnails/gordion3.jpg" alt="Sample of Gordion 3 timber (Juniper) from the Midas Mound Tumulus, Turkey">Sample of Gordion 3 timber (Juniper) from the Midas Mound Tumulus, Turkey
+        </a>
+    </div>
+    <div class="gallery-thumbnail">
+        <a href="http://dendro.cornell.edu/photo.php?photourl=/images/fullres/turkishoak.jpg&photocaption=Oak samples from Northern Turkey.">
+            <img class="thumbnail" src="/images/thumbnails/turkishoak.jpg" alt="Oak samples from Northern Turkey">Oak samples from Northern Turkey</a>
+    </div>
+</div>
+
+<img src="/images/screenshots/usa-example.png">
+<span class="caption">An example of a crossdating plot from a site in Turkey.</span>
+<span></span>
+<p></p>
+<p></p>
+<a name="israel"></a><h3>Israel Dendrochronology Project</h3>
+
+<p>A new dendrochronology sub-project started in 2007 with the aim of building a chronology for Israel.  Further details on this project will be posted soon.</p>
+
+<p>
+If you have any <a href="/samples.php">relevant samples</a> please contact either <a href="mailto:sm456@cornell.edu">Sturt Manning</a> or <a href="mailto:bel9@cornell.edu">Brita Lorentzen</a>. 
+</p>
+<?php
+
+include('../phpinc/2col-footer.php');
+
+?>
---
+++ Webpage/projects/usa.php
@@ -0,0 +1,23 @@
+<?php
+
+$title = "New York State and NE North American Dendrochronology Project ";
+$vertnavbarfilename = "phpinc/vert-navbar-projects.php";
+
+
+include('../phpinc/2col-header.php');
+
+$sideimagesarray["sawinglogmaine.jpg"] = "Sampling a 14,000 year old log in Portland, Maine.";
+$sideimagesarray["coremcgraw.jpg"] = "Coring 19th century timbers, McGraw Hall, Cornell.";
+
+
+?>
+
+<p>The Cornell Tree-Ring Laboratory is working to date historic buildings, and to build tree ring chronologies backwards in time by species and site/area, in the New York State and Northeast North American region. This project began with work by Dr. Carol Griggs on historic buildings in the New York State and wider Northeast North American region and especially work she carried out as part of her PhD thesis on excavated wood at mastodon sites in New York. This project is now formally part of the Cornell Tree Ring Laboratory. We have been delighted with progress so far. This project is also now part of Sturt's <a href="/course.php">dendro course</a> at Cornell for undergraduates, and the students will thus be able to learn practical dendrochronology in the field.</p>
+
+<p>We are interested in all wood from the present back to the late Pleistocene. This ranges from historic houses and buildings and structures in New York and Northeast North America to old to very old wood from excavations, ponds, lakes, road cuttings, etc. We have so far worked with oak, hemlock, pine, spruce, elm, larch, ash, maple and beech (and we are happy to try other species). We can arrange to carry out the sampling where appropriate. For historic buildings we can use small cores taken from timbers, and these can be filled and "hidden" if requested. Please see our <a href="/samples.php">submitting samples</a> page for more information.</p>
+
+<?php
+
+include('../phpinc/2col-footer.php');
+
+?>
