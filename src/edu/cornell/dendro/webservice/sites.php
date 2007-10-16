7b0eeb248e87a41b3f1d7807fa98a4b6d62235d3
petebrew <p.brewer@ltrr.arizona.edu>
2007-10-16 14:27:43 +0000

    Initial commit of sites and notes web services

---
+++ src/edu/cornell/dendro/webservice/sites.php
@@ -0,0 +1,183 @@
+<?php
+//*******************************************************************
+////// PHP Corina Middleware
+////// License: GPL
+////// Author: Peter Brewer
+////// E-Mail: p.brewer@cornell.edu
+//////
+////// Requirements : PHP >= 5.0
+//////*******************************************************************
+header('Content-Type: application/xhtml+xml; charset=utf-8');
+
+require_once("config.php");
+require_once("inc/dbsetup.php");
+require_once("inc/meta.php");
+require_once("inc/site.php");
+
+// Extract parameters from request and ensure no SQL has been injected
+$theMode = strtolower(addslashes($_GET['mode']));
+$theID = (int) $_GET['id'];
+if(isset($_GET['code'])) $theCode = addslashes($_GET['code']);
+if(isset($_GET['name'])) $theName = addslashes($_GET['name']);
+
+// Create new meta object and check required input parameters and data types
+switch($theMode)
+{
+    case "update":
+        $myMetaHeader = new meta("update");
+        if($theID == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'id' field is required.");
+        if(($theName == NULL) && ($theCode==NULL)) $myMetaHeader->setMessage("902", "Missing parameter - either 'name' or 'code' fields (or both) must be specified.");
+        break;
+
+    case "read":
+        $myMetaHeader = new meta("read");
+        //if(!($theName==NULL) && (strlen($theName)<3)) $myMetaHeader->setMessage("904", "Parameter too short - 'name' field must contain three or more characters."); 
+        //if(!($theCode==NULL) && (strlen($theName)<2)) $myMetaHeader->setMessage("904", "Parameter too short - 'name' field must contain two or more characters."); 
+        break;
+
+    case "delete":
+        $myMetaHeader = new meta("delete");
+        if($theID == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'id' field is required.");
+        break;
+
+    case "create":
+        $myMetaHeader = new meta("create");
+        if($theName == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'name' field is required.");
+        if($theCode == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'code' field is required.");
+        break;
+
+    default:
+        $myMetaHeader = new meta("help");
+        $myMetaHeader->setUser("Guest", "", "");
+        // Output the resulting XML
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        echo "<corina>\n";
+        echo $myMetaHeader->asXML();
+        echo "<help> Details of how to use this web service will be added here later! </help>";
+        echo "</corina>\n";
+        die;
+}
+
+// Set user details
+$myMetaHeader->setUser("Guest", "", "");
+
+//Only attempt to run SQL if there are no errors so far
+if(!($myMetaHeader->status == "Error"))
+{
+    // Create site object 
+    $mySite = new site();
+    $parentTagBegin = $mySite->getParentTagBegin();
+    $parentTagEnd = $mySite->getParentTagEnd();
+
+    // Set existing parameters if updating or deleting from database
+    if($theMode=='update' || $theMode=='delete') 
+    {
+        $success = $mySite->setParamsFromDB($theID);
+        if(!$success) 
+        {
+            $myMetaHeader->setMessage($mySite->getLastErrorCode(), $mySite->getLastErrorMessage());
+        }
+    }
+
+    // Update parameters in object if updating or creating an object 
+    if($theMode=='update' || $theMode=='create')
+    {
+        if (isset($theName)) $mySite->setName($theName);
+        if (isset($theCode)) $mySite->setCode($theCode);
+
+        // Write to object to database
+        $success = $mySite->writeToDB();
+        if($success)
+        {
+            $xmldata=$mySite->asXML();
+        }
+        else
+        {
+            $myMetaHeader->setMessage($mySite->getLastErrorCode(), $mySite->getLastErrorMessage());
+        }
+    }
+
+    // Delete record from db if requested
+    if($theMode=='delete')
+    {
+        // Write to Database
+        $success = $mySite->deleteFromDB();
+        if($success)
+        {
+            $xmldata=$mySite->asXML();
+        }
+        else
+        {
+            $myMetaHeader->setMessage($mySite->getLastErrorCode(), $mySite->getLastErrorMessage());
+        }
+    }
+
+    if($theMode=='read')
+    {
+        $dbconnstatus = pg_connection_status($dbconn);
+        if ($dbconnstatus ===PGSQL_CONNECTION_OK)
+        {
+            // DB connection ok
+            // Build SQL depending on parameters
+            if(!$theID==NULL)
+            {
+                $sql="select * from tblsite where siteid=$theID order by siteid";
+            }
+            elseif((isset($theName)) && (isset($theCode)))
+            {
+                echo "2";
+                $sql="select * from tblsite where name='$theName' and code='$theCode' order by siteid";
+            }
+            elseif(isset($theName))
+            {
+                $sql="select * from tblsite where name ilike '%$theName%' order by siteid";
+            }
+            elseif(isset($theCode))
+            {
+                $sql="select * from tblsite where code ilike '%$theCode%' order by siteid";
+            }
+            else
+            {
+                $sql="select * from tblsite order by siteid";
+            }
+
+            if($sql)
+            {
+                // Run SQL
+                $result = pg_query($dbconn, $sql);
+                while ($row = pg_fetch_array($result))
+                {
+                    $mySite = new site();
+                    $success = $mySite->setParamsFromDB($row['siteid']);
+                    $success2 = $mySite->setChildParamsFromDB();
+
+                    if($success && $success2)
+                    {
+                        $xmldata.=$mySite->asXML();
+                    }
+                    else
+                    {
+                        $myMetaHeader->setMessage($mySite->getLastErrorCode, $mySite->getLastErrorMessage);
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            // Connection bad
+            $myMetaHeader->setMessage("001", "Error connecting to database");
+        }
+    }
+}
+
+// Output the resulting XML
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+echo "<corina>\n";
+echo $myMetaHeader->asXML();
+echo "<data>\n";
+echo $parentTagBegin."\n";
+echo $xmldata;
+echo $parentTagEnd."\n";
+echo "</data>\n";
+echo "</corina>";
---
+++ src/edu/cornell/dendro/webservice/treeNotes.php
@@ -0,0 +1,174 @@
+<?php
+//*******************************************************************
+////// PHP Corina Middleware
+////// License: GPL
+////// Author: Peter Brewer
+////// E-Mail: p.brewer@cornell.edu
+//////
+////// Requirements : PHP >= 5.0
+//////*******************************************************************
+header('Content-Type: application/xhtml+xml; charset=utf-8');
+
+require_once("config.php");
+require_once("inc/dbsetup.php");
+require_once("inc/meta.php");
+require_once("inc/treeNote.php");
+
+// Extract parameters from request and ensure no SQL has been injected
+$theMode = strtolower(addslashes($_GET['mode']));
+$theID = (int) $_GET['id'];
+$theNote = addslashes($_GET['note']);
+$theIsStandard = fromStringToPHPBool($_GET['isstandard']);
+
+// Create new meta object and check required input parameters and data types
+switch($theMode)
+{
+    case "update":
+        $myMetaHeader = new meta("update");
+        if($theNote == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'note' field is required.");
+        if($theID == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'id' field is required.");
+        if((gettype($theIsStandard)!="boolean") && ($theIsStandard!=NULL)) $myMetaHeader->setMessage("901", "Invalid parameter - 'isstandard' must be a boolean.");
+        if(!(gettype($theID)=="integer") && !($theID)) $myMetaHeader->setMessage("901", "Invalid parameter - 'id' field must be an integer.");
+        break;
+
+    case "read":
+        $myMetaHeader = new meta("read");
+        if(!(gettype($theID)=="integer") && !($theID==NULL)) $myMetaHeader->setMessage("901", "Invalid parameter - 'id' field must be an integer.");
+        if(!($theID>0) && !($theID==NULL)) $myMetaHeader->setMessage("901", "Invalid parameter - 'id' field must be a valid positive integer.");
+        break;
+
+    case "delete":
+        $myMetaHeader = new meta("delete");
+        if($theID == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'id' field is required.");
+        if(!(gettype($theID)=="integer") && !(isset($theID))) $myMetaHeader->setMessage("901", "Invalid parameter - 'id' field must be an integer.");
+        break;
+
+    case "create":
+        $myMetaHeader = new meta("create");
+        // Set default value if not specified
+        if($theIsStandard == NULL) $theIsStandard = FALSE;
+        if($theNote == NULL) $myMetaHeader->setMessage("902", "Missing parameter - 'note' field is required.");
+        if(!(gettype($theIsStandard)=="boolean")) $myMetaHeader->setMessage("901", "Invalid parameter - 'isstandard' must be a boolean.");
+        break;
+
+    default:
+        $myMetaHeader = new meta("help");
+        $myMetaHeader->setUser("Guest", "", "");
+        // Output the resulting XML
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        echo "<corina>\n";
+        echo $myMetaHeader->asXML();
+        echo "<help> Details of how to use this web service will be added here later! </help>";
+        echo "</corina>\n";
+        die;
+}
+
+// Set user details
+$myMetaHeader->setUser("Guest", "", "");
+
+//Only attempt to run SQL if there are no errors so far
+if(!($myMetaHeader->status == "Error"))
+{
+    // Create treeNote object 
+    $mySiteNote = new treeNote();
+    $parentTagBegin = $mySiteNote->getParentTagBegin();
+    $parentTagEnd = $mySiteNote->getParentTagEnd();
+
+    // Set existing parameters if updating or deleting from database
+    if($theMode=='update' || $theMode=='delete') 
+    {
+        $success = $mySiteNote->setParamsFromDB($theID);
+        if(!$success) 
+        {
+            $myMetaHeader->setMessage($mySiteNote->getLastErrorCode(), $mySiteNote->getLastErrorMessage());
+        }
+
+    }
+
+    // Update parameters in object if updating or creating an object 
+    if($theMode=='update' || $theMode=='create')
+    {	
+        if (isset($theNote)) $mySiteNote->setNote($theNote);
+        if (isset($theIsStandard)) $mySiteNote->setIsStandard($theIsStandard);
+
+        // Write to object to database
+        $success = $mySiteNote->writeToDB();
+        if($success)
+        {
+            $xmldata=$mySiteNote->asXML();
+        }
+        else
+        {
+            $myMetaHeader->setMessage($mySiteNote->getLastErrorCode(), $mySiteNote->getLastErrorMessage());
+        }
+    }
+
+    // Delete record from db if requested
+    if($theMode=='delete')
+    {
+        // Write to Database
+        $success = $mySiteNote->deleteFromDB();
+        if($success)
+        {
+            $xmldata=$mySiteNote->asXML();
+        }
+        else
+        {
+            $myMetaHeader->setMessage($mySiteNote->getLastErrorCode(), $mySiteNote->getLastErrorMessage());
+        }
+    }
+
+    if($theMode=='read')
+    {
+        $dbconnstatus = pg_connection_status($dbconn);
+        if ($dbconnstatus ===PGSQL_CONNECTION_OK)
+        {
+            // DB connection ok
+            // Build SQL depending on parameters
+            if($theID==NULL)
+            {
+                $sql="select * from tlkptreenote order by treenoteid";
+            }
+            else
+            {
+                $sql="select * from tlkptreenote where treenoteid=$theID order by treenoteid";
+            }
+
+            if($sql)
+            {
+                // Run SQL
+                $result = pg_query($dbconn, $sql);
+                while ($row = pg_fetch_array($result))
+                {
+                    $mySiteNote = new treeNote();
+                    $success = $mySiteNote->setParamsFromDB($row['treenoteid']);
+
+                    if($success)
+                    {
+                        $xmldata.=$mySiteNote->asXML();
+                    }
+                    else
+                    {
+                        $myMetaHeader->setMessage($mySiteNote->getLastErrorCode, $mySiteNote->getLastErrorMessage);
+                    }
+                }
+            }
+        }
+        else
+        {
+            // Connection bad
+            $myMetaHeader->setMessage("001", "Error connecting to database");
+        }
+    }
+}
+
+// Output the resulting XML
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+echo "<corina>\n";
+echo $myMetaHeader->asXML();
+echo "<data>\n";
+echo $parentTagBegin."\n";
+echo $xmldata;
+echo $parentTagEnd."\n";
+echo "</data>\n";
+echo "</corina>";
