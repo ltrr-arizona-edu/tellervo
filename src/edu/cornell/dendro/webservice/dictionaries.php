8c68428f17fb2a59aca49f032fdc5a7abc327d66
petebrew <p.brewer@ltrr.arizona.edu>
2007-10-16 14:26:33 +0000

    Changed to use classes

--- src/edu/cornell/dendro/webservice/dictionaries.php
+++ src/edu/cornell/dendro/webservice/dictionaries.php
@@ -14,6 +14,14 @@ require_once("inc/dbsetup.php");
 require_once("inc/meta.php");
 require_once("inc/dictionaries.php");
 require_once("inc/siteNote.php");
+require_once("inc/treeNote.php");
+require_once("inc/readingNote.php");
+require_once("inc/vMeasurementNote.php");
+require_once("inc/specimenType.php");
+require_once("inc/terminalRing.php");
+require_once("inc/specimenQuality.php");
+require_once("inc/specimenContinuity.php");
+require_once("inc/pith.php");
 
 // Extract parameters from request and ensure no SQL has been injected
 $theMode = strtolower(addslashes($_GET['mode']));
@@ -31,7 +39,7 @@ switch($theMode)
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
         echo "<corina>\n";
         echo $myMetaHeader->asXML();
-        echo "<help> Details of how to use this web service will be added here later! </help>";
+        echo "<help> Details of how to use this web service will be added here later! In the mean time try access https:/dendro.cornell.edu/dictionaries.php?mode=read </help>";
         echo "</corina>\n";
         die;
 }
@@ -45,64 +53,194 @@ if(!($myMetaHeader->status == "Error"))
 {
 
     global $dbconn;
-
-    // Specimen Type
-    $myXML.= "<specimenTypeDictionary>\n";
-    $sql = "select * from tlkpSpecimenType";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
-    {
-        $myXML.= "<specimenType id=\"".$row['specimentypeid']."\">".$row['label']."</specimenType>\n";
-    }
-    $myXML.="</specimenTypeDictionary>\n";
     
-    // Terminal Ring
-    $myXML.= "<terminalRingDictionary>\n";
-    $sql = "select * from tlkpTerminalRing";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
+    // Specimen Type 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
     {
-        $myXML.= "<terminalRing id=\"".$row['terminalringid']."\">".$row['label']."</terminalRing>\n";
-    }
-    $myXML.= "</terminalRingDictionary>\n";
+        // DB connection ok
+        $sql="select * from tlkpspecimentype order by specimentypeid";
 
-    // specimen quality 
-    $myXML.= "<specimenQualityDictionary>\n";
-    $sql = "select * from tlkpspecimenquality";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
-    {
-        $myXML.= "<specimenQuality id=\"".$row['specimenqualityid']."\">".$row['label']."</specimenQuality>\n";
+        if($sql)
+        {
+            $myDummySpecimenType = new specimenType();
+            $myXML.=$myDummySpecimenType->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $mySpecimenType = new specimenType();
+                $success = $mySpecimenType->setParamsFromDB($row['specimentypeid']);
+
+                if($success)
+                {
+                    $myXML.=$mySpecimenType->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($mySiteNote->getLastErrorCode(), $mySiteNote->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummySpecimenType->getParentTagEnd();
+        }
     }
-    $myXML.= "</specimenQualityDictionary>\n";
+    else
+    {
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
+    }
     
+    // Terminal Ring 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
+    {
+        // DB connection ok
+        $sql="select * from tlkpterminalring order by terminalringid";
+
+        if($sql)
+        {
+            $myDummyTerminalRing = new terminalRing();
+            $myXML.=$myDummyTerminalRing->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $myTerminalRing = new terminalRing();
+                $success = $myTerminalRing->setParamsFromDB($row['terminalringid']);
+
+                if($success)
+                {
+                    $myXML.=$myTerminalRing->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($myTerminalRing->getLastErrorCode(), $myTerminalRing->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummyTerminalRing->getParentTagEnd();
+        }
+    }
+    else
+    {
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
+    }
+    
+    
+    // Specimen Quality 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
+    {
+        // DB connection ok
+        $sql="select * from tlkpspecimenquality order by specimenqualityid";
+
+        if($sql)
+        {
+            $myDummySpecimenQuality = new specimenQuality();
+            $myXML.=$myDummySpecimenQuality->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $mySpecimenQuality = new specimenQuality();
+                $success = $mySpecimenQuality->setParamsFromDB($row['specimenqualityid']);
+
+                if($success)
+                {
+                    $myXML.=$mySpecimenQuality->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($mySpecimenQuality->getLastErrorCode(), $mySpecimenQuality->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummySpecimenQuality->getParentTagEnd();
+        }
+    }
+    else
+    {
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
+    }
+
     // Pith 
-    $myXML.= "<pithDictionary>\n";
-    $sql = "select * from tlkppith";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
     {
-        $myXML.= "<pith id=\"".$row['pithid']."\">".$row['label']."</pith>\n";
+        // DB connection ok
+        $sql="select * from tlkppith order by pithid";
+
+        if($sql)
+        {
+            $myDummyPith = new pith();
+            $myXML.=$myDummyPith->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $myPith = new pith();
+                $success = $myPith->setParamsFromDB($row['pithid']);
+
+                if($success)
+                {
+                    $myXML.=$myPith->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($myPith->getLastErrorCode(), $myPith->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummyPith->getParentTagEnd();
+        }
     }
-    $myXML.= "</pithDictionary>\n";
-    
-    // Taxon ranks 
-    $myXML.= "<taxonRankDictionary>\n";
-    $sql = "select * from tlkptaxonrank order by rankorder";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
+    else
     {
-        $myXML.= "<taxonRank id=\"".$row['taxonrankid']."\" rankorder=\"".$row['rankorder']."\">".$row['taxonrank']."</taxonRank>\n";
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
     }
-    $myXML.= "</taxonRankDictionary>\n";
     
+    // Specimen Continuity 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
+    {
+        // DB connection ok
+        $sql="select * from tlkpspecimencontinuity order by specimencontinuityid";
+
+        if($sql)
+        {
+            $myDummySpecimenContinuity = new specimenContinuity();
+            $myXML.=$myDummySpecimenContinuity->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $mySpecimenContinuity = new specimenContinuity();
+                $success = $mySpecimenContinuity->setParamsFromDB($row['specimencontinuityid']);
+
+                if($success)
+                {
+                    $myXML.=$mySpecimenContinuity->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($mySpecimenContinuity->getLastErrorCode(), $mySpecimenContinuity->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummySpecimenContinuity->getParentTagEnd();
+        }
+    }
+    else
+    {
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
+    }
     
     // Site note 
     $dbconnstatus = pg_connection_status($dbconn);
     if ($dbconnstatus ===PGSQL_CONNECTION_OK)
     {
         // DB connection ok
-        $sql="select * from tlkpsitenote order by sitenoteid";
+        $sql="select * from tlkpsitenote where isstandard=true order by sitenoteid";
 
         if($sql)
         {
@@ -121,7 +259,7 @@ if(!($myMetaHeader->status == "Error"))
                 }
                 else
                 {
-                    $myMetaHeader->setMessage($mySiteNote->getLastErrorCode, $mySiteNote->getLastErrorMessage);
+                    $myMetaHeader->setMessage($mySiteNote->getLastErrorCode(), $mySiteNote->getLastErrorMessage());
                 }
             }
             $myXML.=$myDummySiteNote->getParentTagEnd();
@@ -133,38 +271,113 @@ if(!($myMetaHeader->status == "Error"))
         $myMetaHeader->setMessage("001", "Error connecting to database");
     }
     
-
     // Tree note 
-    $myXML.= "<treeNoteDictionary>\n";
-    $sql = "select * from tlkptreenote where isstandard='t'";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
     {
-        $myXML.= "<treeNote id=\"".$row['treenoteid']."\">".$row['note']."</treeNote>\n";
-    }
-    $myXML.= "</treeNoteDictionary>\n";
+        // DB connection ok
+        $sql="select * from tlkptreenote where isstandard=true order by treenoteid";
 
-    // Measurement note 
-    $myXML.= "<measurementNoteDictionary>\n";
-    $sql = "select * from tlkpvmeasurementnote where isstandard='t'";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
+        if($sql)
+        {
+            $myDummyTreeNote = new treeNote();
+            $myXML.=$myDummyTreeNote->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $myTreeNote = new treeNote();
+                $success = $myTreeNote->setParamsFromDB($row['treenoteid']);
+
+                if($success)
+                {
+                    $myXML.=$myTreeNote->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($myTreeNote->getLastErrorCode(), $myTreeNote->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummyTreeNote->getParentTagEnd();
+        }
+    }
+    else
     {
-        $myXML.= "<measurementNote id=\"".$row['measurementnoteid']."\">".$row['note']."</measurementNote>\n";
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
     }
-    $myXML.= "</measurementNoteDictionary>\n";
+    
+    // VMeasurement note 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
+    {
+        // DB connection ok
+        $sql="select * from tlkpvmeasurementnote where isstandard=true order by vmeasurementnoteid";
 
+        if($sql)
+        {
+            $myDummyVMeasurementNote = new vmeasurementNote();
+            $myXML.=$myDummyVMeasurementNote->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $myVMeasurementNote = new vmeasurementNote();
+                $success = $myVMeasurementNote->setParamsFromDB($row['vmeasurementnoteid']);
+
+                if($success)
+                {
+                    $myXML.=$myVMeasurementNote->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($myVMeasurementNote->getLastErrorCode(), $myVMeasurementNote->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummyVMeasurementNote->getParentTagEnd();
+        }
+    }
+    else
+    {
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
+    }
+    
     // Reading note 
-    $myXML.= "<readingNoteDictionary>\n";
-    $sql = "select * from tlkpreadingnote where isstandard='t'";
-    $result = pg_query($dbconn, $sql);
-    while ($row = pg_fetch_array($result)) 
+    $dbconnstatus = pg_connection_status($dbconn);
+    if ($dbconnstatus ===PGSQL_CONNECTION_OK)
     {
-        $myXML.= "<readingNote id=\"".$row['readingnoteid']."\">".$row['note']."</readingNote>\n";
+        // DB connection ok
+        $sql="select * from tlkpreadingnote where isstandard=true order by readingnoteid";
+
+        if($sql)
+        {
+            $myDummyReadingNote = new readingNote();
+            $myXML.=$myDummyReadingNote->getParentTagBegin();
+            // Run SQL
+            $result = pg_query($dbconn, $sql);
+            while ($row = pg_fetch_array($result))
+            {
+                $myReadingNote = new readingNote();
+                $success = $myReadingNote->setParamsFromDB($row['readingnoteid']);
+
+                if($success)
+                {
+                    $myXML.=$myReadingNote->asXML();
+                }
+                else
+                {
+                    $myMetaHeader->setMessage($myReadingNote->getLastErrorCode(), $myReadingNote->getLastErrorMessage());
+                }
+            }
+            $myXML.=$myDummyReadingNote->getParentTagEnd();
+        }
     }
-    $myXML.= "</readingNoteDictionary>\n";
-
-
+    else
+    {
+        // Connection bad
+        $myMetaHeader->setMessage("001", "Error connecting to database");
+    }
 }
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
