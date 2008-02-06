74f97c5274bf57efbb630ad1b6951672999ac849
petebrew <p.brewer@ltrr.arizona.edu>
2008-02-06 21:14:51 +0000

    Initial add of webservice index

---
+++ src/edu/cornell/dendro/webservice/index.php
@@ -0,0 +1,22 @@
+<?php
+//*******************************************************************
+////// PHP Corina Middleware
+////// License: GPL
+////// Author: Peter Brewer
+////// E-Mail: p.brewer@cornell.edu
+//////
+////// Requirements : PHP >= 5.0
+//////*******************************************************************
+
+header('Content-Type: application/xhtml+xml; charset=utf-8');
+
+
+require_once("config.php");
+require_once("inc/meta.php");
+require_once("inc/output.php");
+
+$myMetaHeader = new meta("help");
+writeIntroOutput($myMetaHeader);
+
+
+?>
