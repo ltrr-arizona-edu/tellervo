91e70019e471aad6e61639a0239179afbfe535e6
lucasmo <lucas.madar@gmail.com>
2008-05-02 19:31:38 +0000

    Remove the webpage from the repository! mwhaha!

--- Webpage/phpinc/vert-navbar-projects.php
+++
@@ -1,7 +0,0 @@
-	<div id="section-navigation">
-		<ul>
-			<li><a href="/projects/aegean.php">Aegean and Near East</a></li>
-			<li><a href="/projects/usa.php">North America</a></li>
-                        <li><a href="/projects/dendrochem.php">Dendrochemistry</a></li>
-		</ul>
-	</div>
--- Webpage/projects/dendrochem.php
+++
@@ -1,29 +0,0 @@
-<?php
-
-$title = "Dendrochemistry";
-$vertnavbarfilename = "phpinc/vert-navbar-projects.php";
-
-include('../phpinc/2col-header.php');
-$sideimagesarray["charliesync.jpg"] = "Charlotte Pearson working at CHESS";
-$sideimagesarray["ALCa.jpg"] = "Aluminium and calcium in pine from Turkey showing an increase in concentrations following the 1815 Tambora eruption.";
-
-
-?>
-
-<p>
-Working in collaboration with <a href="http://www.chess.cornell.edu">Cornell High Energy Synchrotron Source (CHESS)</a> and utilizing other state of the art instrumentation for geochemical analysis, we have begun an exciting new investigation into the chemistry of long tree-ring sequences. 
-</p>
-
-<p>
-This research has multiple cross disciplinary applications but our primary goal is to ascertain whether it is possible to characterize an elemental fingerprint that could be used to link an absolutely dated tree ring with a particular volcanic eruption.</p>
-
-<p>The basic premise of dendrochemical research is that the annual growth ring can act as a time capsule of the environmental chemistry of the year in which it formed. Although this is a greatly simplified version of a highly complex filtering system between tree and environment, it is a fact substantiated (all be it somewhat controversially) by numerous publications on the impact of anthropogenic pollution.</p>
-
-<p>We hope to build upon our previous research (<a href="http://linkinghub.elsevier.com/retrieve/pii/S0883292706001715">Pearson et al 2006</a> and <a href="http://dendro.cornell.edu/articles/pearson2005a.pdf">Pearson et al 2005</a>) to resolve whether dendrochemical principles can be applied to the detection of volcanic eruptions in long sequences of tree rings from the Aegean and Near Eastern, and other chronologies. If a unique identifying volcanic signature could be characterized we would then provide a provable means to dendrochronologically date volcanic events over the last c.10,000 years. This could open up possibilities for re-evaluating chronologies tied by ice core data or tephra horizons, and strengthen pre-existing correlations with other proxy indicators from the world's master tree ring chronologies.
-</p>
-
-<?php
-
-include('../phpinc/2col-footer.php');
-
-?>
