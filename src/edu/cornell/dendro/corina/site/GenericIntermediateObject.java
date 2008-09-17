728972fcf8669ffca018e3fc3b3cfb2360dfa2b2
lucasmo <lucas.madar@gmail.com>
2008-09-17 21:18:25 +0000

    Add eventmodel to Intermediate Objects

--- src/edu/cornell/dendro/corina/site/GenericIntermediateObject.java
+++ src/edu/cornell/dendro/corina/site/GenericIntermediateObject.java
@@ -1,9 +1,13 @@
 package edu.cornell.dendro.corina.site;
 
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Vector;
 
 import org.jdom.Element;
 
+import edu.cornell.dendro.corina.sample.SampleEvent;
+import edu.cornell.dendro.corina.sample.SampleListener;
 import edu.cornell.dendro.corina.webdbi.ResourceIdentifier;
 
 /**
@@ -75,4 +79,44 @@ public abstract class GenericIntermediateObject {
 	}
 	
 	public abstract Element toXML();
+	
+	// listening interface
+	private Vector<IntermediateObjectListener> listeners = new Vector<IntermediateObjectListener>();
+	
+	public void addIntermediateObjectListener(IntermediateObjectListener l) {
+		if(!listeners.contains(l))
+			listeners.add(l);
+	}
+	
+	public void removeIntermediateObjectListener(IntermediateObjectListener l) {
+		listeners.remove(l);
+	}
+
+	protected void fireEvent(String method) {
+		// alert all listeners
+		Vector<IntermediateObjectListener> l;
+		synchronized (this) {
+			l = (Vector<IntermediateObjectListener>) listeners.clone();
+		}
+
+		int size = l.size();
+
+		if (size == 0)
+			return;
+
+		IntermediateObjectEvent e = new IntermediateObjectEvent(this);
+
+		try {
+			Class<?> types[] = new Class[] { IntermediateObjectEvent.class };
+			Method m = IntermediateObjectListener.class.getMethod(method, types);
+			Object args[] = new Object[] { e };
+
+			for (int i = 0; i < size; i++) {
+				SampleListener listener = (SampleListener) l.elementAt(i);
+				m.invoke(listener, args);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
---
+++ src/edu/cornell/dendro/corina/site/IntermediateObjectListener.java
@@ -0,0 +1,5 @@
+package edu.cornell.dendro.corina.site;
+
+public interface IntermediateObjectListener {
+	public void intermediateObjectChanged(IntermediateObjectEvent ie);
+}
