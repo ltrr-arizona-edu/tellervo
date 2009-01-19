66ae5c43bfb35364fb4b9583ab3e409292b601e4
lucasmo <lucas.madar@gmail.com>
2009-01-19 19:18:55 +0000

    Add function to find all object ancestors

--- databases/cpgdb/recursive-objects.sql
+++ databases/cpgdb/recursive-objects.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION cpgdb.FindObjectAncestor(objid integer)
+CREATE OR REPLACE FUNCTION cpgdb.FindObjectTopLevelAncestor(objid integer)
 RETURNS tblObject AS $$
 DECLARE
    parentid integer;
@@ -14,11 +14,44 @@ BEGIN
       RETURN lastRow;
    END IF;
 
-   SELECT * INTO lastRow FROM cpgdb.FindObjectAncestor(lastRow.parentObjectID);
+   SELECT * INTO lastRow FROM cpgdb.FindObjectTopLevelAncestor(lastRow.parentObjectID);
    RETURN lastRow;
 END;
 $$ LANGUAGE 'plpgsql' STABLE;
 	
+CREATE OR REPLACE FUNCTION cpgdb.recurseFindObjectAncestors(objid integer, _recursionlevel integer)
+RETURNS SETOF tblObject AS $$
+DECLARE
+   parentid integer;
+   lastRow tblObject;
+   lastParent tblObject.parentObjectId%TYPE;
+   ref refcursor;
+   recursionlevel integer := _recursionlevel;
+BEGIN
+   IF recursionlevel > 50 THEN
+      RAISE EXCEPTION 'Maximum recursion exceeded';
+   END IF;
+
+   SELECT * INTO lastRow FROM tblObject WHERE objectId = objid;
+
+   -- Not found? End of the line.
+   IF NOT FOUND THEN
+      EXIT;
+   END IF;
+
+   -- Return myself?
+   IF recursionlevel <> 0 THEN
+      RETURN NEXT lastRow;
+   END IF;
+
+   parentid := lastRow.parentObjectId;
+
+   FOR lastRow IN SELECT * FROM cpgdb.recurseFindObjectAncestors(parentid, recursionlevel + 1) LOOP
+      RETURN NEXT lastRow;
+   END LOOP;
+END;
+$$ LANGUAGE 'plpgsql' STABLE;
+
 CREATE OR REPLACE FUNCTION cpgdb.recurseFindObjectDescendants(objid integer, _recursionlevel integer)
 RETURNS SETOF tblObject AS $$
 DECLARE
@@ -63,8 +96,14 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql' STABLE;
 
+CREATE OR REPLACE FUNCTION cpgdb.FindObjectAncestors(tblObject.ObjectId%TYPE, boolean)
+RETURNS SETOF tblObject AS '
+  SELECT * FROM cpgdb.recurseFindObjectAncestors($1, (SELECT CASE WHEN $2=TRUE THEN 1 ELSE 0 END))
+' LANGUAGE SQL;
+
 CREATE OR REPLACE FUNCTION cpgdb.FindObjectDescendants(tblObject.ObjectId%TYPE, boolean)
 RETURNS SETOF tblObject AS '
   SELECT * FROM cpgdb.recurseFindObjectDescendants($1, (SELECT CASE WHEN $2=TRUE THEN -1 ELSE 0 END))
 ' LANGUAGE SQL;
+
 
