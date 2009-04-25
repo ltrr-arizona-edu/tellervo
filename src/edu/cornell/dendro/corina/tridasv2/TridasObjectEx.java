2b1c1b4a504327d6e70e0a64bb20f7b9615cfdc0
lucasmo <lucas.madar@gmail.com>
2009-04-25 09:40:41 +0000

    Remove old jars, update new generated classes to use a plugin that generates .equals methods and .hashCode methods!

--- src/edu/cornell/dendro/corina/tridasv2/TridasObjectEx.java
+++ src/edu/cornell/dendro/corina/tridasv2/TridasObjectEx.java
@@ -1,6 +1,14 @@
 package edu.cornell.dendro.corina.tridasv2;
 
+import javax.xml.bind.annotation.XmlTransient;
+
 import org.tridas.schema.TridasObject;
 
 public class TridasObjectEx extends TridasObject {
+	/** How many child series do I have (vmeasurements) */
+	@XmlTransient
+	private int mySeriesCount;
+	/** How many child series do my children and I have? */
+	@XmlTransient
+	private int childSeriesCount;
 }
