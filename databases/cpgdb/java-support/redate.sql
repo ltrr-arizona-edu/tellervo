ec2188afa7875fe6b0a324be05e2205608bb8e8e
lucasmo <lucas.madar@gmail.com>
2009-08-04 16:04:50 +0000

    remove redate param from redate support code

--- databases/cpgdb/java-support/redate.sql
+++ databases/cpgdb/java-support/redate.sql
@@ -4,30 +4,30 @@
 -- RETURNS void
 -- IN PARAMETERS
 ---- paramVMeasurementID(uuid)
----- paramRedate(integer)
 ---- paramCurrentVMeasurementResultID(uuid)
 
 CREATE OR REPLACE FUNCTION cpgdbj.qupdVMeasurementResultOpRedate(
   IN paramVMeasurementID uuid,
-  IN paramRedate integer,
   IN paramCurrentVMeasurementResultID uuid
 ) RETURNS void AS $$
 DECLARE
   chosenDatingTypeID tblRedate.RedatingTypeID%TYPE;
+  chosenStartYear tblRedate.StartYear%TYPE;
 BEGIN
+  -- Get the new start year and dating type id
+  SELECT startYear,redatingTypeID INTO chosenStartYear,chosenDatingTypeID 
+   FROM tblRedate
+   WHERE VMeasurementID=$1;
+
   -- Update tblVMeasurementResult with the redating information
   UPDATE tblVMeasurementResult
-   SET VMeasurementID=$1, StartYear=$2
-   WHERE VMeasurementResultID=$3;
-
-  -- Get the new dating type id
-  SELECT redatingTypeID INTO chosenDatingTypeID FROM tblRedate
-   WHERE VMeasurementID=$1;
+   SET VMeasurementID=$1, StartYear=chosenStartYear
+   WHERE VMeasurementResultID=$2;
 
   -- Update to the new dating type, if it's not null
   IF chosenDatingTypeID IS NOT NULL THEN
      UPDATE tblVMeasurementResult SET DatingTypeID=chosenDatingTypeID
-      WHERE VMeasurementResultID=$3;
+      WHERE VMeasurementResultID=$2;
   END IF;
 END;
 $$ LANGUAGE PLPGSQL VOLATILE;
