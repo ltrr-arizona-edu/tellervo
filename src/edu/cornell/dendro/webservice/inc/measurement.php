70e1e623dd0099329da926e4fafbe9951bd4cd5a
petebrew <p.brewer@ltrr.arizona.edu>
2009-08-30 18:57:43 +0000

    Intercepting duplicate version error

--- src/edu/cornell/dendro/webservice/inc/measurement.php
+++ src/edu/cornell/dendro/webservice/inc/measurement.php
@@ -1520,6 +1520,13 @@ class measurement extends measurementEntity implements IDBAccessor
 						$result = pg_get_result($dbconn);
 						if(pg_result_error_field($result, PGSQL_DIAG_SQLSTATE))
 						{
+
+							if(substr(pg_result_error($result), 8, 21)=='EVERSIONALREADYEXISTS')
+							{
+								trigger_error("911"."A series with this version number already exists.  Change version number and try again", E_USER_ERROR);
+								return FALSE;
+							}			
+											
 							$this->setErrorMessage("002", pg_result_error($result)."--- SQL was $sql");
 							return FALSE;
 						}
@@ -1548,6 +1555,11 @@ class measurement extends measurementEntity implements IDBAccessor
 							$result = pg_get_result($dbconn);
 							if(pg_result_error_field($result, PGSQL_DIAG_SQLSTATE))
 							{
+								if(substr(pg_result_error($result), 8, 21)=='EVERSIONALREADYEXISTS')
+								{
+									trigger_error("911"."A series with this version number already exists.  Change version number and try again", E_USER_ERROR);
+									return FALSE;
+								}								
 								// Insert failed
 								$this->setErrorMessage("002", pg_result_error($result)."--- SQL was $insertSQL");
 								return FALSE;
@@ -1568,6 +1580,12 @@ class measurement extends measurementEntity implements IDBAccessor
 					$result = pg_get_result($dbconn);
 					if(pg_result_error_field($result, PGSQL_DIAG_SQLSTATE))
 					{
+						if(substr(pg_result_error($result), 8, 21)=='EVERSIONALREADYEXISTS')
+						{
+							trigger_error("911"."A series with this version number already exists.  Change version number and try again", E_USER_ERROR);
+							return FALSE;
+						}
+												
 						$this->setErrorMessage("002", pg_result_error($result)."--- SQL was $sql");
 						return FALSE;
 					}
@@ -1700,16 +1718,22 @@ class measurement extends measurementEntity implements IDBAccessor
 					foreach($transaction as $stmt)
 					{
 						if ($stmt==NULL) continue;
-						$result = pg_query($dbconn, $stmt);
-						//$result = pg_get_result($dbconn);
-						if(!$result)
+						pg_send_query($dbconn, $stmt);
+						$result = pg_get_result($dbconn);	
+						if(pg_result_error_field($result, PGSQL_DIAG_SQLSTATE))
 						{
-							// All gone badly so throw error and rollback
-							$this->setErrorMessage("002", pg_last_error()."--- SQL was $stmt");
+							if(substr(pg_result_error($result), 8, 21)=='EVERSIONALREADYEXISTS')
+							{
+								trigger_error("911"."A series with this version number already exists.  Change version number and try again", E_USER_ERROR);
+							}	
+							else
+							{
+								trigger_error("002", pg_result_error($result)."--- SQL was $stmt");
+							}
+							
 							pg_query($dbconn, "rollback;");
 							return FALSE;
 						}
-
 					}
 
 					// All gone well so commit transaction to db
