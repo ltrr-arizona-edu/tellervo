1315e9eee2b446eea62a1a7eee6f6ceaff519fb5
petebrew <p.brewer@ltrr.arizona.edu>
2010-03-11 17:59:42 +0000

    Some more TRiDaS v1.2.1 changes

--- databases/cpgdb/views/vwtblobject.sql
+++ databases/cpgdb/views/vwtblobject.sql
@@ -1,14 +1,18 @@
 DROP VIEW vwtblObject CASCADE;
 
 CREATE VIEW vwtblObject AS
-  SELECT cquery.countofchildvmeasurements, tblobject.comments, tblobject.objectid, tblobject.title, tblobject.code, tblobject.createdtimestamp, tblobject.lastmodifiedtimestamp, tblobject.locationgeometry, 
-       tblobject.locationtypeid, tblobject.locationprecision, tblobject.locationcomment, array_to_string(tblobject.file, '><') as file, tblobject.creator, tblobject.owner, tblobject.parentobjectid, tblobject.description, 
-       tblobject.objecttypeid, loctype.locationtype, objtype.objecttype, covtemp.coveragetemporal, covtempfound.coveragetemporalfoundation
-   FROM tblobject
-   LEFT JOIN tlkplocationtype loctype ON tblobject.locationtypeid = loctype.locationtypeid
-   LEFT JOIN tlkpobjecttype objtype ON tblobject.objecttypeid = objtype.objecttypeid
-   LEFT JOIN tlkpcoveragetemporal covtemp ON tblobject.coveragetemporalid = covtemp.coveragetemporalid
-   LEFT JOIN tlkpcoveragetemporalfoundation covtempfound ON tblobject.coveragetemporalfoundationid = covtempfound.coveragetemporalfoundationid
+  SELECT cquery.countofchildvmeasurements, o.comments, o.objectid, o.title, o.code, o.createdtimestamp, o.lastmodifiedtimestamp, o.locationgeometry, 
+       o.locationtypeid, o.locationprecision, o.locationcomment, 
+       o.locationaddressline1, o.locationaddressline2, o.locationcityortown,
+o.locationstateprovinceregion, o.locationpostalcode, o.locationcountry,
+       
+       array_to_string(o.file, '><') as file, o.creator, o.owner, o.parentobjectid, o.description, 
+       o.objecttypeid, loctype.locationtype, objtype.objecttype, covtemp.coveragetemporal, covtempfound.coveragetemporalfoundation
+   FROM tblobject o
+   LEFT JOIN tlkplocationtype loctype ON o.locationtypeid = loctype.locationtypeid
+   LEFT JOIN tlkpobjecttype objtype ON o.objecttypeid = objtype.objecttypeid
+   LEFT JOIN tlkpcoveragetemporal covtemp ON o.coveragetemporalid = covtemp.coveragetemporalid
+   LEFT JOIN tlkpcoveragetemporalfoundation covtempfound ON o.coveragetemporalfoundationid = covtempfound.coveragetemporalfoundationid
    LEFT JOIN (SELECT e.objectid AS masterobjectid, count(e.objectid) AS countofchildvmeasurements
               FROM tblelement e
               JOIN tblsample s ON s.elementid = e.elementid
@@ -16,7 +20,7 @@ CREATE VIEW vwtblObject AS
               JOIN tblmeasurement m ON m.radiusid = r.radiusid
               JOIN tblvmeasurementderivedcache vc ON vc.measurementid = m.measurementid
               GROUP BY e.objectid) cquery 
-        ON cquery.masterobjectid = tblobject.objectid;
+        ON cquery.masterobjectid = o.objectid;
 
 GRANT ALL on vwtblobject to "Webgroup";
 
