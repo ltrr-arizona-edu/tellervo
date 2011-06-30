06445cad2dcd246d741fee7dd24e8f450cf4e104
petebrew <p.brewer@ltrr.arizona.edu>
2011-06-30 14:07:46 +0000

    Removing generated from repository

--- src/main/generated/edu/cornell/dendro/corina/schema/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dendro.cornell.edu/schema/corina/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package edu.cornell.dendro.corina.schema;
