ca35a65a0967bfae87b76d2b2942dd98b2ee33a0
petebrew <p.brewer@ltrr.arizona.edu>
2012-02-22 13:56:08 +0000

    Fixing tree conflict

--- src/main/generated/org/tellervo/schema/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tellervo.org/schema/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.tellervo.schema;
