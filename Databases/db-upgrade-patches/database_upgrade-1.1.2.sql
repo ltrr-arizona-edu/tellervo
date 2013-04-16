899af67580a9512e0a51204b37e5643b7cec76be
petebrew <p.brewer@ltrr.arizona.edu>
2013-04-16 05:49:07 +0000

    Started implementing referencing external files and websites

--- Databases/db-upgrade-patches/database_upgrade-1.1.2.sql
+++ Databases/db-upgrade-patches/database_upgrade-1.1.2.sql
@@ -145,7 +145,34 @@ $BODY$
   LANGUAGE plpgsql STABLE
   COST 100;
 
+DROP VIEW vwtblsample;
 
+CREATE VIEW vwtblsample AS
+    SELECT s.sampleid, s.code, s.comments, s.code AS title, s.elementid, s.samplingdate, s.createdtimestamp, 
+    s.lastmodifiedtimestamp, st.sampletypeid, st.sampletype, array_to_string(s.file, '><'::text) AS file, s."position", 
+    s.state, s.knots, s.description, dc.datecertainty, s.boxid, lc.objectcode, lc.elementcode FROM (((tblsample s LEFT JOIN 
+    tlkpdatecertainty dc ON ((s.datecertaintyid = dc.datecertaintyid))) LEFT JOIN tlkpsampletype st ON ((s.typeid = st.sampletypeid)))
+     LEFT JOIN vwlabcodesforsamples lc ON ((s.sampleid = lc.sampleid)));
+
+DROP VIEW vwtblelement;
+CREATE VIEW vwtblelement AS
+    SELECT (SELECT findobjecttoplevelancestor.code FROM cpgdb.findobjecttoplevelancestor(e.objectid) 
+    findobjecttoplevelancestor(objectid, title, code, createdtimestamp, lastmodifiedtimestamp, locationgeometry, 
+    locationtypeid, locationprecision, locationcomment, creator, owner, 
+    parentobjectid, description, objecttypeid, coveragetemporalid, coveragetemporalfoundationid, comments, coveragetemporal, 
+    coveragetemporalfoundation, locationaddressline1, locationaddressline2, locationcityortown, locationstateprovinceregion, 
+    locationpostalcode, locationcountry)) AS objectcode, e.comments, e.elementid, e.locationprecision, e.code AS title, e.code, 
+    e.createdtimestamp, e.lastmodifiedtimestamp, e.locationgeometry, e.islivetree, e.originaltaxonname, e.locationtypeid, 
+    e.locationcomment, e.locationaddressline1, e.locationaddressline2, e.locationcityortown, e.locationstateprovinceregion, 
+    e.locationpostalcode, e.locationcountry, array_to_string(e.file, '><'::text) AS file, e.description, e.processing, e.marks, 
+    e.diameter, e.width, e.height, e.depth, e.unsupportedxml, e.objectid, e.elementtypeid, e.elementauthenticityid, e.elementshapeid, 
+    auth.elementauthenticity, shape.elementshape, tbltype.elementtype, loctype.locationtype, e.altitude, e.slopeangle, e.slopeazimuth, 
+    e.soildescription, e.soildepth, e.bedrockdescription, vwt.taxonid, vwt.taxonlabel, vwt.parenttaxonid, vwt.colid, vwt.colparentid, 
+    vwt.taxonrank, unit.unit AS units FROM ((((((tblelement e LEFT JOIN tlkpelementauthenticity auth 
+    ON ((e.elementauthenticityid = auth.elementauthenticityid))) LEFT JOIN tlkpelementshape shape ON ((e.elementshapeid = 
+    shape.elementshapeid))) LEFT JOIN tlkpelementtype tbltype ON ((e.elementtypeid = tbltype.elementtypeid)))
+     LEFT JOIN tlkplocationtype loctype ON ((e.locationtypeid = loctype.locationtypeid))) LEFT JOIN tlkpunit unit 
+     ON ((e.units = unit.unitid))) LEFT JOIN vwtlkptaxon vwt ON ((e.taxonid = vwt.taxonid)));
 
 
   
