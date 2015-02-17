2de2d4b343ab9a41b1b1a51197f431d5e6c6ef8f
petebrew <p.brewer@ltrr.arizona.edu>
2015-02-17 22:31:41 +0000

    Initial commit of QC1000 measuring platform

---
+++ src/main/java/org/tellervo/desktop/hardware/device/QC1000.java
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright (C) 2010 Lucas Madar and Peter Brewer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Lucas Madar
+ *     Peter Brewer
+ ******************************************************************************/
+package org.tellervo.desktop.hardware.device;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
+import org.tellervo.desktop.hardware.MeasuringSampleIOEvent;
+
+
+
+
+public class QC1000 extends GenericASCIIDevice {
+
+	@Override
+	public String toString() {
+		return "Metronics Quick-Chek 1000";
+	}
+
+	@Override
+	public void setDefaultPortParams(){
+		
+		//MeasureJ2X defaults to using 2 stop bits but Tellervo/Java/something bombs if you 
+		//try to write to the port with 2 stop bits set.  So lets stick with 1 stop bit for now!
+		
+		baudRate = BaudRate.B_600;
+		dataBits = DataBits.DATABITS_8;
+		stopBits = StopBits.STOPBITS_2;
+		parity = PortParity.NONE;
+		flowControl = FlowControl.NONE;
+		lineFeed = LineFeed.CR;
+		unitMultiplier = UnitMultiplier.TIMES_1000;
+	}
+	
+
+	@Override
+	public Boolean isBaudEditable() {
+		return true;
+	}
+
+	@Override
+	public Boolean isDatabitsEditable() {
+		return false;
+	}
+
+	@Override
+	public Boolean isLineFeedEditable() {
+		return true;
+	}
+
+	@Override
+	public Boolean isParityEditable() {
+		return false;
+	}
+
+	@Override
+	public Boolean isStopbitsEditable() {
+		return false;
+	}
+	
+	@Override
+	public Boolean isFlowControlEditable(){
+		return false;
+	}
+
+	@Override
+	public Boolean isUnitsEditable() {
+		return false;
+	}
+
+	@Override
+	protected void sendRequest(String strCommand)
+	{
+		OutputStream output;
+
+
+    	try {
+    		
+	    output = getSerialPort().getOutputStream();
+	    OutputStream outToPort=new DataOutputStream(output); 
+	    
+	    byte[] command = (strCommand+lineFeed.toCommandString()).getBytes();
+	    outToPort.write(command);
+        fireMeasuringSampleEvent(this, MeasuringSampleIOEvent.RAW_DATA, strCommand, DataDirection.SENT);
+
+	    
+    	}
+    	catch (Exception ioe) {
+			fireMeasuringSampleEvent(this, MeasuringSampleIOEvent.ERROR, "Error sending command to serial port");
+
+    	}	
+	}
+	
+	@Override
+	public Boolean isRequestDataCapable() {
+		return true;
+	}
+	
+	@Override
+	public Boolean isReverseMeasureCapable() {
+		return true;
+	}
+	
+	/**
+	 * Send zero command to Quadra-check QC10
+	 */
+	@Override
+	public void zeroMeasurement()
+	{
+		String strZeroDataCommand = "Z-";
+		sendRequest(strZeroDataCommand);
+	}
+	
+	@Override
+	public void requestMeasurement() {
+		String strZeroDataCommand = "P-";
+		sendRequest(strZeroDataCommand);
+		
+	}
+}
