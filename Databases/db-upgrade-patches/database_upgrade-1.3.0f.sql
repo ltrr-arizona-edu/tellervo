ff47a5a87ab6eb30ed7ea20982f5c53a4b5ea65e
petebrew <p.brewer@ltrr.arizona.edu>
2016-07-01 14:44:36 -0700

    Merged

---
+++ Databases/db-upgrade-patches/database_upgrade-1.3.0f.sql
@@ -0,0 +1,8 @@
+SET pljava.libjvm_location TO '/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so';
+
+DELETE FROM sqlj.jar_repository where jarname='tellervo_jar';
+SELECT sqlj.install_jar('file:///usr/share/tellervo-server/tellervo-pljava.jar', 'tellervo_jar', false);
+SELECT sqlj.set_classpath('cpgdb', 'tellervo_jar');
+UPDATE sqlj.jar_repository SET jarowner='tellervo';
+
+UPDATE tblconfig SET value = '1.3.0' WHERE key = 'wsversion';
