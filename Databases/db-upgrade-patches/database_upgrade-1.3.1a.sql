4cbe396f32b0c81d540e5465f34246fc3fe64e0b
petebrew <p.brewer@ltrr.arizona.edu>
2016-12-19 15:38:55 -0700

    Typo

--- Databases/db-upgrade-patches/database_upgrade-1.3.1a.sql
+++ Databases/db-upgrade-patches/database_upgrade-1.3.1a.sql
@@ -2,4 +2,4 @@ ALTER TABLE tblobject ALTER COLUMN projectid DROP DEFAULT;
 
 -- Remove index so sub-objects with same code can be included in database
 DROP INDEX object_code_index;
-ALTER TABLE tblobject ADD CONSTRAINT unique_objectcode_perparent UNIQUE (code, parentobjectid),
+ALTER TABLE tblobject ADD CONSTRAINT unique_objectcode_perparent UNIQUE (code, parentobjectid);
