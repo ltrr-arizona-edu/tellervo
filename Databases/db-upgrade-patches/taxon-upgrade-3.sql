523f4e635868dee08233adf80531bf8fdd11634f
petebrew <p.brewer@ltrr.arizona.edu>
2017-01-18 10:51:47 -0700

    Fix adding of taxa that aren't in CoL

---
+++ Databases/db-upgrade-patches/taxon-upgrade-3.sql
@@ -0,0 +1,5 @@
+INSERT INTO tlkptaxon (taxonid, taxonrankid, label, parenttaxonid) values (1009, 9, 'Pinus rigida Mill.', 455);
+INSERT INTO tlkptaxon (taxonid, taxonrankid, label, parenttaxonid) values (1010, 9, 'Quercus montana Willd.', 565);
+
+UPDATE tlkptaxon SET colid='X-' || taxonid WHERE colid IS NULL;
+ALTER TABLE tlkptaxon ALTER COLUMN colid SET NOT NULL;
