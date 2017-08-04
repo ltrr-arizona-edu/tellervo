cce649534d422f05dbf9d0bd5368a318237afacc
petebrew <p.brewer@ltrr.arizona.edu>
2017-08-04 10:52:02 -0700

    Added missing taxa request by Christy Rollinson

---
+++ Databases/db-upgrade-patches/taxon-upgrade-4.sql
@@ -0,0 +1,3 @@
+INSERT INTO tlkptaxon (taxonid, taxonrankid, label, parenttaxonid, colid) values (1012, 9, 'Prunus serotina Ehrh.', 552, 'X-1012');
+INSERT INTO tlkptaxon (taxonid, taxonrankid, label, parenttaxonid, colid) values (1013, 9, 'Ostrya virginiana (Mill.) K.Koch', 413, 'X-1013');
+INSERT INTO tlkptaxon (taxonid, taxonrankid, label, parenttaxonid, colid) values (1014, 9, 'Quercus macrocarpa Michx.', 565, 'X-1014');
