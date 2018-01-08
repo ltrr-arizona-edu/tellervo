3dd020b1972a8aef20cc3ca65f057d62cf6937b3
petebrew <p.brewer@ltrr.arizona.edu>
2018-01-08 16:55:32 -0700

    Added ability to enable/disable cascade delete

---
+++ Databases/db-options/disable_cascade_delete.sql
@@ -0,0 +1,15 @@
+ALTER TABLE tblelement
+DROP CONSTRAINT "tblelement_objectid_fkey",
+ADD CONSTRAINT "tblelement_objectid_fkey" FOREIGN KEY (objectid) REFERENCES tblobject(objectid) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE tblsample
+DROP CONSTRAINT "fkey_sample-tree",
+ADD CONSTRAINT "fkey_sample-tree" FOREIGN KEY (elementid) REFERENCES tblelement(elementid) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE tblradius
+DROP CONSTRAINT "fkey_radius-sample",
+ADD CONSTRAINT "fkey_radius-sample" FOREIGN KEY (sampleid) REFERENCES tblsample(sampleid) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE tblmeasurement
+DROP CONSTRAINT "fkey_measurement-radius",
+ADD CONSTRAINT "fkey_measurement-radius" FOREIGN KEY (radiusid) REFERENCES tblradius(radiusid) ON UPDATE CASCADE ON DELETE RESTRICT;
