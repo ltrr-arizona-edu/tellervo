cbb900cf2fefa2d80b5ffd8fc95dd7f1beff2ac2
petebrew <p.brewer@ltrr.arizona.edu>
2019-08-13 15:47:30 -0700

    Fix for issue #118

--- src/main/java/org/tellervo/desktop/gui/menus/actions/ToolsReverseAction.java
+++ src/main/java/org/tellervo/desktop/gui/menus/actions/ToolsReverseAction.java
@@ -3,6 +3,7 @@ package org.tellervo.desktop.gui.menus.actions;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
 
 import org.tellervo.desktop.editor.AbstractEditor;
 import org.tellervo.desktop.manip.Reverse;
@@ -23,7 +24,15 @@ public class ToolsReverseAction extends AbstractAction{
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		Reverse.reverse(editor.getSample());
+		
+		int selectedOption = JOptionPane.showConfirmDialog(editor, 
+                "Are you sure you want to reverse your series?", 
+                "Choose", 
+                JOptionPane.YES_NO_CANCEL_OPTION); 
+		if (selectedOption == JOptionPane.YES_OPTION) {
+			Reverse.reverse(editor.getSample());
+		}
+		
 		
 		
 	}
