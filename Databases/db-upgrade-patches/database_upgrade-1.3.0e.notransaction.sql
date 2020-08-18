31a20e2792aa956c23601029ca5df2f7638227a6
petebrew <p.brewer@ltrr.arizona.edu>
2020-08-17 17:27:59 -0700

    Upgrading server to run on Debian Buster / Ubuntu Focal Fossa

--- Databases/db-upgrade-patches/database_upgrade-1.3.0e.notransaction.sql
+++ Databases/db-upgrade-patches/database_upgrade-1.3.0e.notransaction.sql
@@ -3,6 +3,6 @@
 
 \set ON_ERROR_STOP true
 
-SET pljava.libjvm_location TO '/usr/share/tellervo-server/libjvm.so';
-ALTER SYSTEM SET pljava.libjvm_location TO '/usr/share/tellervo-server/libjvm.so';
+SET pljava.libjvm_location TO '/usr/lib/jvm/java-11-openjdk-amd64/lib/server/libjvm.so';
+ALTER SYSTEM SET pljava.libjvm_location TO '/usr/lib/jvm/java-11-openjdk-amd64/lib/server/libjvm.so';
 CREATE EXTENSION IF NOT EXISTS pljava;
